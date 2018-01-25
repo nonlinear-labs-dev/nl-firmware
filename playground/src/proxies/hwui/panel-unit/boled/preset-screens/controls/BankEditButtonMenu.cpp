a89c1d5c521f0a1a1735813a5a09e87ddf141778
schlenkibus <justus.scheil@parteimail.de>
2018-01-25 13:01:56 +0100

    made it fancier

--- playground/src/proxies/hwui/panel-unit/boled/preset-screens/controls/BankEditButtonMenu.cpp
+++ playground/src/proxies/hwui/panel-unit/boled/preset-screens/controls/BankEditButtonMenu.cpp
@@ -116,7 +116,9 @@ BankEditButtonMenu::FileInfos BankEditButtonMenu::extractFileInfos(std::experime
 bool BankEditButtonMenu::applicableBackupFilesFilter(std::experimental::filesystem::directory_entry term)
 {
   auto fileName = term.path().filename().string();
-  return fileName.find(".xml") == Glib::ustring::npos || fileName.find(".xml.zip") != Glib::ustring::npos && fileName.find(".xml.tar.gz") != Glib::ustring::npos;
+  auto fileIsBank = fileName.find(".xml") != Glib::ustring::npos;
+  auto fileIsBackup = fileName.find(".xml.zip") != Glib::ustring::npos || fileName.find(".xml.tar.gz") != Glib::ustring::npos;
+  return !(fileIsBank && !fileIsBackup);
 }
 
 void BankEditButtonMenu::importBankFromPath(std::experimental::filesystem::directory_entry file)
@@ -160,6 +162,8 @@ void BankEditButtonMenu::exportBank()
   {
     boled.setOverlay(new RenameExportLayout(selBank, [](Glib::ustring newExportName, std::shared_ptr<PresetBank> bank)
     {
+      auto& panelunit = Application::get().getHWUI()->getPanelUnit();
+      auto& boled = panelunit.getEditPanel().getBoled();
       auto outPath = BankEditButtonMenu::createValidOutputPath(newExportName);
       boled.resetOverlay();
       boled.setOverlay(new SplashLayout());
