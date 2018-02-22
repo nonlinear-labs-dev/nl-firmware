64ce2d8dd3b761eee89a16932717ab286d8ddad0
Justus Scheil <justus.scheil@parteimail.de>
2018-02-22 16:29:08 +0100

    Delete SearchingFileDialog.h

--- playground/src/proxies/hwui/panel-unit/boled/file/SearchingFileDialog.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <proxies/hwui/DFBLayout.h>
-
-
-class SearchingFileDialog : public DFBLayout {
-public:
-    SearchingFileDialog(std::string title);
-    bool onButton(int i, bool down, ButtonModifiers modifiers) override;
-};
