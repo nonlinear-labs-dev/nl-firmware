aab5e5b2fbdf7822a205461b5f822a4f00c45cc0
schlenkibus <justus.scheil@parteimail.de>
2018-07-04 18:24:08 +0200

    cleaned the Color Boxes in Preset/EditBuffer Info by extracting into own class

---
+++ NonMaps/src/com/nonlinearlabs/NonMaps/client/world/overlay/InfoDialog/ColorTagBox.java
@@ -0,0 +1,62 @@
+package com.nonlinearlabs.NonMaps.client.world.overlay.InfoDialog;
+
+import com.gargoylesoftware.htmlunit.javascript.host.Console;
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.nonlinearlabs.NonMaps.client.world.maps.presets.bank.preset.ColorTag;
+import com.nonlinearlabs.NonMaps.client.world.maps.presets.bank.preset.ColorTag.Color;
+
+public abstract class ColorTagBox {
+
+	HTMLPanel panel ;
+	Button buttons[];
+	
+	public ColorTagBox() {
+		buttons = new Button[7];
+		panel = new HTMLPanel("div", "");
+		for(Color c: Color.values()) {
+			panel.add(addColorButton(c));
+		}
+		panel.getElement().addClassName("tag-div");
+	}
+	
+	public HTMLPanel getHTML() {
+		return panel;
+	}
+	
+	private Button addColorButton(ColorTag.Color c) {		
+		Button b = new Button();
+		if(c == ColorTag.Color.none)
+			b.setHTML("<div style=\"font-size: smaller;\">✕</div>"); 
+		b.getElement().addClassName("colortaggingfield-" + c.name());
+		b.getElement().addClassName("colortaggingfield-template");
+		b.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				onBoxClick(c);
+				updateCurrentHighlight(c);
+			}
+		});
+		buttons[c.ordinal()] = b;
+		return b;
+	}
+	
+	public void updateCurrentHighlight(Color current) {
+		int activeColorIndex = current.ordinal();
+		for(int i = 0; i < buttons.length; i++) {
+			Button b = buttons[i];
+			
+			GWT.log("updateHighlioght!" + current.name());
+			
+			if(activeColorIndex != i)
+				b.getElement().removeClassName("colortag-current");
+			else
+				b.getElement().addClassName("colortag-current");
+		}
+	}
+	
+	public abstract void onBoxClick(Color c);
+}
