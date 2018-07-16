e45030987b0fed1684f28b649e300aec3edf297d
schlenkibus <justus.scheil@parteimail.de>
2018-07-16 16:31:16 +0200

    wip

--- playground/src/device-info/update-info.sh
+++ playground/src/device-info/update-info.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-
 cd ../..
-echo $("pwd")
 git rev-parse --abbrev-ref HEAD > resources/version-infos/git-branch
 git rev-list HEAD --count > resources/version-infos/git-commit-count
 git rev-parse --short HEAD > resources/version-infos/git-head-ref
+echo "[OKAY!] Wrote Version Info"
