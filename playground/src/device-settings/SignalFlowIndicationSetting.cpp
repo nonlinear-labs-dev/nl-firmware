896ce2c4559354e80b3021dea8c01c4410e92763
schlenkibus <justus.scheil@parteimail.de>
2018-07-19 16:57:43 +0200

    changed bahavior for enum setting editors at ends-> no retuzrn to other end of list, and changed signal flow setting editor to a normal enum editor

--- playground/src/device-settings/SignalFlowIndicationSetting.cpp
+++ playground/src/device-settings/SignalFlowIndicationSetting.cpp
@@ -1,14 +1,4 @@
 #include <device-settings/SignalFlowIndicationSetting.h>
 
 SignalFlowIndicationSetting::SignalFlowIndicationSetting(Settings& settings) : BooleanSetting(settings, true) {
-
-}
-
-SignalFlowIndicationSetting::~SignalFlowIndicationSetting() {
-
-}
-
-bool SignalFlowIndicationSetting::set (tEnum m)
-{
-	return BooleanSetting::set (m);
 }
