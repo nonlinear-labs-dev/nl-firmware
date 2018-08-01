e778e42d365851a7be971bf6841906ea1714555a
schlenkibus <justus.scheil@parteimail.de>
2018-08-01 14:44:38 +0200

    added Lock Indicator in Sound Tab

--- NonMaps/src/com/nonlinearlabs/NonMaps/client/world/overlay/belt/sound/BeltSoundLayout.java
+++ NonMaps/src/com/nonlinearlabs/NonMaps/client/world/overlay/belt/sound/BeltSoundLayout.java
@@ -1,9 +1,14 @@
 package com.nonlinearlabs.NonMaps.client.world.overlay.belt.sound;
 
+import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.xml.client.Node;
+import com.nonlinearlabs.NonMaps.client.NonMaps;
+import com.nonlinearlabs.NonMaps.client.world.overlay.Label;
 import com.nonlinearlabs.NonMaps.client.world.overlay.OverlayControl;
 import com.nonlinearlabs.NonMaps.client.world.overlay.OverlayLayout;
 import com.nonlinearlabs.NonMaps.client.world.overlay.belt.Belt;
+
+import java.util.concurrent.locks.Lock;
 
 public class BeltSoundLayout extends OverlayLayout {
 
@@ -13,6 +18,7 @@ public class BeltSoundLayout extends OverlayLayout {
 	public BeltSoundLayout(Belt belt) {
 		super(belt);
 
+		addChild(new LockSymbol(this));
 		addChild(new InitSoundArea(this));
 		addChild(randomize = new RandomizeSoundArea(this));
 		addChild(transitionTime = new TransitionTimeArea(this));
@@ -29,7 +35,13 @@ public class BeltSoundLayout extends OverlayLayout {
 		double height = h;
 		double yPos = (h - height) / 2;
 
+
+
 		for (OverlayControl c : getChildren()) {
+			if(c instanceof LockSymbol) {
+				c.doLayout(5, 5, 30, 40);
+				continue;
+			}
 			c.doLayout(xPos, yPos, widthPerElement, height);
 			xPos += widthPerElement + margin;
 		}
@@ -38,5 +50,20 @@ public class BeltSoundLayout extends OverlayLayout {
 	public void update(Node settings) {
 		transitionTime.update(settings);
 		randomize.update(settings);
+	}
+
+	private class LockSymbol extends Label {
+
+		public LockSymbol(OverlayLayout parent) {
+			super(parent);
+			setFontHeightInMM(7);
+		}
+
+		@Override
+		public String getDrawText(Context2d ctx) {
+			if(NonMaps.get().getNonLinearWorld().getParameterEditor().isAnyParameterLocked())
+				return "\ue20a";
+			return "";
+		}
 	}
 }
