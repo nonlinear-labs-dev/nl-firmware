9719dca058436573fa3b71059ac21221aa304a3d
schlenkibus <justus.scheil@parteimail.de>
2018-08-16 16:50:32 +0200

    added insert dropping tape drawing and changed general tape color scheme

--- NonMaps/src/com/nonlinearlabs/NonMaps/client/world/maps/presets/bank/Tape.java
+++ NonMaps/src/com/nonlinearlabs/NonMaps/client/world/maps/presets/bank/Tape.java
@@ -8,6 +8,7 @@ import com.nonlinearlabs.NonMaps.client.world.Position;
 import com.nonlinearlabs.NonMaps.client.world.RGB;
 import com.nonlinearlabs.NonMaps.client.world.Rect;
 import com.nonlinearlabs.NonMaps.client.world.maps.MapsControl;
+import com.nonlinearlabs.NonMaps.client.world.maps.NonDimension;
 import com.nonlinearlabs.NonMaps.client.world.maps.NonPosition;
 import com.nonlinearlabs.NonMaps.client.world.maps.presets.PresetManager;
 import com.nonlinearlabs.NonMaps.client.world.overlay.DragProxy;
@@ -59,6 +60,49 @@ public class Tape extends MapsControl {
 		return false;
 	}
 
+	private boolean isInsertTape() {
+		return getParent().hasSlaveInDirection(getOrientation());
+	}
+
+	private boolean isCurrentlyDraggedOverMe() {
+		if(!isInsertTape())
+			return false;
+
+		Overlay o = getNonMaps().getNonLinearWorld().getViewport().getOverlay();
+		for (DragProxy d : o.getDragProxies()) {
+			Control r = d.getCurrentReceiver();
+			if (r == this)
+				if(getPixRect().contains(d.getMousePosition()) && prospectBank != null)
+					return true;
+		}
+		return false;
+	}
+
+	public void layout(NonDimension oldDim) {
+
+		double tapeSize = getParent().getAttachArea();
+
+		switch(getOrientation()) {
+
+			case North:
+				setNonSize(new NonDimension(oldDim.getWidth(), tapeSize));
+				moveTo(new NonPosition(tapeSize, 0));
+				break;
+			case South:
+				setNonSize(new NonDimension(oldDim.getWidth(), tapeSize));
+				moveTo(new NonPosition(tapeSize, oldDim.getHeight() + tapeSize));
+				break;
+			case East:
+				setNonSize(new NonDimension(tapeSize, oldDim.getHeight()));
+				moveTo(new NonPosition(oldDim.getWidth() + tapeSize, 0 + tapeSize));
+				break;
+			case West:
+				setNonSize(new NonDimension(tapeSize, oldDim.getHeight()));
+				moveTo(new NonPosition(0, tapeSize));
+				break;
+		}
+	}
+
 	private boolean isActiveInsertTape() {
 		Overlay o = getNonMaps().getNonLinearWorld().getViewport().getOverlay();
 
@@ -86,19 +130,103 @@ public class Tape extends MapsControl {
 		this.orientation = orientation;
 	}
 
+	public Bank getBankInOrientation(Orientation o) {
+		switch(o) {
+			case North:
+				return getParent().getMasterTop();
+			case South:
+				return getParent().getBottomSlave();
+			case East:
+				return getParent().getRightSlave();
+			case West:
+				return getParent().getMasterLeft();
+		}
+		return null;
+	}
+
+	private double getMidPoint(double v, double v2) {
+		return (v + v2) / 2.0;
+	}
+
 	@Override
 	public void draw(Context2d ctx, int invalidationMask) {
 		super.draw(ctx, invalidationMask);
-		getPixRect().fill(ctx, getTapeColor());
+		Rect r = getPixRect().copy();
+		if(isInsertTape()) {
+			ctx.beginPath();
+			Position begin = new Position();
+			Position end = new Position();
+			Rect bankRect = getPixRect().copy();
+			Bank b = getBankInOrientation(getOrientation());
+			Rect bRect = b.getPixRect().copy();
+			switch(getOrientation()) {
+				case North:
+					begin.setX(getMidPoint(bankRect.getLeft(), bRect.getLeft()));
+					begin.setY(getMidPoint(bankRect.getTop(), bRect.getBottom()));
+					end.setX(getMidPoint(bankRect.getRight(), bRect.getRight()));
+					end.setY(getMidPoint(bankRect.getTop(), bRect.getBottom()));
+					break;
+				case South:
+					begin.setX(getMidPoint(bankRect.getLeft(), bRect.getLeft()));
+					begin.setY(getMidPoint(bankRect.getBottom(), bRect.getTop()));
+					end.setX(getMidPoint(bankRect.getRight(), bankRect.getRight()));
+					end.setY(getMidPoint(bankRect.getBottom(), bRect.getTop()));
+					break;
+				case East:
+					begin.setX(getMidPoint(bankRect.getRight(), bRect.getLeft()));
+					begin.setY(getMidPoint(bankRect.getTop(), bRect.getTop()));
+					end.setX(getMidPoint(bankRect.getRight(), bRect.getLeft()));
+					end.setY(Math.max(bankRect.getBottom(), bRect.getBottom()));
+					break;
+				case West:
+					begin.setX(getMidPoint(bankRect.getLeft(), bRect.getRight()));
+					begin.setY(getMidPoint(bankRect.getTop(), bRect.getTop()));
+					end.setX(getMidPoint(bankRect.getLeft(), bRect.getRight()));
+					end.setY(Math.max(bankRect.getBottom(), bRect.getBottom()));
+					break;
+			}
+			if(isCurrentlyDraggedOverMe()) {
+				if(getOrientation() == Orientation.North || getOrientation() == Orientation.South)
+					ctx.setLineWidth(getPixRect().getHeight());
+				else
+					ctx.setLineWidth(getPixRect().getWidth());
+			} else {
+				if(getOrientation() == Orientation.North || getOrientation() == Orientation.South)
+					ctx.setLineWidth(getPixRect().getHeight() / 5);
+				else
+					ctx.setLineWidth(getPixRect().getWidth() / 5);
+			}
+			ctx.moveTo(begin.getX(), begin.getY());
+
+			ctx.setStrokeStyle(getInsertColor().toString());
+
+			ctx.lineTo(end.getX(), end.getY());
+			ctx.stroke();
+			return;
+		} else if(isInsertTape()) {
+			if(getOrientation() == Orientation.East) {
+				r.setLeft(r.getRight());
+				r.setWidth(r.getWidth() / 2);
+			} else if(getOrientation() == Orientation.South) {
+				r.setTop(r.getBottom());
+				r.setHeight(r.getHeight() / 2);
+			} else {
+				return;
+			}
+		} else {
+			getPixRect().fill(ctx, getTapeColor());
+		}
+		r.fill(ctx, getTapeColor());
+	}
+
+	private RGB getInsertColor() {
+		return new RGB(250, 250, 250);
 	}
 
 	private RGB getTapeColor() {
-		RGB activeColor = new RGB(173, 181, 217);
+		RGB activeColor = new RGB(172, 185, 198);
 
-		if (prospectBank != null)
-			activeColor = new RGB(250, 250, 250);
-
-		return getParent().getParent().isAttachingTape(this) ? activeColor : new RGB(98, 113, 183);
+		return getParent().getParent().isAttachingTape(this) ? activeColor : new RGB(51, 83, 171);
 	}
 
 	public boolean fitsTo(Tape others) {
@@ -129,7 +257,7 @@ public class Tape extends MapsControl {
 
 	@Override
 	public Control drag(Rect pos, DragProxy dragProxy) {
-		if (isActiveInsertTape())
+		if (isInsertTape())
 			return insertTapeDrag(pos, dragProxy);
 
 		if (isVisible() && !getParent().isDraggingControl()) {
@@ -185,18 +313,29 @@ public class Tape extends MapsControl {
 		return b == prospectBank;
 	}
 
+	private Orientation invert(Orientation o) {
+		switch (o) {
+			case North:
+				return Orientation.South;
+			case South:
+				return Orientation.North;
+			case East:
+				return Orientation.West;
+			case West:
+				return Orientation.East;
+		}
+		return null;
+	}
+
 	public Control insertTapeDrag(Rect pos, DragProxy dragProxy) {
 		if (isVisible() && !getParent().isDraggingControl()) {
 			if (dragProxy.getOrigin() instanceof Bank) {
 				Bank other = (Bank) dragProxy.getOrigin();
-				Position mouse = dragProxy.getMousePosition();
 
-				if (getPixRect().contains(mouse)) {
+				if(getPixRect().contains(dragProxy.getMousePosition())) {
 					if (getParent() != other) {
 						if (fitsTo(other)) {
-							// Display attaching!
 							setShouldInsert(other);
-							getParent().getParent().setAttachingTapes(this, null);
 							return this;
 						}
 					}
