f9388814e4e1ef40813af4ca3e6f9c0285737cc5
Henry Hoegelow <henry@hoegelow.com>
2018-08-23 14:31:54 +0200

    on click, wait for 250ms for a double click and do only the double click action in case

---
+++ NonMaps/src/com/nonlinearlabs/NonMaps/client/world/pointer/DoubleClickWaiter.java
@@ -0,0 +1,33 @@
+package com.nonlinearlabs.NonMaps.client.world.pointer;
+
+import java.util.function.Consumer;
+
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.RepeatingCommand;
+import com.nonlinearlabs.NonMaps.client.world.Position;
+
+public class DoubleClickWaiter {
+
+	private boolean canceled = false;
+
+	public DoubleClickWaiter(Position p, Consumer<Position> c) {
+
+		Scheduler.get().scheduleFixedDelay(new RepeatingCommand() {
+
+			@Override
+			public boolean execute() {
+				if (!canceled)
+					c.accept(p);
+				return false;
+			}
+		}, 250);
+	}
+
+	public void onDoubleClick() {
+		cancel();
+	}
+
+	public void cancel() {
+		canceled = true;
+	}
+}
