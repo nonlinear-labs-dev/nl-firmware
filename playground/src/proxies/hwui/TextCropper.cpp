7dc4f59bb01f0ca54f92af0361e81e7976143262
Henry Hoegelow <henry@hoegelow.com>
2018-08-30 13:38:56 +0200

    added testability

--- playground/src/proxies/hwui/TextCropper.cpp
+++ playground/src/proxies/hwui/TextCropper.cpp
@@ -5,17 +5,21 @@
 #include "TextCropper.h"
 #include "Oleds.h"
 
-
-static TestDriver<TextCropper> tests{};
+static TestDriver<TextCropper> tests;
 
 void TextCropper::registerTests()
 {
-  g_test_add_func ("/TextCropper/simple", []()
-                 {
-                   g_assert(
-                       TextCropper::shortenStringIfNeccessary(Oleds::get ().getFont ("Emphase_9_Regular", 8), "Fo", 5) ==
-                       "Fo");
-                 });
+  g_test_add_func("/TextCropper/shortenStringIfNeccessary", []() {
+    auto font = make_shared<Font>("./resources/Emphase_9_Regular.ttf", 9);
+
+    auto testShortenStringIfNeccessary = [font](std::string in, int len, std::string expected) {
+      auto shortened = TextCropper::shortenStringIfNeccessary(font, in, len);
+      g_assert_cmpstr(shortened.c_str(), ==, expected.c_str());
+    };
+
+    testShortenStringIfNeccessary("Fo", 5, "..");
+    testShortenStringIfNeccessary("Fo", 100, "Fo");
+  });
 };
 
 Glib::ustring TextCropper::shortenStringIfNeccessary(shared_ptr<Font> font, const Glib::ustring &text, double maxWidth)
@@ -23,62 +27,69 @@ Glib::ustring TextCropper::shortenStringIfNeccessary(shared_ptr<Font> font, cons
   return preserveEndShorten(font, text, maxWidth);
 }
 
-ustring TextCropper::preserveEndShorten(const shared_ptr<Font> &font, const ustring &text, double maxWidth) {
+ustring TextCropper::preserveEndShorten(const shared_ptr<Font> &font, const ustring &text, double maxWidth)
+{
   const auto textLen = text.length();
   const auto min = 0;
   auto edgeCaseString = edgeCases(min, textLen, text);
-  if(!edgeCaseString.empty()) {
+  if(!edgeCaseString.empty())
+  {
     return edgeCaseString;
   }
 
   auto spaceSplitString = spaceBasedCropping(text, font, maxWidth * 0.95);
-  if(!spaceSplitString.empty()) {
+  if(!spaceSplitString.empty())
+  {
     return spaceSplitString;
   }
 
-  return rigidSplit(font,text,maxWidth);
+  return rigidSplit(font, text, maxWidth);
 }
 
-Glib::ustring TextCropper::rigidSplit(const shared_ptr<Font> &font, const ustring &text, const double maxWidth) {
+Glib::ustring TextCropper::rigidSplit(const shared_ptr<Font> &font, const ustring &text, const double maxWidth)
+{
 
   ustring front;
   ustring back;
   const ustring padding = "..";
   int i = 0;
-  while(font->getStringWidth(front + padding) < maxWidth / 2) {
+  while(font->getStringWidth(front + padding) < maxWidth / 2)
+  {
     front.push_back(text.at(static_cast<ustring::size_type>(i++)));
   }
   i = static_cast<int>(text.length() - 1);
-  while(font->getStringWidth(back + padding) < maxWidth / 2) {
+  while(font->getStringWidth(back + padding) < maxWidth / 2)
+  {
     back.insert(back.begin(), text.at(static_cast<ustring::size_type>(i--)));
   }
   return front + ".." + back;
 }
 
-Glib::ustring TextCropper::edgeCases(const int min, const ustring::size_type len, const ustring &ustring) {
+Glib::ustring TextCropper::edgeCases(const int min, const ustring::size_type len, const ustring &ustring)
+{
   if(len == 0)
     return "";
   if(len < min)
     return ustring;
-  if ((min + 1) == len)
+  if((min + 1) == len)
     return ustring.substr(0, static_cast<ustring::size_type>(min + 1)) + "..";
 
   return "";
 }
 
-Glib::ustring
-TextCropper::spaceBasedCropping(const ustring &text, const shared_ptr<Font> &font, double maxWidth) {
+Glib::ustring TextCropper::spaceBasedCropping(const ustring &text, const shared_ptr<Font> &font, double maxWidth)
+{
   const auto &c = text;
   if(maxWidth == 0)
     return "";
   auto roughTwoThirds = (text.length() / 3) * 2;
-  if(auto pos = text.find_last_of(' ')) {
-    if(pos < text.length() && pos >= roughTwoThirds) {
+  if(auto pos = text.find_last_of(' '))
+  {
+    if(pos < text.length() && pos >= roughTwoThirds)
+    {
       auto partAfterSpace = c.substr(pos + 1, c.length() - 1);
       Glib::ustring front = "";
-      for(int i = 0;
-          (font->getStringWidth(to_string(front + partAfterSpace + "...")) < maxWidth);
-          i++)
+      for(int i = 0; (font->getStringWidth(to_string(front + partAfterSpace + "...")) < maxWidth); i++)
       {
         front += text[i];
       }
--- playground/src/proxies/hwui/TextCropper.h
+++ playground/src/proxies/hwui/TextCropper.h
@@ -5,14 +5,19 @@
 
 class Control;
 
-class TextCropper {
-public:
-  static Glib::ustring shortenStringIfNeccessary(std::shared_ptr<Font> font, const Glib::ustring &text, double maxWidth) ;
+class TextCropper
+{
+ public:
+  static Glib::ustring shortenStringIfNeccessary(std::shared_ptr<Font> font, const Glib::ustring &text,
+                                                 double maxWidth);
   static void registerTests();
-protected:
-  static Glib::ustring preserveEndShorten(const std::shared_ptr<Font>& font, const Glib::ustring &text, double maxWidth) ;
-  static Glib::ustring rigidSplit(const std::shared_ptr<Font> &font, const Glib::ustring &text, double maxWidth) ;
-  static Glib::ustring edgeCases(int min, Glib::ustring::size_type len, const Glib::ustring &ustring) ;
 
-  static Glib::ustring spaceBasedCropping(const Glib::ustring &text, const std::shared_ptr<Font>& font, double maxWidth) ;
+ protected:
+  static Glib::ustring preserveEndShorten(const std::shared_ptr<Font> &font, const Glib::ustring &text,
+                                          double maxWidth);
+  static Glib::ustring rigidSplit(const std::shared_ptr<Font> &font, const Glib::ustring &text, double maxWidth);
+  static Glib::ustring edgeCases(int min, Glib::ustring::size_type len, const Glib::ustring &ustring);
+
+  static Glib::ustring spaceBasedCropping(const Glib::ustring &text, const std::shared_ptr<Font> &font,
+                                          double maxWidth);
 };
