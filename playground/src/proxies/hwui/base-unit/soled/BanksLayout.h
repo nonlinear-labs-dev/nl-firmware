b8b5e3292ede43af7739098997d3dc08d5489b43
schlenkibus <justus.scheil@parteimail.de>
2018-08-30 10:02:34 +0200

    added bank changed connection + refactor

--- playground/src/proxies/hwui/base-unit/soled/BanksLayout.h
+++ playground/src/proxies/hwui/base-unit/soled/BanksLayout.h
@@ -19,9 +19,14 @@ class BanksLayout : public DFBLayout
   private:
     void onBankSelected(shared_ptr<PresetBank> bank);
     void onAutoLoadSettingChanged(const Setting *s);
+    void onBankChanged();
 
     Label *m_number;
     Label *m_name;
+    sigc::connection m_bankconnection;
     DirectLoadIndicator *m_directLoad;
+    shared_ptr<PresetBank> m_bank;
+
+  void updateFromBank(const shared_ptr<PresetBank> &bank) const;
 };
 
