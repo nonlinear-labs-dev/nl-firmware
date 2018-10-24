be5a363dfeb70e42bea2d1111e594b6054c3dc4d
Henry Hoegelow <henry@hoegelow.com>
2018-10-24 09:17:27 +0200

    removed unnecessary code

--- NonMaps/src/com/nonlinearlabs/NonMaps/client/tools/NLMath.java
+++ NonMaps/src/com/nonlinearlabs/NonMaps/client/tools/NLMath.java
@@ -1,8 +1,8 @@
 package com.nonlinearlabs.NonMaps.client.tools;
 
 public class NLMath {
-	public static double clamp(double modRight, double d, double e) {
-		return Math.max(d, Math.min(e, modRight));
+	public static double clamp(double v, double min, double max) {
+		return Math.max(min, Math.min(max, v));
 	}
 
 	public static double quantize(double v, double numSteps) {
--- NonMaps/src/com/nonlinearlabs/NonMaps/client/world/overlay/belt/parameters/UpperLowerBoundListener.java
+++ NonMaps/src/com/nonlinearlabs/NonMaps/client/world/overlay/belt/parameters/UpperLowerBoundListener.java
@@ -1,15 +1,12 @@
 package com.nonlinearlabs.NonMaps.client.world.overlay.belt.parameters;
 
 import com.nonlinearlabs.NonMaps.client.NonMaps;
-import com.nonlinearlabs.NonMaps.client.Tracer;
-import com.nonlinearlabs.NonMaps.client.tools.NLMath;
 import com.nonlinearlabs.NonMaps.client.world.maps.parameters.ModulatableParameter;
 import com.nonlinearlabs.NonMaps.client.world.maps.parameters.Parameter;
 import com.nonlinearlabs.NonMaps.client.world.maps.parameters.Parameter.Initiator;
 import com.nonlinearlabs.NonMaps.client.world.maps.parameters.PlayControls.MacroControls.MacroControlParameter;
 import com.nonlinearlabs.NonMaps.client.world.maps.parameters.PlayControls.MacroControls.Macros.MacroControls;
 import com.nonlinearlabs.NonMaps.client.world.maps.parameters.value.QuantizedClippedValue;
-import com.nonlinearlabs.NonMaps.client.world.overlay.belt.parameters.BeltParameterLayout.Mode;
 
 class UpperLowerBoundListener implements QuantizedClippedValue.ChangeListener {
 	private final boolean upper;
@@ -20,23 +17,14 @@ class UpperLowerBoundListener implements QuantizedClippedValue.ChangeListener {
 
 	@Override
 	public void onQuantizedValueChanged(Initiator initiator, double oldQuantizedValue, double newQuantizedValue) {
-				
-		BeltParameterLayout bpm = NonMaps.theMaps.getNonLinearWorld().getViewport().getOverlay().getBelt().getParameterLayout();
-		
-		if(bpm != null) {
-			if(upper && bpm.getMode() == Mode.mcLower)
-				return;
-			else if (!upper && bpm.getMode() == Mode.mcUpper)
-				return;
-		}
-		
 		Parameter p = NonMaps.theMaps.getNonLinearWorld().getParameterEditor().getSelectedOrSome();
 
 		if (p instanceof ModulatableParameter) {
 			ModulatableParameter modulatedParam = (ModulatableParameter) p;
 			MacroControls s = modulatedParam.getModulationSource();
 			if (s != MacroControls.NONE) {
-				MacroControlParameter mc = NonMaps.theMaps.getNonLinearWorld().getParameterEditor().getMacroControls().getControl(s);
+				MacroControlParameter mc = NonMaps.theMaps.getNonLinearWorld().getParameterEditor().getMacroControls()
+						.getControl(s);
 
 				double modAmount = modulatedParam.getModulationAmount().getClippedValue();
 
@@ -63,15 +51,15 @@ class UpperLowerBoundListener implements QuantizedClippedValue.ChangeListener {
 				newValue = modulatedParam.getValue().clip(newValue);
 				newValue = modulatedParam.getValue().getQuantizedValue(newValue, true);
 
-				
 				if (modulatedParam.isBiPolar())
 					newModAmount /= 2;
-								
+
 				modulatedParam.getModulationAmount().setRawValue(Initiator.INDIRECT_USER_ACTION, newModAmount);
 				modulatedParam.getValue().setRawValue(Initiator.INDIRECT_USER_ACTION, newValue);
 
 				if (initiator == Initiator.EXPLICIT_USER_ACTION)
-					NonMaps.theMaps.getServerProxy().setModulationAmountAndValue(modulatedParam, newModAmount, newValue);
+					NonMaps.theMaps.getServerProxy().setModulationAmountAndValue(modulatedParam, newModAmount,
+							newValue);
 			}
 		}
 	}
