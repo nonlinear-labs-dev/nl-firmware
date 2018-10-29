ea16b022e8a8495895487abd466fbc84c7d24642
schlenkibus <justus.scheil@parteimail.de>
2018-10-29 16:50:45 +0100

    fixed prev next preset buttons, by looking at the selected preset rather than the loaded one

--- NonMaps/src/com/nonlinearlabs/NonMaps/client/world/maps/presets/PresetManager.java
+++ NonMaps/src/com/nonlinearlabs/NonMaps/client/world/maps/presets/PresetManager.java
@@ -481,7 +481,7 @@ public class PresetManager extends MapsLayout {
 		if (isInStoreSelectMode())
 			return m_storeSelectMode.canNext();
 
-		Preset p = findLoadedPreset();
+		Preset p = findSelectedPreset();
 		if (p != null) {
 			Bank b = p.getParent();
 			if (b != null)
@@ -494,7 +494,7 @@ public class PresetManager extends MapsLayout {
 		if (isInStoreSelectMode())
 			return m_storeSelectMode.canPrev();
 
-		Preset p = findLoadedPreset();
+		Preset p = findSelectedPreset();
 		if (p != null) {
 			Bank b = p.getParent();
 			if (b != null)
