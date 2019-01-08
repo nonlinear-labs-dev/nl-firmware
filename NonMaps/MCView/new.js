0e85594af9db9e915a999b75aa36689f67df276d
schlenkibus <justus.scheil@parteimail.de>
2019-01-08 19:00:14 +0100

    working as excpeted now, only communication to playground is needed -> but I have to think about how i will implement smoothing and sending values -> cpu?

--- NonMaps/MCView/new.js
+++ NonMaps/MCView/new.js
@@ -30,6 +30,7 @@ class MC {
 
   setValue(val) {
     this.paramValue = val;
+    this.targetValue = undefined;
     this.onValueChanged.onChange(val);
   }
 
@@ -60,12 +61,36 @@ class MCModel {
       }
     }
   }
+
+  setTarget(id, target) {
+    model.mcs[id - 243].setTarget(target);
+  }
+
+  update() {
+    var changed = false;
+    var i;
+    for(i = 0; i < model.mcs.length; i++) {
+      var mc = model.mcs[i];
+      if(mc.targetValue !== undefined && mc.targetValue != mc.paramValue) {
+        var n = 1.0 / (1 * 0.5 + 1.0);
+        var oldParam = mc.paramValue;
+      	mc.paramValue = (1 - n) * mc.targetValue + n * mc.paramValue;
+
+        if(oldParam != mc.paramValue)
+          changed = true;
+      }
+    }
+    if(changed) {
+      view.redraw(model);
+    }
+    setInterval(this.update, 250);
+  }
 }
 
 class RangeDivision {
   constructor() {
     this.controls = [{"ID":0,"x":0,   "y":0,    "w":0.5,  "h":0.5,  "type":"xy",  "MCX":243,  "MCY":244},
-                     {"ID":1,"x":0.5, "y":0,    "w":0.5,  "h":0.5,  "type":"none",  "MCX":null, "MCY":null},
+                     {"ID":1,"x":0.5, "y":0,    "w":0.5,  "h":0.5,  "type":"none","MCX":null, "MCY":null},
                      {"ID":2,"x":0,   "y":0.5,  "w":1,    "h":0.25, "type":"x",   "MCX":245,  "MCY":null},
                      {"ID":3,"x":0,   "y":0.75, "w":1,    "h":0.25, "type":"x",   "MCX":246,  "MCY":null}];
   }
@@ -82,37 +107,43 @@ class MCView {
       });
     });
 
-    this.canvas.addEventListener('touchstart', function(event) {
-  		event.preventDefault();
-  		controller.touches = event.touches;
-      controller.onChange();
-  	});
+    this.body = document.getElementById('body');
 
-  	this.canvas.addEventListener('mousemove', function(event) {
-  		controller.lastMouseEvent = event;
-      controller.onChange();
-  	});
+    var importantElements = [this.body, this.canvas];
+    for(var i = 0; i < importantElements.length; i++){
+      var element = importantElements[i];
+      element.addEventListener('touchstart', function(event) {
+    		event.preventDefault();
+    		controller.touches = event.touches;
+        controller.onChange();
+    	});
 
-  	this.canvas.addEventListener('mousedown', function() {
-  	   controller.mouseDown = 1;
-       controller.onChange();
-  	});
+    	element.addEventListener('mousemove', function(event) {
+    		controller.lastMouseEvent = event;
+        controller.onChange();
+    	});
 
-  	this.canvas.addEventListener('mouseup', function() {
-  	   controller.mouseDown = 0;
-       controller.onChange();
-  	});
+    	element.addEventListener('mousedown', function() {
+    	   controller.mouseDown = 1;
+         controller.onChange();
+    	});
 
-  	this.canvas.addEventListener('touchmove', function(event) {
-  		event.preventDefault();
-  		controller.touches = event.touches;
-      controller.onChange();
-  	});
+    	element.addEventListener('mouseup', function() {
+    	   controller.mouseDown = 0;
+         controller.onChange();
+    	});
 
-  	this.canvas.addEventListener('touchend', function(event) {
-  		controller.touches = event.touches;
-      controller.onChange();
-  	});
+    	element.addEventListener('touchmove', function(event) {
+    		event.preventDefault();
+    		controller.touches = event.touches;
+        controller.onChange();
+    	});
+
+    	element.addEventListener('touchend', function(event) {
+    		controller.touches = event.touches;
+        controller.onChange();
+    	});
+    }
   }
 
   redraw(model) {
@@ -123,6 +154,7 @@ class MCView {
     var heigth = canvas.height;
 
     ctx.font = '4em nonlinearfont';
+    ctx.strokeStyle = "black";
     ctx.fillStyle = "#2b2b2b";
     ctx.fillRect(0, 0, width, heigth);
 
@@ -133,13 +165,11 @@ class MCView {
       var y = heigth * division.y;
       var w = width * division.w;
       var h = heigth * division.h;
+
       ctx.beginPath();
-      ctx.strokeStyle = "#00000";
-      ctx.moveTo(x, y);
-      ctx.lineTo(x+w, y);
-      ctx.lineTo(x+w, h+y);
-      ctx.lineTo(x, h+y);
-      ctx.lineTo(x, y);
+      ctx.lineWidth = "1";
+      ctx.strokeStyle = "black";
+      ctx.rect(x, y, w, h);
       ctx.stroke();
 
       this.drawHandle(division);
@@ -147,6 +177,16 @@ class MCView {
   }
 
   drawHandle(division) {
+    if(division.type == null)
+      return;
+    if(division.type.startsWith("xy")) {
+      this.draw2DDivision(division);
+    } else if(division.type.startsWith("x")) {
+      this.draw1DDivision(division);
+    }
+  }
+
+  draw2DDivision(division) {
     var canvas = view.canvas;
     var ctx = canvas.getContext('2d');
     var width = canvas.width;
@@ -156,46 +196,60 @@ class MCView {
     var w = width * division.w;
     var h = heigth * division.h;
 
-    ctx.save();
+    var xTarget = model.mcs[division.MCX - 243].targetValue;
+    var yTarget = model.mcs[division.MCY - 243].targetValue;
+    var xVal = model.mcs[division.MCX - 243].paramValue;
+    var yVal = model.mcs[division.MCY - 243].paramValue;
+
     ctx.beginPath();
-
-    if(division.type.startsWith("xy")) {
-      var xTarget = model.mcs[division.MCX - 243].targetValue;
-      var yTarget = model.mcs[division.MCY - 243].targetValue;
-
-      var xVal = model.mcs[division.MCX - 243].paramValue;
-      var yVal = model.mcs[division.MCY - 243].paramValue;
-    	ctx.fillStyle = "blue";
-    	ctx.arc(w / 100 * xVal, h / 100 * yVal, canvas.width / 200 * 2, 0, 2*Math.PI, true);
-      ctx.fill();
-
-      if(xTarget !== undefined && yTarget !== undefined) {
-        ctx.strokeStyle = "gray";
-        ctx.fillStyle = "transparent";
-        ctx.lineWidth = w / 200 * 2;
-        ctx.arc(w / 100 * xTarget, h / 100 * yTarget, canvas.width / 200 * 3, 0, 2*Math.PI, true);
-      }
-
-    } else if(division.type.startsWith("x")) {
-      var xVal = model.mcs[division.MCX - 243].paramValue;
-    	ctx.strokeStyle = "blue";
-      ctx.lineWidth = w / 200 * 1;
-    	ctx.moveTo(w / 100 * xVal, y + 1);
-      ctx.lineTo(w / 100 * xVal, y + h - 1);
-
-      ctx.strokeStyle = "black";
-      ctx.fillStyle = "gray";
-      var mcText = String(getUnicodeForMC(mcId);
-      var valueText = String(xVal);
-      var maxWidth = Math.max(, );
-      ctx.fillText(mcText, w / 2 - ctx.measureText(mcText) / 2, h / 2);
-      ctx.fillText(valueText, w / 2 - ctx.measureText(valueText) / 2, h / 1.5);
-    }
-
-    ctx.fill();
+    ctx.strokeStyle = "transparent";
+    ctx.fillStyle = "blue";
+    var size = canvas.width / 200 * 2;
+    ctx.arc(w / 100 * xVal, h / 100 * yVal, size, 0, 2*Math.PI, true);
     ctx.stroke();
-    ctx.restore();
+    ctx.fill();
 
+    if(xTarget !== undefined && yTarget !== undefined) {
+      ctx.beginPath();
+      ctx.strokeStyle = "gray";
+      ctx.fillStyle = "transparent";
+      ctx.arc(w / 100 * xTarget, h / 100 * yTarget, size, 0, 2*Math.PI, true);
+      ctx.fill();
+      ctx.stroke();
+    }
+  }
+
+  draw1DDivision(division) {
+    var canvas = view.canvas;
+    var ctx = canvas.getContext('2d');
+    var width = canvas.width;
+    var heigth = canvas.height;
+    var x = width * division.x;
+    var y = heigth * division.y;
+    var w = width * division.w;
+    var h = heigth * division.h;
+
+    var xVal = model.mcs[division.MCX - 243].paramValue;
+    var xTarget = model.mcs[division.MCX - 243].targetValue;
+
+    ctx.beginPath();
+    ctx.fillStyle = "transparent";
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = w / 200 * 1;
+    ctx.moveTo(w / 100 * xVal, y + 1);
+    ctx.lineTo(w / 100 * xVal, y + h - 1);
+    ctx.stroke();
+    ctx.fill();
+
+    if(xTarget !== undefined) {
+      ctx.beginPath();
+      ctx.fillStyle = "transparent";
+      ctx.strokeStyle = "grey";
+      ctx.moveTo(w / 100 * xTarget, y + 1);
+      ctx.lineTo(w / 100 * xTarget, y + h - 1);
+      ctx.stroke();
+      ctx.fill();
+    }
   }
 
   getViewInfo() {
@@ -237,7 +291,7 @@ class MCController {
 
   onChange() {
     this.userInputs = this.collectInputs();
-    //model.setTargets(this.inputsToTargets());
+    this.inputsToTargets();
     view.redraw(model);
   }
 
@@ -245,22 +299,73 @@ class MCController {
     var targets = [];
     var displayInformation = view.getViewInfo();
 
-    for(var input in this.userInputs) {
+    var i;
+    for(i in displayInformation.controls) {
+      var division = displayInformation.controls[i];
+      var activeInputs = [];
+      var cW = view.canvas.width;
+      var cH = view.canvas.height;
+      var rect = view.canvas.getBoundingClientRect();
+      var dX = (cW * division.x) + rect.left;
+      var dY = (cH * division.y) + rect.top;
+      var dW = cW * division.w;
+      var dH = cH * division.h;
 
+      for(var iI in this.userInputs) {
+        var input = this.userInputs[iI];
+
+        if(input.x >= dX && input.x <= dX + dW &&
+           input.y >= dY && input.y <= dY + dH) {
+          activeInputs.push(input);
+        }
+      }
+
+      var inputCount = activeInputs.length;
+      if(inputCount <= 0 || division.type == null) {
+        continue;
+      }
+
+      var rect = view.canvas.getBoundingClientRect();
+      var x = -rect.left;
+      var y = -rect.top;
+      activeInputs.forEach(function(input) {
+        x+=input.x;
+        y+=input.y;
+      });
+      if(inputCount > 0) {
+        x /= inputCount;
+        y /= inputCount;
+      }
+
+      var xVal = Number(x / dW * 100).toFixed(1);
+      var yVal = Number(y / dH * 100).toFixed(1);
+
+      //To Be Safe!
+      xVal = Math.min(100, Math.max(xVal, 0));
+      yVal = Math.min(100, Math.max(yVal, 0));
+
+
+      if(division.type.startsWith("xy")) {
+        model.setTarget(division.MCX, xVal);
+        model.setTarget(division.MCY, yVal);
+      } else if(division.type.startsWith("x")) {
+        model.setTarget(division.MCX, xVal);
+      }
     }
-    return targets;
   }
 
   collectInputs() {
     var activePositions = [];
 
-    for(var touch in this.touches) {
+
+    for(var i = 0; i < this.touches.length; i++) {
+      var touch = this.touches[i];
       activePositions.push(new Input(touch.pageX, touch.pageY));
     }
 
     if(this.lastMouseEvent !== undefined) {
       if(this.mouseDown !== 0) {
-        activePositions.push(new Input(this.lastMouseEvent.clientX, this.lastMouseEvent.clientY));
+        activePositions.push(new Input(this.lastMouseEvent.pageX, this.lastMouseEvent.pageY));
       }
     }
 
@@ -281,5 +386,6 @@ function onLoad() {
     model = new MCModel(webSocket);
     view = new MCView();
     controller = new MCController();
+    model.update();
   };
 }
