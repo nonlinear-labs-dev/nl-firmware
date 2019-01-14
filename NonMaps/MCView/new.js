8f2ff1ba30bf216699b1266ac1ecc7a9c5782f96
schlenkibus <justus.scheil@parteimail.de>
2019-01-14 18:41:43 +0100

    ip...

--- NonMaps/MCView/new.js
+++ NonMaps/MCView/new.js
@@ -33,7 +33,7 @@ class Slot {
 
 class ServerProxy {
   constructor(onStartCB) {
-    this.webSocket = new WebSocket('ws://192.168.0.2:8080/ws/');
+    this.webSocket = new WebSocket('ws://192.168.8.2:80/ws/');
     this.uuid = new UUID();
     this.webSocket.onopen =  onStartCB;
     this.webSocket.onmessage = this.onMessage;
