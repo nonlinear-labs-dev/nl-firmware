b86e14fc9c025cc9440f9bea9cfec2f43359270d
schlenkibus <justus.scheil@parteimail.de>
2019-02-13 11:51:37 +0100

    initialized value

--- playground/src/parameters/MacroControlParameter.h
+++ playground/src/parameters/MacroControlParameter.h
@@ -75,7 +75,7 @@ class MacroControlParameter : public Parameter
   Glib::ustring m_lastMCViewUuid;
 
   void propagateMCChangeToMCViews(const Initiator &initiatior);
-  tControlPositionValue lastBroadcastedControlPosition;
+  tControlPositionValue lastBroadcastedControlPosition = std::numeric_limits<tControlPositionValue>::max();
   Throttler mcviewThrottler;
 
   sigc::signal<void> m_targetListChanged;
