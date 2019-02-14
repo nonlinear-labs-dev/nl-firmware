2e8cede0d47cb009c10df36886bb94cb4c924cc8
Henry Hoegelow <henry@hoegelow.com>
2019-02-14 16:30:21 +0100

    even more debug info

--- playground/src/presets/Preset.cpp
+++ playground/src/presets/Preset.cpp
@@ -5,6 +5,7 @@
 #include "PresetParameterGroup.h"
 #include <serialization/PresetSerializer.h>
 #include <Application.h>
+#include <device-settings/DebugLevel.h>
 #include <device-settings/DeviceName.h>
 #include <device-settings/Settings.h>
 #include <device-info/DeviceInformation.h>
@@ -35,7 +36,10 @@ Preset::Preset(UpdateDocumentContributor *parent, const EditBuffer &editBuffer)
   m_name = editBuffer.getName();
 }
 
-Preset::~Preset() = default;
+Preset::~Preset()
+{
+  DebugLevel::warning(__PRETTY_FUNCTION__, __LINE__);
+}
 
 void Preset::load(UNDO::Transaction *transaction, RefPtr<Gio::File> presetPath)
 {
