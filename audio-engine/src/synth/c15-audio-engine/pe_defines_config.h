6b0a8abc357e038530176349205637a87542c881
Matthias Seeber <matthias.seeber@nonlinear-labs.de>
2019-03-20 15:18:11 +0100

    test commit, remote mode 1.56

--- audio-engine/src/synth/c15-audio-engine/pe_defines_config.h
+++ audio-engine/src/synth/c15-audio-engine/pe_defines_config.h
@@ -48,7 +48,7 @@
 #define test_fast_fold_asym 1  // 0: slow clock (producing audible artifacts), 1: fast clock (recommended)
 #define test_preload_update 1  // 0: non-optimized preload update, 1: optimized preload update (recommended)
 #define test_flushModeFlag 1   // 0: flushes ONLY Buffers, 1: flushes Buffers AND Filter State Variables
-#define test_inputModeFlag 0   // 0: receive TCD MIDI, 1: receive Remote MIDI (and produce TCD internally)
+#define test_inputModeFlag 1   // 0: receive TCD MIDI, 1: receive Remote MIDI (and produce TCD internally)
 #define test_whichEnvelope 1   // specify which env engine should be used: old (0) or new (1)
 
 #define test_reverbParams 1    // 0: fast rendering (like Reaktor), 1: slow rendering (experimental)
