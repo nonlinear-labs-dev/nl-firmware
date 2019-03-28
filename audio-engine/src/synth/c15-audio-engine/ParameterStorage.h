3861deca7c8344b2bd2e657ef5a278d521688eda
Henry Hoegelow <henry@hoegelow.com>
2019-03-28 12:56:10 +0100

    to avoid any complains regarding performance issues, change the ParameterStorage interface that mimis the old access pattern the best.

--- audio-engine/src/synth/c15-audio-engine/ParameterStorage.h
+++ audio-engine/src/synth/c15-audio-engine/ParameterStorage.h
@@ -10,12 +10,13 @@ class ParameterStorage
 
   inline float &operator[](int param)
   {
-    return m_paramsignaldata[m_boundVoice][param];
+    return m_boundVoice[param];
   }
 
   ParameterStorage &bindToVoice(int v)
   {
-    m_boundVoice = v;
+    m_boundVoice = m_paramsignaldata[v];
+    return *this;
   }
 
   float get(int voice, int param)
@@ -24,6 +25,6 @@ class ParameterStorage
   }
 
  private:
-  int m_boundVoice = 0;
   float m_paramsignaldata[dsp_number_of_voices][sig_number_of_signal_items] = {};
+  float *m_boundVoice = m_paramsignaldata[0];
 };
