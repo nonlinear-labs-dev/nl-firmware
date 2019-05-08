dae1e908eb5c7c291189da3a2ebf1cb1b432424b
schlenkibus <justus.scheil@parteimail.de>
2019-05-08 11:38:46 +0200

    reduce calls to EditBufferSnapshotMaker to only EditBuffer::copyFrom

--- playground/src/http/UndoScope.cpp
+++ playground/src/http/UndoScope.cpp
@@ -1,10 +1,8 @@
 #include "UndoScope.h"
 #include "xml/Writer.h"
-#include <presets/EditBufferSnapshotMaker.h>
 
 void UndoScope::onAddTransaction(UNDO::Transaction* transaction)
 {
-  EditBufferSnapshotMaker::get().addSnapshotIfRequired(transaction);
   UNDO::Scope::onAddTransaction(transaction);
 }
 
