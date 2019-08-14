0ab4c3cc7a75f24b43d26616aa43de40feb9510b
Henry Hoegelow <henry@hoegelow.com>
2019-08-14 09:42:26 +0200

    fix deprectaed and cmake warnings

--- audio-engine/src/Types.h
+++ audio-engine/src/Types.h
@@ -2,7 +2,7 @@
 
 #include <stdint.h>
 #include <stdlib.h>
-#include <asoundlib.h>
+#include <alsa/asoundlib.h>
 #include <iostream>
 #include <chrono>
 
--- audio-engine/src/io/audio/AudioWriter.h
+++ audio-engine/src/io/audio/AudioWriter.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <Types.h>
-#include <asoundlib.h>
 #include <array>
 #include <memory>
 
