454ba58f72a53c3370c5feb94a1068de27146c72
schlenkibus <justus.scheil@parteimail.de>
2019-11-05 15:41:11 +0100

    cleanup

--- playground/src/proxies/hwui/panel-unit/boled/sound-screens/SoundInfoLayout.cpp
+++ playground/src/proxies/hwui/panel-unit/boled/sound-screens/SoundInfoLayout.cpp
@@ -1,21 +1,48 @@
-#include <proxies/hwui/panel-unit/boled/preset-screens/controls/SoundInfoContent.h>
 #include <proxies/hwui/panel-unit/boled/parameter-screens/controls/HeadlineLabel.h>
 #include <proxies/hwui/panel-unit/boled/parameter-screens/controls/InfoLabel.h>
 #include <proxies/hwui/panel-unit/boled/parameter-screens/controls/ModuleCaption.h>
+#include <proxies/hwui/panel-unit/boled/sound-screens/controls/SoundInfoContent.h>
+#include <proxies/hwui/controls/LabelRegular8.h>
+#include <proxies/hwui/panel-unit/boled/preset-screens/controls/InvertedLabel.h>
 #include "SoundInfoLayout.h"
 
-SoundInfoLayout::SoundInfoLayout() : InfoLayout()
+SoundInfoLayout::SoundInfoLayout()
+    : InfoLayout()
 {
 }
 
+struct SoundInfoModuleCaption : public InvertedLabel
+{
+ public:
+  explicit SoundInfoModuleCaption(const Rect& r)
+      : InvertedLabel("Sound", r)
+  {
+  }
+};
+
+struct HeadlinePlaceholder : public Control
+{
+  HeadlinePlaceholder(const Rect& r)
+      : Control(r)
+  {
+  }
+
+  bool redraw(FrameBuffer& fb) override
+  {
+    fb.setColor(FrameBuffer::Colors::C77);
+    fb.fillRect(getPosition());
+    return true;
+  }
+};
+
 void SoundInfoLayout::addModuleCaption()
 {
-  addControl(new ModuleCaption(Rect(0, 0, 64, 13)));
+  addControl(new SoundInfoModuleCaption(Rect(0, 0, 64, 13)));
 }
 
 void SoundInfoLayout::addHeadline()
 {
-  addControl(new HeadlineLabel(Rect(64, 0, 128, 13)));
+  addControl(new HeadlinePlaceholder(Rect(64, 0, 128, 13)));
 }
 
 void SoundInfoLayout::addInfoLabel()
@@ -23,7 +50,7 @@ void SoundInfoLayout::addInfoLabel()
   addControl(new InfoLabel(Rect(192, 0, 64, 13)));
 }
 
-Scrollable *SoundInfoLayout::createScrollableContent()
+Scrollable* SoundInfoLayout::createScrollableContent()
 {
   return new SoundInfoContent();
 }
