c396bb05c6b16eaf118f8f79c9039b3e1f76ae57
schlenkibus <justus.scheil@parteimail.de>
2019-11-05 09:27:36 +0100

    added new dual sound type symbols

--- playground/src/proxies/hwui/panel-unit/boled/preset-screens/CurrentVoiceGroupSelectionIndicator.h
+++ playground/src/proxies/hwui/panel-unit/boled/preset-screens/CurrentVoiceGroupSelectionIndicator.h
@@ -6,14 +6,4 @@ class CurrentVoiceGroupSelectionIndicator : public LabelRegular8
  public:
   explicit CurrentVoiceGroupSelectionIndicator(const Rect& r);
   void focusChanged();
-
- private:
-  /*
-      split.a.01	E0F0
-      split.a.02	E0F1
-      split.a.03	E0F2
-      layer.a.01	E0F3
-      layer.a.02	E0F4
-      layer.a.03	E0F5
-   */
 };
