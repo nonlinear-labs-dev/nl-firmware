2f88f2d334e363e97d2a436d51402d0fd6494b1b
schlenkibus <justus.scheil@parteimail.de>
2019-12-17 14:41:43 +0100

    replaced template class with discrete classes that contain the logic

--- playground/src/groups/UnisonGroup.cpp
+++ playground/src/groups/UnisonGroup.cpp
@@ -53,63 +53,68 @@ namespace Detail
       return ParameterSelectLayout2::onButton(i, down, modifiers);
     }
   };
-
-  template <typename tParameter> class UnisonParameter : public tParameter
-  {
-   public:
-    template <typename... tArgs>
-    explicit UnisonParameter(tArgs... args)
-        : tParameter(args...){};
-
-    DFBLayout *createLayout(FocusAndMode focusAndMode) const override
-    {
-      using dispatch = std::is_base_of<ModulateableParameter, tParameter>;
-      return createLayoutImpl(focusAndMode, dispatch());
-    }
-
-    DFBLayout *createLayoutImpl(FocusAndMode focusAndMode, std::true_type) const
-    {
-      switch(focusAndMode.mode)
-      {
-        case UIMode::Select:
-        default:
-          return new ModulateableParameterSelectLayout2();
-        case UIMode::Edit:
-          return new ModulateableParameterEditLayout2();
-        case UIMode::Info:
-          return new ParameterInfoLayout();
-      }
-    }
-
-    DFBLayout *createLayoutImpl(FocusAndMode focusAndMode, std::false_type) const
-    {
-      switch(focusAndMode.mode)
-      {
-        case UIMode::Select:
-        default:
-          return new UnisonLayout();
-        case UIMode::Edit:
-          return new UnmodulateableParameterEditLayout2();
-        case UIMode::Info:
-          return new ParameterInfoLayout();
-      }
-    }
-  };
 }
+class UnmodulateableUnisonParameter : public Parameter
+{
+ public:
+  template <typename... tArgs>
+  UnmodulateableUnisonParameter(tArgs... args)
+      : Parameter(args...)
+  {
+  }
+
+  DFBLayout *createLayout(FocusAndMode focusAndMode) const override
+  {
+    switch(focusAndMode.mode)
+    {
+      case UIMode::Select:
+      default:
+        return new Detail::UnisonLayout();
+      case UIMode::Edit:
+        return new UnmodulateableParameterEditLayout2();
+      case UIMode::Info:
+        return new ParameterInfoLayout();
+    }
+  }
+};
+
+class ModulateableUnisonParameterWithUnusualModUnit : public ModulateableParameterWithUnusualModUnit
+{
+ public:
+  template <typename... tArgs>
+  ModulateableUnisonParameterWithUnusualModUnit(tArgs... args)
+      : ModulateableParameterWithUnusualModUnit(args...)
+  {
+  }
+
+  DFBLayout *createLayout(FocusAndMode focusAndMode) const override
+  {
+    switch(focusAndMode.mode)
+    {
+      case UIMode::Select:
+      default:
+        return new ModulateableParameterSelectLayout2();
+      case UIMode::Edit:
+        return new ModulateableParameterEditLayout2();
+      case UIMode::Info:
+        return new ParameterInfoLayout();
+    }
+  }
+};
 
 void UnisonGroup::init()
 {
-  appendParameter(new Detail::UnisonParameter<Parameter>(
+  appendParameter(new UnmodulateableUnisonParameter(
       this, ParameterId{ 249, getVoiceGroup() }, ScaleConverter::get<LinearCountScaleConverter<24, VoicesDimension> >(),
       0, 23, 23));
 
-  appendParameter(new Detail::UnisonParameter<ModulateableParameterWithUnusualModUnit>(
-      this, ParameterId{ 250, getVoiceGroup() }, ScaleConverter::get<Fine12STScaleConverter>(),
-      ScaleConverter::get<FineBipolar12STScaleConverter>(), 0, 120, 12000));
+  appendParameter(new ModulateableUnisonParameterWithUnusualModUnit(this, ParameterId{250, getVoiceGroup() },
+                                                                    ScaleConverter::get<Fine12STScaleConverter>(),
+                                                                    ScaleConverter::get<FineBipolar12STScaleConverter>(), 0, 120, 12000));
 
-  appendParameter(new Detail::UnisonParameter<Parameter>(
+  appendParameter(new UnmodulateableUnisonParameter(
       this, ParameterId{ 252, getVoiceGroup() }, ScaleConverter::get<Linear360DegreeScaleConverter>(), 0, 360, 3600));
 
-  appendParameter(new Detail::UnisonParameter<Parameter>(
+  appendParameter(new UnmodulateableUnisonParameter(
       this, ParameterId{ 253, getVoiceGroup() }, ScaleConverter::get<Linear100PercentScaleConverter>(), 0, 100, 1000));
 }
