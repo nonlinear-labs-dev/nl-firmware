100bfa565087a12ef44d6a8f3fb4954c4046d8ee
kleiner-bill <matthias.seeber@nonlinear-labs.de>
2019-12-17 19:56:35 +0100

    reverb: re-included reverb internal code, seems to have solved the reverb issue

--- audio-engine/src/synth/c15-audio-engine/ae_mono_reverb.h
+++ audio-engine/src/synth/c15-audio-engine/ae_mono_reverb.h
@@ -20,7 +20,7 @@
 #define REV_G_3 0.64093f
 #define REV_G_4 0.653011f
 
-// Primes - 1 for 48 kHz
+/*********** Primes - 1 for 48 kHz ***********/
 #define REV_DEL_L1 280   // 563
 #define REV_DEL_L2 1122  // 2243
 #define REV_DEL_L3 862   // 1723
@@ -46,37 +46,116 @@ namespace Engine
   class MonoReverb
   {
    public:
-    float m_out_L, m_out_R, m_out_dry, m_out_wet;
     MonoReverb();
+    float m_out_L, m_out_R, m_out_dry, m_out_wet;
+
     void init(const float _samplerate, const uint32_t _upsampleFactor);
     void set(MonoSignals &_signals);
     void apply(MonoSignals &_signals, const float _rawSample_L, const float _rawSample_R);
     void resetDSP();
 
    private:
-    // delay buffer
-    std::vector<float> m_buffer_L, m_buffer_L1, m_buffer_L2, m_buffer_L3, m_buffer_L4, m_buffer_L5, m_buffer_L6,
-        m_buffer_L7, m_buffer_L8, m_buffer_L9, m_buffer_R, m_buffer_R1, m_buffer_R2, m_buffer_R3, m_buffer_R4,
-        m_buffer_R5, m_buffer_R6, m_buffer_R7, m_buffer_R8, m_buffer_R9;
-    float
-        // reverb modulation
-        m_mod_1a,
-        m_mod_2a, m_mod_1b, m_mod_2b, m_lfo_omega_1, m_lfo_omega_2, m_lfo_stateVar_1, m_lfo_stateVar_2, m_depth,
-        // loop filter
-        m_warpConst_PI, m_omegaClip_max, m_lp_a0, m_lp_a1, m_lp_omega, m_hp_a0, m_hp_a1, m_hp_omega, m_lp_stateVar_L,
-        m_lp_stateVar_R, m_hp_stateVar_L, m_hp_stateVar_R,
-        // delay buffer
-        m_fb_amnt, m_absorb, m_bal_half, m_bal_full, m_preDel_L, m_preDel_R, m_stateVar_L1, m_stateVar_R1,
-        m_stateVar_L2, m_stateVar_R2, m_stateVar_L3, m_stateVar_R3, m_stateVar_L4, m_stateVar_R4, m_stateVar_L5,
-        m_stateVar_R5, m_stateVar_L6, m_stateVar_R6, m_stateVar_L7, m_stateVar_R7, m_stateVar_L8, m_stateVar_R8,
-        m_stateVar_L9, m_stateVar_R9,
-        // smoothing
-        m_smooth_inc, m_depth_inc, m_depth_ramp, m_depth_target, m_depth_base, m_depth_diff, m_size, m_size_ramp,
-        m_size_target, m_size_base, m_size_diff, m_bal, m_bal_ramp, m_bal_target, m_bal_base, m_bal_diff,
-        m_preDel_L_ramp, m_preDel_L_target, m_preDel_L_base, m_preDel_L_diff, m_preDel_R_ramp, m_preDel_R_target,
-        m_preDel_R_base, m_preDel_R_diff, m_lp_omega_ramp, m_lp_omega_target, m_lp_omega_base, m_lp_omega_diff,
-        m_hp_omega_ramp, m_hp_omega_target, m_hp_omega_base, m_hp_omega_diff;
-    // clock division and delay buffer indices
-    uint32_t m_slow_tick, m_slow_thrsh, m_buffer_indx, m_buffer_sz_m1, m_buffer_sz_m2;
+    uint32_t m_slow_tick;
+    uint32_t m_slow_thrsh;
+
+    //************************** Reverb Modulation ***************************//
+    float m_mod_1a, m_mod_2a, m_mod_1b, m_mod_2b;
+    float m_lfo_omega_1, m_lfo_omega_2;
+    float m_lfo_stateVar_1, m_lfo_stateVar_2;
+    float m_depth;
+
+    //****************************** Loop Filter *****************************//
+    float m_warpConst_PI;
+    float m_omegaClip_max;
+
+    float m_lp_a0, m_lp_a1, m_lp_omega;
+    float m_hp_a0, m_hp_a1, m_hp_omega;
+
+    float m_lp_stateVar_L, m_lp_stateVar_R;
+    float m_hp_stateVar_L, m_hp_stateVar_R;
+
+    //***************************** Delay Buffer *****************************//
+    float m_fb_amnt;
+    float m_absorb;
+    float m_bal_half, m_bal_full;
+
+    uint32_t m_buffer_indx;
+    uint32_t m_buffer_sz_m1, m_buffer_sz_m2;
+
+    float m_preDel_L, m_preDel_R;
+
+    std::vector<float> m_buffer_L;
+    std::vector<float> m_buffer_R;
+
+    std::vector<float> m_buffer_L1;
+    std::vector<float> m_buffer_L2;
+    std::vector<float> m_buffer_L3;
+    std::vector<float> m_buffer_L4;
+    std::vector<float> m_buffer_L5;
+    std::vector<float> m_buffer_L6;
+    std::vector<float> m_buffer_L7;
+    std::vector<float> m_buffer_L8;
+    std::vector<float> m_buffer_L9;
+
+    std::vector<float> m_buffer_R1;
+    std::vector<float> m_buffer_R2;
+    std::vector<float> m_buffer_R3;
+    std::vector<float> m_buffer_R4;
+    std::vector<float> m_buffer_R5;
+    std::vector<float> m_buffer_R6;
+    std::vector<float> m_buffer_R7;
+    std::vector<float> m_buffer_R8;
+    std::vector<float> m_buffer_R9;
+
+    float m_stateVar_L1, m_stateVar_R1;
+    float m_stateVar_L2, m_stateVar_R2;
+    float m_stateVar_L3, m_stateVar_R3;
+    float m_stateVar_L4, m_stateVar_R4;
+    float m_stateVar_L5, m_stateVar_R5;
+    float m_stateVar_L6, m_stateVar_R6;
+    float m_stateVar_L7, m_stateVar_R7;
+    float m_stateVar_L8, m_stateVar_R8;
+    float m_stateVar_L9, m_stateVar_R9;
+
+    //******************************* Smoothing ******************************//
+    float m_smooth_inc;
+
+    float m_depth_inc;
+    float m_depth_ramp;
+    float m_depth_target;
+    float m_depth_base;
+    float m_depth_diff;
+
+    float m_size;
+    float m_size_ramp;
+    float m_size_target;
+    float m_size_base;
+    float m_size_diff;
+
+    float m_bal;
+    float m_bal_ramp;
+    float m_bal_target;
+    float m_bal_base;
+    float m_bal_diff;
+
+    float m_preDel_L_ramp;
+    float m_preDel_L_target;
+    float m_preDel_L_base;
+    float m_preDel_L_diff;
+
+    float m_preDel_R_ramp;
+    float m_preDel_R_target;
+    float m_preDel_R_base;
+    float m_preDel_R_diff;
+
+    float m_lp_omega_ramp;
+    float m_lp_omega_target;
+    float m_lp_omega_base;
+    float m_lp_omega_diff;
+
+    float m_hp_omega_ramp;
+    float m_hp_omega_target;
+    float m_hp_omega_base;
+    float m_hp_omega_diff;
   };
-}  // namespace Engine
+}
