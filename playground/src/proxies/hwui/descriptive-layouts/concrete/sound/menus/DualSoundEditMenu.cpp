1a3115297e3c5e42e8bd10b394384e1c5fb209d9
schlenkibus <justus.scheil@parteimail.de>
2019-12-18 17:23:30 +0100

    added init sound for dual sound

--- playground/src/proxies/hwui/descriptive-layouts/concrete/sound/menus/DualSoundEditMenu.cpp
+++ playground/src/proxies/hwui/descriptive-layouts/concrete/sound/menus/DualSoundEditMenu.cpp
@@ -5,6 +5,7 @@
 #include <proxies/hwui/descriptive-layouts/concrete/sound/menus/items/PartLabelItem.h>
 #include <proxies/hwui/descriptive-layouts/concrete/menu/menu-items/TextItem.h>
 #include <proxies/hwui/HWUI.h>
+#include <proxies/hwui/descriptive-layouts/concrete/sound/menus/items/InitSound.h>
 
 DualSoundEditMenu::DualSoundEditMenu(const Rect &r)
     : ScrollMenu(r)
@@ -18,4 +19,5 @@ void DualSoundEditMenu::init()
   addItem<LoadPresetIntoVoiceGroupItem>(fullWidth);
   addItem<PartLabelItem>(fullWidth);
   addItem<ConvertToSingleItem>(fullWidth);
+  addItem<InitSound>(fullWidth);
 }
