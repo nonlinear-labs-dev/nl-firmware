563e5e0ae15ea8a630f670c02753b99f3ce08100
schlenkibus <justus.scheil@parteimail.de>
2020-01-02 14:52:16 +0100

    added send document details on scale update for unison voices parameter

--- playground/src/parameters/UnisonVoicesParameter.h
+++ playground/src/parameters/UnisonVoicesParameter.h
@@ -6,4 +6,10 @@ class UnisonVoicesParameter : public UnmodulateableUnisonParameter
  public:
   explicit UnisonVoicesParameter(ParameterGroup* group, VoiceGroup vg);
   void updateScaling(SoundType type);
+
+protected:
+  bool shouldWriteDocProperties(tUpdateID knownRevision) const override;
+  bool didScalingChange() const;
+
+  mutable bool m_scalingChanged = false;
 };
