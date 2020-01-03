20055dcd8492927e20c9ba39f8d2d873f8565877
schlenkibus <justus.scheil@parteimail.de>
2020-01-03 16:24:30 +0100

    include logging code of Milestone_1-5 and options from 1.7

--- bbbb/src/Options.cpp
+++ bbbb/src/Options.cpp
@@ -1,10 +1,30 @@
 #include "Options.h"
 #include <glibmm/optiongroup.h>
 
-Options::Options(int &argc, char **&argv)
+namespace OptionDetail
+{
+  inline Glib::OptionEntry createOption(const char* l, char s, const char* desc)
+  {
+    Glib::OptionEntry entry;
+    entry.set_long_name(l);
+    entry.set_short_name(s);
+    entry.set_description(desc);
+    return entry;
+  }
+}
+
+Options::Options(int& argc, char**& argv)
 {
   Glib::OptionGroup mainGroup("common", "common options");
   Glib::OptionContext ctx;
+
+  auto timeStamps
+      = OptionDetail::createOption("timestamps", 't', "measure turn around time encoder -> playground -> oled");
+  auto heartBeatLog = OptionDetail::createOption("log-heart-beat", 'h', "log lpc heart beat to console");
+  auto rawLpcLog = OptionDetail::createOption("log-lpc-raw", 'l', "log raw lpc messages to console");
+
+  auto pgHost = OptionDetail::createOption("playground-host", 'p', "where to find the playground");
+  auto aeHost = OptionDetail::createOption("audioengine-host", 'a', "where to find the audio engine");
 
   Glib::OptionEntry pg;
   pg.set_long_name("playground-host");
@@ -32,4 +52,19 @@ Glib::ustring Options::getPlaygroundHost() const
 Glib::ustring Options::getAudioEngineHost() const
 {
   return m_audioengineHost;
+}
+
+bool Options::doTimeStamps() const
+{
+  return m_doTimeStamps;
+}
+
+bool Options::logLPCRaw() const
+{
+  return m_logLpcRaw;
+}
+
+bool Options::logHeartBeat() const
+{
+  return m_logHeartBeat;
 }
