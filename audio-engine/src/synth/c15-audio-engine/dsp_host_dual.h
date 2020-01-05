4b179cd1666a0995436a817e043e3afa38352372
kleiner-bill <matthias.seeber@nonlinear-labs.de>
2020-01-05 13:47:09 +0100

    wip: expanded velocity resolution to full 14 bit range [0 ... 16383] (#1285)

--- audio-engine/src/synth/c15-audio-engine/dsp_host_dual.h
+++ audio-engine/src/synth/c15-audio-engine/dsp_host_dual.h
@@ -113,8 +113,8 @@ class dsp_host_dual
   MonoSection m_mono[2];
   LayerSignalCollection m_z_layers[2];
   // helper values
-  const float m_format_vel = 4095.0f / 127.0f, m_format_hw = 16000.0f / 127.0f, m_format_pb = 16000.0f / 16383.0f,
-              m_norm_vel = 1.0f / 4095.0f, m_norm_hw = 1.0f / 16000.0f;
+  const float m_format_vel = 16383.0f / 127.0f, m_format_hw = 16000.0f / 127.0f, m_format_pb = 16000.0f / 16383.0f,
+              m_norm_vel = 1.0f / 16383.0f, m_norm_hw = 1.0f / 16000.0f;
   uint32_t m_key_pos = 0, m_tone_state = 0;
   bool m_key_valid = false, m_layer_changed = false, m_glitch_suppression = false;
   // handles for inconvenient stuff
