3e72c5249fe67ed12ed504b26c0ed0b68f96397c
schlenkibus <justus.scheil@parteimail.de>
2020-01-07 19:46:22 +0100

    removed unused class

--- playground/src/proxies/hwui/descriptive-layouts/concrete/sound/menus/items/ConvertToSoundTypeItem.h
+++ playground/src/proxies/hwui/descriptive-layouts/concrete/sound/menus/items/ConvertToSoundTypeItem.h
@@ -2,7 +2,6 @@
 
 #include <proxies/hwui/descriptive-layouts/concrete/menu/menu-items/AnimatedGenericItem.h>
 #include <nltools/Types.h>
-#include <proxies/hwui/descriptive-layouts/concrete/sound/menus/SoundEditMenu.h>
 
 class ConvertToSoundTypeItem : public AnimatedGenericItem
 {
