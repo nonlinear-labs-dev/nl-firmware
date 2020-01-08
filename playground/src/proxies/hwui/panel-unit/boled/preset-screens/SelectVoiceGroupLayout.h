c46ca0c3aeced67ac9e00a0acbca2da173583748
schlenkibus <justus.scheil@parteimail.de>
2020-01-08 12:35:11 +0100

    fixes #1348

--- playground/src/proxies/hwui/panel-unit/boled/preset-screens/SelectVoiceGroupLayout.h
+++ playground/src/proxies/hwui/panel-unit/boled/preset-screens/SelectVoiceGroupLayout.h
@@ -1,13 +1,23 @@
 #pragma once
 #include <proxies/hwui/DFBLayout.h>
+#include <nltools/Types.h>
 #include "PresetManagerLayout.h"
 
 class SelectVoiceGroupLayout : public DFBLayout
 {
-public:
-  SelectVoiceGroupLayout(PresetManagerLayout *parent);
+ public:
+  SelectVoiceGroupLayout(PresetManagerLayout* parent);
 
   bool onButton(Buttons i, bool down, ::ButtonModifiers modifiers) override;
-private:
+
+ private:
+  VoiceGroup m_loadFrom = VoiceGroup::I;
+
+  void toggleSelection();
+  void load();
+  void cancel();
+
+  Button* m_I;
+  Button* m_II;
   PresetManagerLayout* m_parent;
 };
