ddb8b463261919fa8adb3ba0e64bcbb7ae743acf
schlenkibus <justus.scheil@parteimail.de>
2020-01-12 11:42:57 +0100

    fixes #1396 menu svg's needed! developer art currently

--- NonMaps/src/main/java/com/nonlinearlabs/client/world/overlay/belt/presets/LoadModeContextMenu.java
+++ NonMaps/src/main/java/com/nonlinearlabs/client/world/overlay/belt/presets/LoadModeContextMenu.java
@@ -1,6 +1,7 @@
 package com.nonlinearlabs.client.world.overlay.belt.presets;
 
 import com.nonlinearlabs.client.dataModel.setup.SetupModel.LoadMode;
+import com.nonlinearlabs.client.presenters.PresetManagerPresenter;
 import com.nonlinearlabs.client.presenters.PresetManagerPresenterProvider;
 import com.nonlinearlabs.client.useCases.EditBufferUseCases;
 import com.nonlinearlabs.client.world.Control;
@@ -13,17 +14,20 @@ public class LoadModeContextMenu extends ContextMenu {
 
     public LoadModeContextMenu(OverlayLayout parent) {
         super(parent);
-
-        String[] entries = PresetManagerPresenterProvider.get().getPresenter().loadModeMenuEntries;
-
-        addChild(new ContextMenuItem(this, entries[0]) {
-            @Override
-            public Control click(Position eventPoint) {
-                EditBufferUseCases.get().setLoadMode(LoadMode.LoadToPart);
-                return super.click(eventPoint);
-            }
-        });
-
+        PresetManagerPresenter prov = PresetManagerPresenterProvider.get().getPresenter();
+        String[] entries = prov.loadModeMenuEntries;
+        
+        if(prov.loadToPartEnabled)
+        {
+            addChild(new ContextMenuItem(this, entries[0]) {
+                @Override
+                public Control click(Position eventPoint) {
+                    EditBufferUseCases.get().setLoadMode(LoadMode.LoadToPart);
+                    return super.click(eventPoint);
+                }
+            });
+        }
+        
         addChild(new ContextMenuItem(this, entries[1]) {
             @Override
             public Control click(Position eventPoint) {
@@ -31,7 +35,7 @@ public class LoadModeContextMenu extends ContextMenu {
                 return super.click(eventPoint);
             }
         });
-
+    
         addChild(new ContextMenuItem(this, entries[2]) {
             @Override
             public Control click(Position eventPoint) {
@@ -40,5 +44,4 @@ public class LoadModeContextMenu extends ContextMenu {
             }
         });
     }
-
 }
