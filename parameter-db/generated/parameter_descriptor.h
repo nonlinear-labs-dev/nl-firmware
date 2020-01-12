12664225cd4fbeb7118ff34d614ea52a30cf5add
kleiner-bill <matthias.seeber@nonlinear-labs.de>
2020-01-12 20:21:33 +0100

    fixed #1400, #1405

--- parameter-db/generated/parameter_descriptor.h
+++ parameter-db/generated/parameter_descriptor.h
@@ -2,7 +2,7 @@
 
 /******************************************************************************/
 /**   @file       parameter_descriptor.h
-      @date       2020-01-11, 17:51
+      @date       2020-01-12, 20:13
       @version    1.7B-3
       @author     M. Seeber
       @brief      provide a flexible descriptor structure
