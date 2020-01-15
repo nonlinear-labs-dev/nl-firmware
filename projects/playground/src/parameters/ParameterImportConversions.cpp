776372d0d5e5e46738a23438996c52802da52ab0
schlenkibus <justus.scheil@parteimail.de>
2020-01-15 12:07:00 +0100

    unison voices was 12 voices before factor of 0.5 was correct

--- projects/playground/src/parameters/ParameterImportConversions.cpp
+++ projects/playground/src/parameters/ParameterImportConversions.cpp
@@ -198,6 +198,7 @@ void ParameterImportConversions::registerTests()
 
 tControlPositionValue ParameterImportConversions::voicesV5ToV6(tControlPositionValue unisonVoices) const
 {
+#warning "check!"
   if(unisonVoices == 1.0)
     return 0.5 - (1.0 / 24.0);
   return unisonVoices * 0.5;
