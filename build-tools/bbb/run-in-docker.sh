933fa554795d5d297a1fca83278f6c355592526d
Henry Hoegelow <henry@hoegelow.com>
2020-01-24 15:27:56 +0100

    fix type

--- build-tools/bbb/run-in-docker.sh
+++ build-tools/bbb/run-in-docker.sh
@@ -9,7 +9,7 @@ SCRIPT="echo \"Starting script:\""
 
 for var in "$@"
 do
-        SCRIPT="$SCRIPT && $var"
+    SCRIPT="$SCRIPT && $var"
 done
 
 SCRIPT="$SCRIPT && echo \"Script done.\""
@@ -18,4 +18,4 @@ USER_ID=$(id -u $USER)
 DOCKER_GROUP_ID=$(getent group docker | cut -d: -f3)
 DOCKERNAME="nl-cross-build-environment-$USER"
 
-docker run -u $USER_ID:$DOCKER_GROUP_ID --rm -v $BINARY_DIR:/workdir -v $SOURCE_DIR:/sources $DOCKERNAME bash -c "$COMMANDS"
+docker run -u $USER_ID:$DOCKER_GROUP_ID --rm -v $BINARY_DIR:/workdir -v $SOURCE_DIR:/sources $DOCKERNAME bash -c "$SCRIPT"
