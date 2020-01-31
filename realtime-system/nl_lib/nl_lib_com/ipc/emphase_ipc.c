bd34c70369487fef6fd05df16eac95adb3d4baf9
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2020-01-31 08:35:19 +0100

    ring buffers for all adc channels, refactoring

--- realtime-system/nl_lib/nl_lib_com/ipc/emphase_ipc.c
+++ realtime-system/nl_lib/nl_lib_com/ipc/emphase_ipc.c
@@ -7,14 +7,107 @@
 #include <stdint.h>
 #include "ipc/emphase_ipc.h"
 
-#define EMPHASE_IPC_KEYBUFFER_SIZE 64
-#define EMPHASE_IPC_KEYBUFFER_MASK EMPHASE_IPC_KEYBUFFER_SIZE - 1
+// ADC ring buffers
+// Must be 2^N in size. This also determines the averaging.
+// Size should NOT be larger than the number of aquisitions between M4 read-out operations
+#define IPC_ADC_BUFFER_SIZE (8)
+#define IPC_ADC_BUFFER_MASK (IPC_ADC_BUFFER_SIZE - 1)
 
+typedef struct
+{
+  int32_t values[IPC_ADC_BUFFER_SIZE];
+} ADC_BUFFER_T;
+
+#define EMPHASE_IPC_KEYBUFFER_SIZE (64)
+#define EMPHASE_IPC_KEYBUFFER_MASK (EMPHASE_IPC_KEYBUFFER_SIZE - 1)
+
+// data in shared memory is accessible via pointers only, can't define variables directly
 static volatile IPC_KEY_EVENT_T* keyBufferData;
 static volatile uint32_t*        keyBufferWritePos;
 static volatile uint32_t*        keyBufferReadPos;
-static volatile int32_t*         playBufferData;
+static volatile ADC_BUFFER_T*    adcBufferData;
+static volatile uint32_t*        adcConfigData;
+static volatile int*             adcBufferWriteIndex;
+static volatile int*             adcBufferReadIndex;
 
+
+/******************************************************************************
+*	Functions for both the M4 and M0 to interface the PlayBuffers.
+******************************************************************************/
+
+/******************************************************************************/
+/**	@brief      Read ADC channel value
+*   @param[in]	IPC id of the adc channel 0...15
+*   @return     adc channel value
+******************************************************************************/
+int32_t IPC_ReadAdcBuffer(uint8_t adc_id)
+{
+  return adcBufferData[adc_id].values[*adcBufferReadIndex];
+}
+
+/******************************************************************************/
+/**	@brief      Read ADC channel value as average of whole ring buffer contents
+*   @param[in]	IPC id of the adc channel 0...15
+*   @return     adc channel value
+******************************************************************************/
+int32_t IPC_ReadAdcBufferAveraged(uint8_t adc_id)
+{
+  int32_t x = 0;
+  for (int i = 0; i < IPC_ADC_BUFFER_SIZE; i++)
+    x += adcBufferData[adc_id].values[i];
+  return x / IPC_ADC_BUFFER_SIZE;
+}
+
+/******************************************************************************/
+/**	@brief      Write ADC value
+*   @param[in]	IPC id of the adc channel 0...15
+*   @param[in]  adc channel value
+******************************************************************************/
+void IPC_WriteAdcBuffer(uint8_t adc_id, int32_t value)
+{
+  adcBufferData[adc_id].values[*adcBufferWriteIndex] = value;
+}
+
+
+/******************************************************************************/
+/**	@brief      Advance write buffer index to next position
+******************************************************************************/
+void IPC_AdcBufferWriteNext(void)
+{
+	*adcBufferWriteIndex = (*adcBufferWriteIndex + 1) & IPC_ADC_BUFFER_MASK;
+}
+
+/******************************************************************************/
+/**	@brief      Update read buffer index to current position
+******************************************************************************/
+void IPC_AdcUpdateReadIndex(void)
+{
+	*adcBufferReadIndex = *adcBufferWriteIndex;
+}
+
+/******************************************************************************/
+/**	@brief      Read the ADC line input config for all physical pedal channels
+*   @return     config bits [32 bits, 4 bytes from ADC67(MSB) to ADC12(LSB)]
+*   for bit masks see  espi/dev/nl_espi_dev_pedals.h
+******************************************************************************/
+uint32_t IPC_ReadPedalAdcConfig(void)
+{
+  return *adcConfigData;
+}
+
+/******************************************************************************/
+/**	@brief      Read the ADC line input config for all physical pedal channels
+*   @param{in]  config bits [32 bits, 4 bytes from ADC67(MSB) to ADC12(LSB)]
+*   for bit masks see  espi/dev/nl_espi_dev_pedals.h
+******************************************************************************/
+void IPC_WritePedalAdcConfig(uint32_t config)
+{
+  *adcConfigData = config;
+}
+
+/******************************************************************************/
+/**	@brief   Initialize the pointers to the shared memory
+******************************************************************************/
 static void Init_Addr(void)
 {
   uint32_t addr = SHARED_MEMORY_BASE;
@@ -28,56 +121,49 @@ static void Init_Addr(void)
   keyBufferReadPos = (uint32_t*) (addr);
   addr += sizeof(uint32_t);
 
-  playBufferData = (int32_t*) (addr);
-  addr += EMPHASE_NUMBER_OF_PLAY_DEVICES * sizeof(uint32_t);
+  adcBufferData = (ADC_BUFFER_T*) (addr);
+  addr += IPC_ADC_NUMBER_OF_CHANNELS * sizeof adcBufferData[0];
+
+  adcConfigData = (uint32_t*) (addr);
+  addr += sizeof adcConfigData[0];
+
+  adcBufferWriteIndex = (int*) (addr);
+  addr += sizeof *adcBufferWriteIndex;
+
+  adcBufferReadIndex = (int*) (addr);
+  addr += sizeof *adcBufferReadIndex;
 }
 
 /******************************************************************************/
-/**
+/**	@brief  setup and clear data in shared memory
 *******************************************************************************/
-void Emphase_IPC_M4_Init(void)
-{
-  Init_Addr();
-
-  *keyBufferReadPos = 0;
-
-  uint8_t i;
-  for (i = 0; i < EMPHASE_NUMBER_OF_PLAY_DEVICES; i++)
-  {
-    playBufferData[i] = 0;
-  }
-}
-
-/******************************************************************************/
-/**
-*******************************************************************************/
-void Emphase_IPC_M0_Init(void)
+void Emphase_IPC_Init(void)
 {
   Init_Addr();
 
   *keyBufferWritePos = 0;
 
-  uint8_t i;
-  for (i = 0; i < EMPHASE_IPC_KEYBUFFER_SIZE; i++)
+  for (int i = 0; i < EMPHASE_IPC_KEYBUFFER_SIZE; i++)
   {
     keyBufferData[i].key       = 0;
     keyBufferData[i].timeInUs  = 0;
     keyBufferData[i].direction = 0;
   }
 
-  for (i = 0; i < EMPHASE_NUMBER_OF_PLAY_DEVICES; i++)
-  {
-    playBufferData[i] = 0;
-  }
+  for (int i = 0; i < IPC_ADC_NUMBER_OF_CHANNELS; i++)
+    for (int k = 0; k < IPC_ADC_BUFFER_SIZE; k++)
+      adcBufferData[i].values[k] = 0;
+  *adcConfigData = 0;
+  adcBufferReadIndex = 0;
+  adcBufferWriteIndex = 0;
 }
 
-/******************************************************************************
-  @brief  		Here the M0 write key events to a circular buffer
-				that the M4 will read
-  @param[in]	keyEvent: A struct containing the index of the key
-				and the direction and travel time of the last key action
+/******************************************************************************/
+/**  @brief     Here the M0 write key events to a circular buffer
+                that the M4 will read
+     @param[in] keyEvent: A struct containing the index of the key
+                and the direction and travel time of the last key action
 *******************************************************************************/
-
 void Emphase_IPC_M0_KeyBuffer_WriteKeyEvent(IPC_KEY_EVENT_T keyEvent)
 {
   keyBufferData[*keyBufferWritePos] = keyEvent;
@@ -85,15 +171,14 @@ void Emphase_IPC_M0_KeyBuffer_WriteKeyEvent(IPC_KEY_EVENT_T keyEvent)
   *keyBufferWritePos = (*keyBufferWritePos + 1) & (EMPHASE_IPC_KEYBUFFER_MASK);
 }
 
-/******************************************************************************
-  @brief		Here the M4 reads key events from a circular buffer
-				that have been written by the M0
-  @param[in]	pKeyEvent: pointer to an array of key events
-				that will be processed by the voice allocation
-				maxNumOfEventsToRead: size of the array pointed by pKeyEvent
-  @return		Number of new key events (0: nothing to do)
+/******************************************************************************/
+/** @brief      Here the M4 reads key events from a circular buffer
+                that have been written by the M0
+    @param[in]  pKeyEvent: pointer to an array of key events
+                that will be processed by the voice allocation
+                maxNumOfEventsToRead: size of the array pointed by pKeyEvent
+    @return     Number of new key events (0: nothing to do)
 *******************************************************************************/
-
 uint32_t Emphase_IPC_M4_KeyBuffer_ReadBuffer(IPC_KEY_EVENT_T* pKeyEvent, uint8_t maxNumOfEventsToRead)
 {
   uint8_t count = 0;
@@ -111,55 +196,8 @@ uint32_t Emphase_IPC_M4_KeyBuffer_ReadBuffer(IPC_KEY_EVENT_T* pKeyEvent, uint8_t
 }
 
 /******************************************************************************/
-/**	@brief  Functions for both, the M4 and M0 to interface the PlayBuffer.
-  			=> see the defines in the header file for details
-
-    Name							ID				range   Emphase Version
-    -----------------------------------------------------------------------
- 	ESPI_DEV_Pedals[8]		    	0..7			12 Bit
-    ESPI_DEV_Pitchbender	    	8				12 Bit
-    ESPI_DEV_Aftertouch		    	9				12 Bit
-    ESPI_DEV_Ribbons[2]		    	10..11 | Up/Low	12 Bit
-    ESPI_DEV_VolPoti		    	12				12 Bit
-    ESPI_DEV_Attenuator		    	13				12 Bit
-    ESPI_DEV_Detect[4]				14..17 | 0..3	 1 Bit
-	ESPI_DEV_PullResistors[8]		18..25 | 0..7	 1 Bit  v4
-	ESPI_DEV_PedalsV2_SetPedalState	26..29 | 0..3    4 Bit  v5
-
+/** @brief      Return size of key buffer
 *******************************************************************************/
-void Emphase_IPC_PlayBuffer_Write(uint8_t id, int32_t val)
-{
-  playBufferData[id] = val;
-}
-
-void Emphase_IPC_PlayBuffer_WriteSummed(uint8_t id, int32_t val)
-{
-  playBufferData[id] += val;
-  playBufferData[id + 1]++;
-}
-void Emphase_IPC_PlayBuffer_ResetSummed(uint8_t id)
-{
-  playBufferData[id]     = 0;
-  playBufferData[id + 1] = 0;
-}
-
-/******************************************************************************/
-/**	@brief
- 	@return
-*******************************************************************************/
-int32_t Emphase_IPC_PlayBuffer_Read(uint8_t id)
-{
-  return playBufferData[id];
-}
-int32_t Emphase_IPC_PlayBuffer_ReadSummed(uint8_t id)
-{
-  return playBufferData[id] / playBufferData[id + 1];
-}
-
-/******************************************************************************/
-/**
-*******************************************************************************/
-
 uint32_t Emphase_IPC_KeyBuffer_GetSize()
 {
   return EMPHASE_IPC_KEYBUFFER_SIZE;
