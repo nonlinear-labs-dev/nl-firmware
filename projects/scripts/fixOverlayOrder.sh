ca103eaea1903a5751318eddb8c763e13186185d
Henry Hoegelow <henry@hoegelow.com>
2020-02-27 17:32:10 +0100

    WIP: fix overlay order

---
+++ projects/scripts/fixOverlayOrder.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -x
+
+if mount | grep "/lroot:/nloverlay/os-overlay"; then
+    mkdir /tmp/sda2
+    mount /dev/sda2 /tmp/sda2
+    sed -i 's@/lroot:/nloverlay/os-overlay@/nloverlay/os-overlay:/lroot@' /tmp/sda2/lib/initcpio/hooks/oroot
+    umount /tmp/sda2
+    mkinitcpio -p linux
+    mkinitcpio -p linux-rt
+    reboot
+fi
