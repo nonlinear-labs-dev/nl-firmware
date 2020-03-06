41f3f37b6c67be16e97f3109a645080be8e2ac66
AntonSchmied <37212595+AntonSchmied@users.noreply.github.com>
2020-03-06 15:38:02 +0100

    quick fix - renaming the update.tar
    nonlinear-c15-update.tar is renamed before run.sh is executed to avoid eternal update spirals of doom, since run.sh ends with a system reboot and causes new installation.

--- projects/scripts/bbb/install-update.sh
+++ projects/scripts/bbb/install-update.sh
@@ -7,9 +7,9 @@ if [ -e /mnt/usb-stick/nonlinear-c15-update.tar ]
 then
 	cp /mnt/usb-stick/nonlinear-c15-update.tar /update		
 	cd /update
-    tar xf nonlinear-c15-update.tar
+	tar xf nonlinear-c15-update.tar
+	mv /mnt/usb-stick/nonlinear-c15-update.tar /mnt/usb-stick/nonlinear-c15-update.tar-copied
 	chmod +x /update/run.sh
 	/bin/sh /update/run.sh
-	mv /mnt/usb-stick/nonlinear-c15-update.tar /mnt/usb-stick/nonlinear-c15-update.tar-copied
 fi
 
