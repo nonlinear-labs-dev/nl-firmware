4991ef7e9c7d7c0e3ae0b67dd5c630ce7192be39
schlenkibus <justus.scheil@parteimail.de>
2020-03-09 10:37:11 +0100

    moved hysteresis tests to catch and removed uneccessary import conversion tests from parameter import, as the tests did not add more value than the existing test case for decibel conversion

---
+++ projects/playground/src/testing/unit-tests/parameters/HysteresisTests.cpp
@@ -0,0 +1,15 @@
+#include <testing/TestHelper.h>
+#include <parameters/value/Hysteresis.h>
+
+TEST_CASE("Hysteresis Set", "[Parameter]")
+{
+  Hysteresis h(0.0, 0.1);
+  CHECK(h.set(0.01) == 0.0);
+  CHECK(h.set(-0.01) == 0.0);
+  CHECK(h.set(0.05) == 0.05);
+  CHECK(h.set(-0.04) == 0.05);
+  CHECK(h.set(-0.05) == -0.05);
+  CHECK(h.set(-0.125) == -0.125);
+  CHECK(h.set(-0.15) == -0.15);
+  CHECK(h.set(-0.125) == -0.15);
+}
