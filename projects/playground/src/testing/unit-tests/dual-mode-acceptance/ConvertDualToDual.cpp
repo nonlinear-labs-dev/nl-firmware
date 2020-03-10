b8edb8bd8cfed0585ee35deb50de96819812698c
schlenkibus <justus.scheil@parteimail.de>
2020-03-10 15:01:59 +0100

    fixes #1527
    check if the old type is single, only then copy the first voice group to voice group I
    if not single we are converting dual to dual and the voice groups will remain untouched

--- projects/playground/src/testing/unit-tests/dual-mode-acceptance/ConvertDualToDual.cpp
+++ projects/playground/src/testing/unit-tests/dual-mode-acceptance/ConvertDualToDual.cpp
@@ -1,6 +1,6 @@
 #include <testing/TestHelper.h>
 
-TEST_CASE("Convert Layer to Split Sound", "[EditBuffer][Convert]")
+TEST_CASE("Convert Layer to Split Sound Resets Split", "[EditBuffer][Convert]")
 {
   auto eb = TestHelper::getEditBuffer();
   auto splitPoint = eb->findParameterByID({ 356, VoiceGroup::Global });
@@ -21,4 +21,27 @@ TEST_CASE("Convert Layer to Split Sound", "[EditBuffer][Convert]")
   }
 
   CHECK_PARAMETER_CP_EQUALS_FICTION(splitPoint, 0.5);
+}
+
+TEST_CASE("Convert Layer to Split works for parameter Groups", "[EditBuffer][Convert]")
+{
+  auto eb = TestHelper::getEditBuffer();
+
+  auto paramI = eb->findParameterByID({ 0, VoiceGroup::I });
+  auto paramII = eb->findParameterByID({ 0, VoiceGroup::II });
+
+  WHEN("Parameters I / II have different values")
+  {
+    auto scope = TestHelper::createTestScope();
+    TestHelper::initDualEditBuffer<SoundType::Layer>();
+    paramI->setCPFromHwui(scope->getTransaction(), 0);
+    paramII->setCPFromHwui(scope->getTransaction(), 1);
+
+    THEN("Converted to Split Sound the Values still differ")
+    {
+      eb->undoableConvertToDual(scope->getTransaction(), SoundType::Split);
+      REQUIRE(eb->getType() == SoundType::Split);
+      REQUIRE(paramI->getValue().getRawValue() != paramII->getValue().getRawValue());
+    }
+  }
 }
