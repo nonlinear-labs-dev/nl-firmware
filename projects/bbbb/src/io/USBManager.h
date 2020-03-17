f348852585c0cbcd43881c03a9b6affce150f8a6
schlenkibus <justus.scheil@parteimail.de>
2020-03-17 12:50:59 +0100

    fixup master tune mod scale and usbmanager ub

--- projects/bbbb/src/io/USBManager.h
+++ projects/bbbb/src/io/USBManager.h
@@ -10,10 +10,10 @@ class USBManager
   ~USBManager();
 
  private:
+  std::mutex cv_m;
   std::atomic_bool m_running { true };
   std::thread m_backgroundThread;
   std::condition_variable m_condition;
-  std::mutex cv_m;
 
   static bool usbAvailable();
   static bool updateAvailable();
