e09b25a53c09c48db6c497c8e4e413ddc3da3ace
Henry Hoegelow <henry@hoegelow.com>
2020-03-20 18:31:30 +0100

    fix update folder permission

--- build-tools/create-c15-update/update_scripts/bbb_update.sh
+++ build-tools/create-c15-update/update_scripts/bbb_update.sh
@@ -60,7 +60,8 @@ move_files(){
 update(){
     mkdir /update/BBB/rootfs \
     && gzip -dc /update/BBB/rootfs.tar.gz | tar -C /update/BBB/rootfs -xf - \
-    &&  LD_LIBRARY_PATH=/update/utilities /update/utilities/rsync -cax --exclude 'etc/hostapd.conf' -f 'P update/' --delete /update/BBB/rootfs/ /
+    && LD_LIBRARY_PATH=/update/utilities /update/utilities/rsync -cax --exclude 'etc/hostapd.conf' -f 'P update/' --delete /update/BBB/rootfs/ /
+    && chown -R root.root /update
     if [ $? -ne 0 ]; then report_and_quit "E58 BBB update: Syncing rootfs failed ..." "58"; fi
     rm -rf /update/BBB/rootfs/*
     rm -rf /update/BBB/rootfs
