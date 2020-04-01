a7eac53f4516c5d03873cdbbff8a69c4c871e277
Henry Hoegelow <henry@hoegelow.com>
2020-04-01 16:38:12 +0200

    WIP

--- projects/acceptance-tests/src/VoiceAllocation.cpp
+++ projects/acceptance-tests/src/VoiceAllocation.cpp
@@ -12,38 +12,28 @@ namespace Tests
 
     auto options = createEmptyAudioEngineOptions();
     auto synth = std::make_unique<C15Synth>(options.get());
-    loadTestPreset(synth.get(), "voices-remain-on-preset-load", "1ed29aea-5382-4ab5-b04c-ab853699a1dd");
+    auto preset1 = "dcc9b3d3-009d-4363-a64d-930c95d435a5";
+    auto preset2 = "119284ae-b3d4-42a7-a155-31f04ed340ac";
+    loadTestPreset(synth.get(), "voices-remain-on-preset-load", preset1);
 
     GIVEN("some notes are played without glitch supression")
     {
       synth->getDsp()->onSettingGlitchSuppr(false);
       synth->simulateKeyDown(50);
-      synth->simulateKeyDown(55);
-      synth->simulateKeyDown(60);
+      synth->simulateKeyDown(53);
+      synth->simulateKeyDown(56);
 
       synth->measurePerformance(250ms);
 
-      WHEN("preset 643fe46e-6f9f-4919-82f0-27a6a3aae902 is loaded")
+      WHEN("preset 2 is loaded")
       {
-        loadTestPreset(synth.get(), "voices-remain-on-preset-load", "643fe46e-6f9f-4919-82f0-27a6a3aae902");
+        loadTestPreset(synth.get(), "voices-remain-on-preset-load", preset2);
         synth->measurePerformance(250ms);
 
         THEN("sound is still playing")
         {
           auto result = synth->measurePerformance(250ms);
-          REQUIRE(getMaxLevel(std::get<0>(result)) > 0.25f);
-        }
-      }
-
-      WHEN("preset b590cb36-af0e-4b3f-9088-7a4aca603198 is loaded")
-      {
-        loadTestPreset(synth.get(), "voices-remain-on-preset-load", "b590cb36-af0e-4b3f-9088-7a4aca603198");
-        synth->measurePerformance(250ms);
-
-        THEN("sound is still playing")
-        {
-          auto result = synth->measurePerformance(250ms);
-          REQUIRE(getMaxLevel(std::get<0>(result)) > 0.25f);
+          REQUIRE(getMaxLevel(std::get<0>(result)) > 0.5f);
         }
       }
     }
@@ -52,32 +42,20 @@ namespace Tests
     {
       synth->getDsp()->onSettingGlitchSuppr(true);
       synth->simulateKeyDown(50);
-      synth->simulateKeyDown(55);
-      synth->simulateKeyDown(60);
+      synth->simulateKeyDown(53);
+      synth->simulateKeyDown(56);
 
       synth->measurePerformance(250ms);
 
-      WHEN("preset 643fe46e-6f9f-4919-82f0-27a6a3aae902 is loaded")
+      WHEN("preset 2 is loaded")
       {
-        loadTestPreset(synth.get(), "voices-remain-on-preset-load", "643fe46e-6f9f-4919-82f0-27a6a3aae902");
+        loadTestPreset(synth.get(), "voices-remain-on-preset-load", preset2);
         synth->measurePerformance(250ms);
 
         THEN("sound is still playing")
         {
           auto result = synth->measurePerformance(250ms);
-          REQUIRE(getMaxLevel(std::get<0>(result)) > 0.25f);
-        }
-      }
-
-      WHEN("preset b590cb36-af0e-4b3f-9088-7a4aca603198 is loaded")
-      {
-        loadTestPreset(synth.get(), "voices-remain-on-preset-load", "b590cb36-af0e-4b3f-9088-7a4aca603198");
-        synth->measurePerformance(250ms);
-
-        THEN("sound is still playing")
-        {
-          auto result = synth->measurePerformance(250ms);
-          REQUIRE(getMaxLevel(std::get<0>(result)) > 0.25f);
+          REQUIRE(getMaxLevel(std::get<0>(result)) > 0.5f);
         }
       }
     }
