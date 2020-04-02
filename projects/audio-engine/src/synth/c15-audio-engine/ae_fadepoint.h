2688311555dfe790066e7a189fac6d124bda1c4f
Henry Hoegelow <henry@hoegelow.com>
2020-04-02 13:48:19 +0200

    move recall-methods into UI thread, add spinning wait in UI thread

--- projects/audio-engine/src/synth/c15-audio-engine/ae_fadepoint.h
+++ projects/audio-engine/src/synth/c15-audio-engine/ae_fadepoint.h
@@ -11,29 +11,29 @@
 
 #include <c15_config.h>
 #include <vector>
-#include <atomic>
-
-enum MuteTask
-{
-  MuteTask_Trigger_Tone = 1 << 0,
-  MuteTask_Trigger_Unison = 1 << 1,
-  MuteTask_Trigger_Mono = 1 << 2,
-  MuteTask_Recall_Single = 1 << 3,
-  MuteTask_Recall_Split = 1 << 4,
-  MuteTask_Recall_Layer = 1 << 5
-};
 
 class atomic_fade_table
 {
  public:
-  std::atomic<uint32_t> m_muteTasks = 0;
   atomic_fade_table();
   void init(const float _samplerate);
-  bool evalTaskStatus();
-  void setTask(const MuteTask _task);
+  void evalTaskStatus();
+
+  template <typename Callable> void muteAndDo(Callable &&c)
+  {
+    m_shouldMute = true;
+    waitForFadeSpinning();
+    c();
+    m_shouldMute = false;
+  }
+
   float getValue();
+  bool isMuted() const;
+
+  void waitForFadeSpinning() const;
 
  private:
   uint32_t m_currentMuteRampIndex = 0, m_finalMuteRampIndex = 0;
   std::vector<float> m_data;
+  bool m_shouldMute = false;
 };
