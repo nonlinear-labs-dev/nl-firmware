99edc212f784d167c2ee909b974f66bc2d4b27cf
Henry Hoegelow <henry@hoegelow.com>
2020-04-23 10:36:05 +0200

    Split the OTA updates into pieces for each processor to increase development speed. Use static paths to build artifacts wherever appropriate to improve stability and readability.

--- build-tools/create-c15-update/create-c15-update.sh
+++ build-tools/create-c15-update/create-c15-update.sh
@@ -2,18 +2,37 @@
 #
 # Author:       Anton Scmied
 # Date:         12.02.2020
-# TODO:         - deploy lpc update
-
 # vom Cmake übergebene Pfade zu den .tarS
+
 EPC_UPDATE=$1
 BBB_UPDATE=$2
 LPC_CORE_0=$3
 LPC_CORE_1=$4
 BINARY_DIR=$5
-SOURCE_DIR=$6/create-c15-update
-OUT_DIRECTORY=$BINARY_DIR/nonlinear-c15-update
+SOURCE_DIR=$6/build-tools/create-c15-update
+OUTNAME=$7
+ASPECTS=$8
+OUT_DIRECTORY=$BINARY_DIR/$OUTNAME
+OUT_TAR=$BINARY_DIR/$OUTNAME.tar
 
+UPDATE_BBB=0
+UPDATE_LPC=0
+UPDATE_EPC=0
 
+if [[ $ASPECTS = *epc* ]]
+then
+    UPDATE_EPC=1
+fi
+
+if [[ $ASPECTS = *lpc* ]]
+then
+    UPDATE_LPC=1
+fi
+
+if [[ $ASPECTS = *bbb* ]]
+then
+    UPDATE_BBB=1
+fi
 
 fail_and_exit() {
     echo "Failed to created an update!"
@@ -21,94 +40,105 @@ fail_and_exit() {
 }
 
 check_preconditions () {
-    if [ -z "$EPC_UPDATE" ]; then echo "ePC update missing..." && return 1; fi
-    if [ -z "$BBB_UPDATE" ]; then echo "BBB update missing..." && return 1; fi
-    if [ -z "$LPC_CORE_0" ]; then echo "LPC update missing..." && return 1; fi
-    if [ -z "$LPC_CORE_1" ]; then echo "LPC update missing..." && return 1; fi
+    if [ -z "$EPC_UPDATE" -a $UPDATE_EPC == 1 ]; then echo "ePC update missing..." && return 1; fi
+    if [ -z "$BBB_UPDATE" -a $UPDATE_BBB == 1 ]; then echo "BBB update missing..." && return 1; fi
+    if [ -z "$LPC_CORE_0" -a $UPDATE_LPC == 1 ]; then echo "LPC update missing..." && return 1; fi
+    if [ -z "$LPC_CORE_1" -a $UPDATE_LPC == 1 ]; then echo "LPC update missing..." && return 1; fi
     return 0
 }
 
 create_update_file_structure() {
     echo "Creating Update Structure..."
-    if mkdir -p $OUT_DIRECTORY \
-        && mkdir -p $OUT_DIRECTORY/BBB \
-        && mkdir -p $OUT_DIRECTORY/EPC \
-        && mkdir -p $OUT_DIRECTORY/LPC \
-        && mkdir -p $OUT_DIRECTORY/utilities; then
-        echo "Creating Update Structure done."
-        return 0
-    fi
-    echo "Creating Update Structure failed."
-    return 1
+    mkdir -p $OUT_DIRECTORY || fail_and_exit
+    if [ $UPDATE_BBB == 1 ]; then mkdir -p $OUT_DIRECTORY/BBB || fail_and_exit; fi
+    if [ $UPDATE_EPC == 1 ]; then mkdir -p $OUT_DIRECTORY/EPC || fail_and_exit; fi
+    if [ $UPDATE_LPC == 1 ]; then mkdir -p $OUT_DIRECTORY/LPC || fail_and_exit; fi
+    mkdir -p $OUT_DIRECTORY/utilities || fail_and_exit
+    echo "Creating Update Structure done."
+    return 0
 }
 
 deploy_updates() {
     echo "Deploying updates..."
-    if cp $EPC_UPDATE $OUT_DIRECTORY/EPC/update.tar \
-        && chmod 666 $OUT_DIRECTORY/EPC/update.tar \
-        && cp $BBB_UPDATE $OUT_DIRECTORY/BBB/ \
-        && chmod 666 $OUT_DIRECTORY/BBB/rootfs.tar.gz \
-        && cp $LPC_CORE_0 $OUT_DIRECTORY/LPC/M0_project.bin \
-        && chmod 666 $OUT_DIRECTORY/LPC/M0_project.bin \
-        && cp $LPC_CORE_1 $OUT_DIRECTORY/LPC/M4_project.bin \
-        && chmod 666 $OUT_DIRECTORY/LPC/M4_project.bin; then
-        echo "Deploying updates done."
-        return 0
-     fi
-     echo echo "Deploying updates failed."
-     return 1
+
+    if [ $UPDATE_BBB == 1 ]; then
+        cp $BBB_UPDATE $OUT_DIRECTORY/BBB/ && chmod 666 $OUT_DIRECTORY/BBB/rootfs.tar.gz || fail_and_exit;
+    fi
+
+    if [ $UPDATE_EPC == 1 ]; then
+        cp $EPC_UPDATE $OUT_DIRECTORY/EPC/update.tar && chmod 666 $OUT_DIRECTORY/EPC/update.tar || fail_and_exit;
+    fi
+
+    if [ $UPDATE_LPC == 1 ]; then
+        cp $LPC_CORE_0 $OUT_DIRECTORY/LPC/M0_project.bin && chmod 666 $OUT_DIRECTORY/LPC/M0_project.bin || fail_and_exit;
+        cp $LPC_CORE_1 $OUT_DIRECTORY/LPC/M4_project.bin && chmod 666 $OUT_DIRECTORY/LPC/M4_project.bin || fail_and_exit;
+    fi
+
+    echo "Deploying updates done."
+    return 0
 }
 
 deploy_scripts() {
     echo "Deploying update scripts..."
-    if cp $SOURCE_DIR/update_scripts/run.sh $OUT_DIRECTORY/ \
-        && chmod 777 $OUT_DIRECTORY/run.sh \
-        && cp $SOURCE_DIR/update_scripts/bbb_update.sh $OUT_DIRECTORY/BBB/ \
-        && chmod 777 $OUT_DIRECTORY/BBB/bbb_update.sh \
-        && cp $SOURCE_DIR/update_scripts/epc_pull_update.sh $OUT_DIRECTORY/EPC/ \
-        && chmod 777 $OUT_DIRECTORY/EPC/epc_pull_update.sh \
-        && cp $SOURCE_DIR/update_scripts/epc_push_update.sh $OUT_DIRECTORY/EPC/ \
-        && chmod 777 $OUT_DIRECTORY/EPC/epc_push_update.sh \
-        && cp $SOURCE_DIR/update_scripts/lpc_update.sh $OUT_DIRECTORY/LPC/ \
-        && chmod 777 $OUT_DIRECTORY/LPC/lpc_update.sh; then
-        echo "Deploying update scripts done."
 
-        echo "Creating run.sh dummys..."
-        if [ -e $OUT_DIRECTORY/run_v2.sh ] && [ -L $OUT_DIRECTORY/run_v2.sh ]; then
-            rm -f $OUT_DIRECTORY/run_v2.sh
-        fi
-        if [ -e $OUT_DIRECTORY/run_v3.sh ] && [ -L $OUT_DIRECTORY/run_v3.sh ]; then
-            rm -f $OUT_DIRECTORY/run_v3.sh
-        fi
-        touch $OUT_DIRECTORY/run_v2.sh && chmod 777 $OUT_DIRECTORY/run_v2.sh
-        touch $OUT_DIRECTORY/run_v3.sh && chmod 777 $OUT_DIRECTORY/run_v3.sh
-        echo "Creating run.sh dummys done"
+    cp $SOURCE_DIR/update_scripts/run.sh $OUT_DIRECTORY/ && chmod 777 $OUT_DIRECTORY/run.sh || fail_and_exit;
+    sed -i "s/ASPECTS=\"TO_BE_REPLACED\"/ASPECTS=\"$ASPECTS\"/g" $OUT_DIRECTORY/run.sh
 
-        return 0
-     fi
-     echo echo "Deploying update scripts failed."
-     return 1
+    if [ $UPDATE_BBB == 1 ]; then
+        cp $SOURCE_DIR/update_scripts/bbb_update.sh $OUT_DIRECTORY/BBB/ && \
+            chmod 777 $OUT_DIRECTORY/BBB/bbb_update.sh || \
+            fail_and_exit;
+    fi
+
+    if [ $UPDATE_EPC == 1 ]; then
+        cp $SOURCE_DIR/update_scripts/epc_pull_update.sh $OUT_DIRECTORY/EPC/ && \
+            chmod 777 $OUT_DIRECTORY/EPC/epc_pull_update.sh && \
+            cp $SOURCE_DIR/update_scripts/epc_push_update.sh $OUT_DIRECTORY/EPC/ && \
+            chmod 777 $OUT_DIRECTORY/EPC/epc_push_update.sh || \
+            fail_and_exit;
+    fi
+
+    if [ $UPDATE_LPC == 1 ]; then
+        cp $SOURCE_DIR/update_scripts/lpc_update.sh $OUT_DIRECTORY/LPC/ && \
+            chmod 777 $OUT_DIRECTORY/LPC/lpc_update.sh || \
+            fail_and_exit;
+    fi
+
+    echo "Deploying update scripts done."
+
+    echo "Creating run.sh dummys..."
+    if [ -e $OUT_DIRECTORY/run_v2.sh ] && [ -L $OUT_DIRECTORY/run_v2.sh ]; then
+        rm -f $OUT_DIRECTORY/run_v2.sh
+    fi
+    if [ -e $OUT_DIRECTORY/run_v3.sh ] && [ -L $OUT_DIRECTORY/run_v3.sh ]; then
+        rm -f $OUT_DIRECTORY/run_v3.sh
+    fi
+    touch $OUT_DIRECTORY/run_v2.sh && chmod 777 $OUT_DIRECTORY/run_v2.sh
+    touch $OUT_DIRECTORY/run_v3.sh && chmod 777 $OUT_DIRECTORY/run_v3.sh
+    echo "Creating run.sh dummys done"
+
+    return 0
 }
 
 get_tools_from_rootfs() {
     echo "Getting tools from rootfs..."
-    mkdir -p $BINARY_DIR/rootfs && tar -xf $BBB_UPDATE -C $BINARY_DIR/rootfs
+    mkdir -p $BINARY_DIR/build-tools/bbb/rootfs && tar -xf $BBB_UPDATE -C $BINARY_DIR/build-tools/bbb/rootfs
 
     for i in sshpass text2soled rsync socat thttpd; do
-        if ! cp $(find $BINARY_DIR/rootfs/usr -type f -name "$i") $OUT_DIRECTORY/utilities/; then
+        if ! cp $(find $BINARY_DIR/build-tools/bbb/rootfs/usr -type f -name "$i") $OUT_DIRECTORY/utilities/; then
           echo "could not get $i from rootfs"
           return 1
         fi
     done
 
     for lib in libpopt.so.0.0.0 libpopt.so.0 libpopt.so; do
-        if ! cp $(find $BINARY_DIR/rootfs/lib/ -name "$lib") $OUT_DIRECTORY/utilities/; then
+        if ! cp $(find $BINARY_DIR/build-tools/bbb/rootfs/lib/ -name "$lib") $OUT_DIRECTORY/utilities/; then
                echo "could not get library $lib from rootfs"
           return 1
         fi
     done
 
-    cp -R $BINARY_DIR/rootfs/usr/C15/text2soled/resources $OUT_DIRECTORY/utilities/
+    cp -R $BINARY_DIR/build-tools/bbb/rootfs/usr/C15/text2soled/resources $OUT_DIRECTORY/utilities/
 
     echo "Getting tools from rootfs done."
     return 0
@@ -116,13 +146,13 @@ get_tools_from_rootfs() {
 
 
 create_update_tar () {
-    echo "Creating nonlinear-c15-update.tar..."
+    echo "Creating $OUT_TAR..."
     rm -rf $OUT_DIRECTORY/../nonlinear-c15-update.tar
-    if cd $OUT_DIRECTORY/ && tar -cf ../nonlinear-c15-update.tar * ; then
-        echo "Creating nonlinear-c15-update.tar done."
+    if cd $OUT_DIRECTORY/ && tar -cf $OUT_TAR * ; then
+        echo "Creating $OUT_TAR done."
         return 0
     fi
-    echo "Creating nonlinear-c15-update.tar failed."
+    echo "Creating $OUT_TAR failed."
     return 1
 }
 
