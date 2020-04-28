19625332564b72f09354cf6d41ab92c33eec6780
schlenkibus <justus.scheil@parteimail.de>
2020-04-28 11:28:34 +0200

    fix splitpoint macro assignemts via webui

--- projects/NonMaps/src/main/java/com/nonlinearlabs/client/world/maps/parameters/VoiceGroupMaster/VoiceGroupMasterColumn.java
+++ projects/NonMaps/src/main/java/com/nonlinearlabs/client/world/maps/parameters/VoiceGroupMaster/VoiceGroupMasterColumn.java
@@ -2,6 +2,7 @@ package com.nonlinearlabs.client.world.maps.parameters.VoiceGroupMaster;
 
 import com.nonlinearlabs.client.world.maps.MapsLayout;
 import com.nonlinearlabs.client.world.maps.parameters.ModulatableParameter;
+import com.nonlinearlabs.client.world.maps.parameters.ModulationSourceSlider;
 import com.nonlinearlabs.client.world.maps.parameters.NumericalControlSmall;
 import com.nonlinearlabs.client.world.maps.parameters.Parameter;
 import com.nonlinearlabs.client.world.maps.parameters.ParameterColumn;
@@ -30,28 +31,13 @@ public class VoiceGroupMasterColumn extends ParameterColumn {
         }
     }
 
-    private class SplitPoint extends ModulatableParameter {
-        private SplitPoint(MapsLayout parent) {
-            super(parent, 356);
-            addChild(new SliderHorizontal(this, 356) {
-                @Override 
-                public double getTopMargin() {
-                    if(presenter.hidden)
-                        return 0;
-                    return 20;
-                }
-            });
-            addChild(new ValueDisplayLarge(this, 356));
-        }
-    }
-
     public VoiceGroupMasterColumn(MapsLayout parent) {
         super(parent);
         addChild(new Level(this));
         addChild(new UnmodulateablePartMasterParameter(this, 360));
         addChild(new UnmodulateablePartMasterParameter(this, 396));
         addChild(new UnmodulateablePartMasterParameter(this, 397));
-        addChild(new SplitPoint(this));
+        addChild(new ModulationSourceSlider(this, 356));
     }
 
 }
