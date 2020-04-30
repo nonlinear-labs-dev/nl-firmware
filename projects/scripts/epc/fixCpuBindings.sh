7700904d6bdb58d89bf0e7885f1f641943c12d02
Henry Hoegelow <henry@hoegelow.com>
2020-04-30 08:33:44 +0200

    fix missing quotes for grub command line

--- projects/scripts/epc/fixCpuBindings.sh
+++ projects/scripts/epc/fixCpuBindings.sh
@@ -8,7 +8,7 @@ else
     mkdir -p /mnt/sda2
     mount /dev/sda2 /mnt/sda2
     mount /dev/sda1 /mnt/sda2/boot
-    arch-chroot /mnt/sda2 /bin/bash -c "sed -i s/GRUB_CMDLINE_LINUX_DEFAULT.*$/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet ip=192.168.10.10:::::eth0:none isolcpus=0,2\"/g /etc/default/grub"
+    arch-chroot /mnt/sda2 /bin/bash -c "sed -i s/GRUB_CMDLINE_LINUX_DEFAULT.*$/GRUB_CMDLINE_LINUX_DEFAULT=\\\"quiet ip=192.168.10.10:::::eth0:none isolcpus=0,2\\\"/g /etc/default/grub"
     arch-chroot /mnt/sda2 /bin/bash -c "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck"
     arch-chroot /mnt/sda2 /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
     arch-chroot /mnt/sda2 /bin/bash -c "mkdir -p /boot/EFI/BOOT"
