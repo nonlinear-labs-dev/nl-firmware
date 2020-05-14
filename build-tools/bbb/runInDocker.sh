95b5019a39eaf454ac19da8c123494e54a3820de
Henry Hoegelow <henry@hoegelow.com>
2020-05-14 16:48:36 +0200

    import host ssh keys into docker

--- build-tools/bbb/runInDocker.sh
+++ build-tools/bbb/runInDocker.sh
@@ -7,13 +7,23 @@ shift
 DOCKER_ARGS="$1"
 shift
 
+SSH_DIR=$(realpath "$HOME/.ssh")
+
 TMPSCRIPT=$(mktemp)
 TMPSCRIPTBASE=$(basename $TMPSCRIPT)
 TMPDIRNAME="$BINARY_DIR/tmp"
 TMPSCRIPT="$TMPDIRNAME/$TMPSCRIPTBASE"
 mkdir -p $TMPDIRNAME
 
-echo "" > $TMPSCRIPT
+# COPY local ssh files into the docker container to be able to push onto github
+echo "
+mkdir -p /docker-ssh
+rsync -a /host-ssh/ /docker-ssh
+chown -R root /docker-ssh
+if ! grep \"/docker-ssh\" /etc/ssh/ssh_config; then
+ echo \"IdentityFile /docker-ssh/id_rsa\" >> /etc/ssh/ssh_config
+fi
+" > $TMPSCRIPT
 
 for var in "$@"
 do
@@ -21,4 +31,4 @@ do
 done
 
 DOCKERNAME="nl-cross-build-environment"
-docker run $DOCKER_ARGS --privileged --rm -v $TMPDIRNAME:/script -v $BINARY_DIR:/workdir -v $SOURCE_DIR:/sources $DOCKERNAME bash /script/$TMPSCRIPTBASE
+docker run $DOCKER_ARGS --privileged --rm -v $SSH_DIR:/host-ssh -v $TMPDIRNAME:/script -v $BINARY_DIR:/workdir -v $SOURCE_DIR:/sources $DOCKERNAME bash /script/$TMPSCRIPTBASE
