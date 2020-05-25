149708b9d84dc2412761a8680637fe2e4b53e446
Anton Schmied <aschmied@localhost.localdomain>
2020-05-25 11:43:00 +0200

    added journal exclusion to update

--- projects/scripts/bbb/install-update.sh
+++ projects/scripts/bbb/install-update.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-rm -rf /update
-mkdir -p /update
-
 if [ -e /mnt/usb-stick/nonlinear-c15-update.tar ]
 then
-	cp /mnt/usb-stick/nonlinear-c15-update.tar /update		
-	cd /update
-	tar xf nonlinear-c15-update.tar
-	mv /mnt/usb-stick/nonlinear-c15-update.tar /mnt/usb-stick/nonlinear-c15-update.tar-copied
-	chmod +x /update/run.sh
-	/bin/sh /update/run.sh
+    rm -rf /update/*
+    cp /mnt/usb-stick/nonlinear-c15-update.tar /update
+    cd /update
+    tar xf nonlinear-c15-update.tar
+    mv /mnt/usb-stick/nonlinear-c15-update.tar /mnt/usb-stick/nonlinear-c15-update.tar-copied
+    chmod +x /update/run.sh
+    /bin/sh /update/run.sh
 fi
 
