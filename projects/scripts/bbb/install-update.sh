bf9ac0ca42ac501cd37c7b74471ae6e745833876
Anton Schmied <aschmied@localhost.localdomain>
2020-05-25 15:16:50 +0200

    rm and mkdir /update as it used to be. added check: only rename the update file and start run.sh if previous steps were successful

--- projects/scripts/bbb/install-update.sh
+++ projects/scripts/bbb/install-update.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-if [ -e /mnt/usb-stick/nonlinear-c15-update.tar ]
-then
-    rm -rf /update/*
-    cp /mnt/usb-stick/nonlinear-c15-update.tar /update
-    cd /update
-    tar xf nonlinear-c15-update.tar
-    mv /mnt/usb-stick/nonlinear-c15-update.tar /mnt/usb-stick/nonlinear-c15-update.tar-copied
-    chmod +x /update/run.sh
-    /bin/sh /update/run.sh
+rm -rf /update
+mkdir -p /update
+
+if [ -e /mnt/usb-stick/nonlinear-c15-update.tar ]; then
+    cp /mnt/usb-stick/nonlinear-c15-update.tar /update \
+    && cd /update \
+    && tar xf nonlinear-c15-update.tar
+
+    if [ $? -eq 0 ]; then
+        mv /mnt/usb-stick/nonlinear-c15-update.tar /mnt/usb-stick/nonlinear-c15-update.tar-copied
+        chmod +x /update/run.sh
+        /bin/sh /update/run.sh
+    fi
 fi
 
