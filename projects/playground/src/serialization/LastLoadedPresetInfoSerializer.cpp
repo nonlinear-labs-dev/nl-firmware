9ad9079003ea2e541c7dd3d5f738a1646b7f0b29
schlenkibus <justus.scheil@parteimail.de>
2020-05-27 14:54:26 +0200

    hotfix serialize preset part origin

--- projects/playground/src/serialization/LastLoadedPresetInfoSerializer.cpp
+++ projects/playground/src/serialization/LastLoadedPresetInfoSerializer.cpp
@@ -22,6 +22,11 @@ void LastLoadedPresetInfoSerializer::writeTagContent(Writer &writer) const
   writer.writeTextElement("loaded-presets-name", m_editBuffer->getName());
   writer.writeTextElement("voice-group-label-I", m_editBuffer->getVoiceGroupName(VoiceGroup::I));
   writer.writeTextElement("voice-group-label-II", m_editBuffer->getVoiceGroupName(VoiceGroup::II));
+
+  writer.writeTextElement("origin-I", m_editBuffer->getAttribute("origin-I", ""));
+  writer.writeTextElement("origin-I-vg", m_editBuffer->getAttribute("origin-I-vg", ""));
+  writer.writeTextElement("origin-II", m_editBuffer->getAttribute("origin-II", ""));
+  writer.writeTextElement("origin-II-vg", m_editBuffer->getAttribute("origin-II-vg", ""));
 }
 
 void LastLoadedPresetInfoSerializer::readTagContent(Reader &reader) const
@@ -30,4 +35,20 @@ void LastLoadedPresetInfoSerializer::readTagContent(Reader &reader) const
   reader.loadTextElement("loaded-presets-name", m_editBuffer->m_name);
   reader.loadTextElement("voice-group-label-I", m_editBuffer->m_voiceGroupLabels[0]);
   reader.loadTextElement("voice-group-label-II", m_editBuffer->m_voiceGroupLabels[1]);
+
+  reader.onTextElement("origin-I", [&](auto text, auto) mutable {
+    m_editBuffer->setAttribute(reader.getTransaction(), "origin-I", text);
+  });
+
+  reader.onTextElement("origin-II", [&](auto text, auto) mutable {
+    m_editBuffer->setAttribute(reader.getTransaction(), "origin-II", text);
+  });
+
+  reader.onTextElement("origin-I-vg", [&](auto text, auto) mutable {
+    m_editBuffer->setAttribute(reader.getTransaction(), "origin-I-vg", text);
+  });
+
+  reader.onTextElement("origin-II-vg", [&](auto text, auto) mutable {
+    m_editBuffer->setAttribute(reader.getTransaction(), "origin-II-vg", text);
+  });
 }
