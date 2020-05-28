10c6e610fc844d519167d5cc9f3dbba00fe965e2
Anton Schmied <aschmied@localhost.localdomain>
2020-05-28 12:43:24 +0200

    added check for 1.5 update, was soll der Geiz

--- projects/scripts/bbb/install-update.sh
+++ projects/scripts/bbb/install-update.sh
@@ -7,7 +7,9 @@ if [ -e /mnt/usb-stick/nonlinear-c15-update.tar ]; then
         && cd /update \
         && tar xf nonlinear-c15-update.tar; then
             mv /mnt/usb-stick/nonlinear-c15-update.tar /mnt/usb-stick/nonlinear-c15-update.tar-copied
-            if ! ls -l /update/BBB/ | grep bbb_update_1811a.sh && ! ls -l /update/BBB/ | grep bbb_update_1905a.sh; then
+            if ! ls -l /update/BBB/ | grep bbb_update_1811a.sh \
+                && ! ls -l /update/BBB/ | grep bbb_update_1905a.sh \
+                && ! ls -l /update/BBB/ | grep playground-RELEASE-2019-05-1.5-706569a; then
                 chmod +x /update/run.sh
                 /bin/sh /update/run.sh
             fi
