3a515b418da40aba623d676e341943550d1464a8
Anton Schmied <aschmied@localhost.localdomain>
2020-05-28 12:31:24 +0200

    added old version check to intall_update.sh this will prevent the C15 (running version 1.7) from even executing run.sh if old files are found under /update/BBB/

--- projects/scripts/bbb/install-update.sh
+++ projects/scripts/bbb/install-update.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 if [ -e /mnt/usb-stick/nonlinear-c15-update.tar ]; then
-    rm -rf /update \
-    && mkdir -p /update \
-    && cp /mnt/usb-stick/nonlinear-c15-update.tar /update \
-    && cd /update \
-    && tar xf nonlinear-c15-update.tar
-
-    if [ $? -eq 0 ]; then
-        mv /mnt/usb-stick/nonlinear-c15-update.tar /mnt/usb-stick/nonlinear-c15-update.tar-copied
-        chmod +x /update/run.sh
-        /bin/sh /update/run.sh
+    if rm -rf /update \
+        && mkdir -p /update \
+        && cp /mnt/usb-stick/nonlinear-c15-update.tar /update \
+        && cd /update \
+        && tar xf nonlinear-c15-update.tar; then
+            mv /mnt/usb-stick/nonlinear-c15-update.tar /mnt/usb-stick/nonlinear-c15-update.tar-copied
+            if ! ls -l /update/BBB/ | grep bbb_update_1811a.sh && ! ls -l /update/BBB/ | grep bbb_update_1905a.sh; then
+                chmod +x /update/run.sh
+                /bin/sh /update/run.sh
+            fi
     fi
 fi
