310dc0949db9e933853c828db6c5078ef0263950
Henry Hoegelow <henry@hoegelow.com>
2020-06-04 13:04:50 +0200

    fix #1893

--- projects/NonMaps/src/main/java/com/nonlinearlabs/client/world/maps/parameters/EnvC/EnvCExtraBlock.java
+++ projects/NonMaps/src/main/java/com/nonlinearlabs/client/world/maps/parameters/EnvC/EnvCExtraBlock.java
@@ -4,8 +4,21 @@ import com.nonlinearlabs.client.world.Rect;
 import com.nonlinearlabs.client.world.maps.MapsLayout;
 import com.nonlinearlabs.client.world.maps.parameters.ModulationSourceSlider;
 import com.nonlinearlabs.client.world.maps.parameters.ParameterGroupControls;
+import com.nonlinearlabs.client.world.maps.parameters.Spacer;
 
-class EnvCLevelVelLevelKTTimeKT extends ParameterGroupControls {
+class EnvCExtraBlock extends ParameterGroupControls {
+
+	private class Retrigger extends ModulationSourceSlider {
+
+		private Retrigger(MapsLayout parent) {
+			super(parent, 340);
+		}
+
+		@Override
+		protected int getBackgroundRoundings() {
+			return Rect.ROUNDING_ALL;
+		}
+	}
 
 	private class LvlVel extends ModulationSourceSlider {
 
@@ -43,8 +56,10 @@ class EnvCLevelVelLevelKTTimeKT extends ParameterGroupControls {
 		}
 	}
 
-	EnvCLevelVelLevelKTTimeKT(MapsLayout parent) {
+	EnvCExtraBlock(MapsLayout parent) {
 		super(parent);
+		addChild(new Retrigger(this));
+		addChild(new Spacer(this, 20, 1));
 		addChild(new LvlVel(this));
 		addChild(new LvlKT(this));
 		addChild(new TimeKT(this));
