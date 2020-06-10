db195b36d89e3be8339b2cec26575db4ff408b16
schlenkibus <justus.scheil@parteimail.de>
2020-06-10 17:27:22 +0200

    fixup roundings in voices group closes #1954

--- projects/NonMaps/src/main/java/com/nonlinearlabs/client/world/maps/parameters/MonoAndUnison/UnisonColumn.java
+++ projects/NonMaps/src/main/java/com/nonlinearlabs/client/world/maps/parameters/MonoAndUnison/UnisonColumn.java
@@ -1,5 +1,6 @@
 package com.nonlinearlabs.client.world.maps.parameters.MonoAndUnison;
 
+import com.nonlinearlabs.client.world.Rect;
 import com.nonlinearlabs.client.world.maps.parameters.ModulationSourceSlider;
 import com.nonlinearlabs.client.world.maps.parameters.ParameterColumn;
 import com.nonlinearlabs.client.world.maps.parameters.ParameterGroup;
@@ -9,7 +10,12 @@ public class UnisonColumn extends ParameterColumn {
       
     public UnisonColumn(ParameterGroup parent) {
         super(parent);
-        addChild(new SmallModulateableParameter(this, 249));
+        addChild(new SmallModulateableParameter(this, 249) {
+            @Override
+            protected int getBackgroundRoundings() {
+                return Rect.ROUNDING_NONE;
+            }
+        });
         addChild(new SmallModulateableParameter(this, 250));
         addChild(new ModulationSourceSlider(this, 252));
         addChild(new ModulationSourceSlider(this, 253));
