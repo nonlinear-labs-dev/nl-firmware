f4d6a5c7e5d7069e5ca89c9cad7a0a5c6014e11c
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2020-06-11 13:29:39 +0200

    update "lpc" tool : managed LPC reset
    - LPC can now be force-reset via the GPIO line.
    - After reset, a check is done after one second if the LPC is up by checking if it sent anything.
    - A number of retries can be specified.
    - Return value of the lpc tool reflects the up(==0) or down(==1) state of the LPC.
    
    Note : Make sure the bridge (bbbb) is not running, it will steal the messages otherwise and the tool will report false "lpc down" events.

--- projects/realtime-system/supplements/native-bbbb/lpc/mk.bat
+++ projects/realtime-system/supplements/native-bbbb/lpc/mk.bat
@@ -1,1 +1,1 @@
-C:\gcc-linaro-7.5.0-2019.12-i686-mingw32_arm-linux-gnueabihf\bin\arm-linux-gnueabihf-gcc.exe -Os -s -Wall -Wextra -I../../../.. %1.c -o %1
+C:\gcc-linaro-7.5.0-2019.12-i686-mingw32_arm-linux-gnueabihf\bin\arm-linux-gnueabihf-gcc.exe -Os -s -Wall -Wextra -I../../../.. %1.c linuxgpio.c -o %1
