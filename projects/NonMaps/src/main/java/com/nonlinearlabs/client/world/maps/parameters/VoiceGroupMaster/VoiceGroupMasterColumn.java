b271056ff5568ddc57d3a91d4c6082fe8ada72c5
Henry Hoegelow <henry@hoegelow.com>
2020-06-11 21:08:09 +0200

    fix #1968

--- projects/NonMaps/src/main/java/com/nonlinearlabs/client/world/maps/parameters/VoiceGroupMaster/VoiceGroupMasterColumn.java
+++ projects/NonMaps/src/main/java/com/nonlinearlabs/client/world/maps/parameters/VoiceGroupMaster/VoiceGroupMasterColumn.java
@@ -1,5 +1,8 @@
 package com.nonlinearlabs.client.world.maps.parameters.VoiceGroupMaster;
 
+import com.nonlinearlabs.client.dataModel.editBuffer.EditBufferModel.SoundType;
+import com.nonlinearlabs.client.presenters.EditBufferPresenterProvider;
+import com.nonlinearlabs.client.world.Rect;
 import com.nonlinearlabs.client.world.maps.MapsLayout;
 import com.nonlinearlabs.client.world.maps.parameters.ModulatableHorizontalSlider;
 import com.nonlinearlabs.client.world.maps.parameters.ModulatableParameterWithoutName;
@@ -27,7 +30,14 @@ public class VoiceGroupMasterColumn extends ParameterColumn {
         addChild(new MasterVolume(this, 358));
         addChild(new SmallModulateableParameter(this, 360));
         addChild(new SmallModulateableParameter(this, 396));
-        addChild(new SmallModulateableParameter(this, 397));
+        addChild(new SmallModulateableParameter(this, 397) {
+            @Override
+            protected int getBackgroundRoundings() {
+                if (EditBufferPresenterProvider.getPresenter().soundType == SoundType.Layer)
+                    return Rect.ROUNDING_BOTTOM;
+                return super.getBackgroundRoundings();
+            }
+        });
         addChild(new ModulatableHorizontalSlider(this, 356));
     }
 
