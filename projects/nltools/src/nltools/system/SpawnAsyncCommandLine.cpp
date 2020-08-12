cbed01d00550db82e715931dc7547ac2830c0d5b
schlenkibus <justus.scheil@parteimail.de>
2020-08-12 12:30:36 +0200

    wip

--- projects/nltools/src/nltools/system/SpawnAsyncCommandLine.cpp
+++ projects/nltools/src/nltools/system/SpawnAsyncCommandLine.cpp
@@ -7,8 +7,7 @@ SpawnAsyncCommandLine::SpawnAsyncCommandLine(const std::string& command)
   nltools::Log::warning(__LINE__, __PRETTY_FUNCTION__, command);
   m_thread = std::thread([&]() {
     nltools::Log::warning("Spawning Async Command", command);
-    SpawnCommandLine cmd(command);
-    nltools::Log::warning("Async Command", command, "out:", cmd.getStdOutput(), "err:", cmd.getStdError());
+    system(command.c_str());
   });
 }
 
