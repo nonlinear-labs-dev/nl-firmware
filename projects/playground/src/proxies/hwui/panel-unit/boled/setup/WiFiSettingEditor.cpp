992d4a5612634ca9f78b155d8a987bfe00cfeb1b
Henry Hoegelow <henry@hoegelow.com>
2020-08-13 19:16:58 +0200

    WIP

--- projects/playground/src/proxies/hwui/panel-unit/boled/setup/WiFiSettingEditor.cpp
+++ projects/playground/src/proxies/hwui/panel-unit/boled/setup/WiFiSettingEditor.cpp
@@ -3,6 +3,10 @@
 #include "WiFiSettingEditor.h"
 #include <device-settings/Settings.h>
 
+#warning("known problems)
+// - when switching, the current poll might return with the wrong value, fixing itself only after ip to 10 sec
+// - after update with disabled wifi, the masking link in /etc will be removed, thus activating the wifi
+
 WiFiSettingEditor::WiFiSettingEditor()
 {
   Application::get().getSettings()->getSetting<WifiSetting>()->onChange(
