5d9c0de723ff6ddb43acdaff1bc03b7d4194ffe5
schlenkibus <justus.scheil@parteimail.de>
2020-08-24 14:14:05 +0200

    added layer exeption in preset::writeDiff, closes #2143

--- projects/playground/src/presets/Preset.cpp
+++ projects/playground/src/presets/Preset.cpp
@@ -406,9 +406,32 @@ void Preset::writeGroups(Writer &writer, const Preset *other, VoiceGroup vgOfThi
 {
   std::vector<std::string> writtenGroups;
 
+  auto isSpecialLayerGroup = [](GroupId id) { return id.getName() == "Unison" || id.getName() == "Mono"; };
+
   for(auto &g : m_parameterGroups[static_cast<size_t>(vgOfThis)])
   {
-    g.second->writeDiff(writer, g.first, other->findParameterGroup({ g.first.getName(), vgOfOther }));
+    PresetParameterGroup *myGroup = nullptr;
+    PresetParameterGroup *otherGroup = nullptr;
+
+    if(getType() == SoundType::Layer && isSpecialLayerGroup(g.first))
+    {
+      myGroup = findParameterGroup({ g.first.getName(), VoiceGroup::I });
+    }
+    else
+    {
+      myGroup = g.second.get();
+    }
+
+    if(other->getType() == SoundType::Layer && isSpecialLayerGroup(g.first))
+    {
+      otherGroup = other->findParameterGroup({ g.first.getName(), VoiceGroup::I });
+    }
+    else
+    {
+      otherGroup = other->findParameterGroup({ g.first.getName(), vgOfOther });
+    }
+
+    myGroup->writeDiff(writer, g.first, otherGroup);
     writtenGroups.emplace_back(g.first.getName());
   }
 
