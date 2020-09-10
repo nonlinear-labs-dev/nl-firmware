b820d85668a67e382b0d3f8fefe2d20873715436
schlenkibus <justus.scheil@parteimail.de>
2020-09-10 11:34:48 +0200

    first round of redesigns #1932

--- projects/epc/web-ui/src/main/java/com/nonlinearlabs/client/world/maps/parameters/Cabinet/TiltColumn.java
+++ projects/epc/web-ui/src/main/java/com/nonlinearlabs/client/world/maps/parameters/Cabinet/TiltColumn.java
@@ -1,13 +1,19 @@
 package com.nonlinearlabs.client.world.maps.parameters.Cabinet;
 
+import com.nonlinearlabs.client.world.Rect;
 import com.nonlinearlabs.client.world.maps.MapsLayout;
-import com.nonlinearlabs.client.world.maps.parameters.ModulateableKnob;
+import com.nonlinearlabs.client.world.maps.parameters.ModulateableKnobWithSlidersHeight;
 import com.nonlinearlabs.client.world.maps.parameters.ParameterColumn;
 
 class TiltColumn extends ParameterColumn {
 
 	TiltColumn(MapsLayout parent) {
 		super(parent);
-		addChild(new ModulateableKnob(this, 192));
+		addChild(new ModulateableKnobWithSlidersHeight(this, 192) {
+			@Override
+			protected int getBackgroundRoundings() {
+				return Rect.ROUNDING_LEFT;
+			}
+		});
 	}
 }
--- projects/epc/web-ui/src/main/java/com/nonlinearlabs/client/world/maps/parameters/Echo/TimeColumn.java
+++ projects/epc/web-ui/src/main/java/com/nonlinearlabs/client/world/maps/parameters/Echo/TimeColumn.java
@@ -2,6 +2,7 @@ package com.nonlinearlabs.client.world.maps.parameters.Echo;
 
 import com.nonlinearlabs.client.world.maps.MapsLayout;
 import com.nonlinearlabs.client.world.maps.parameters.ModulateableKnob;
+import com.nonlinearlabs.client.world.maps.parameters.ModulateableSmallKnobParameter;
 import com.nonlinearlabs.client.world.maps.parameters.ParameterColumn;
 
 class TimeColumn extends ParameterColumn {
@@ -9,5 +10,6 @@ class TimeColumn extends ParameterColumn {
 	TimeColumn(MapsLayout parent) {
 		super(parent);
 		addChild(new ModulateableKnob(this, 225));
+		addChild(new ModulateableSmallKnobParameter(this, 227));
 	}
 }
