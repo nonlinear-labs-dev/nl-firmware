d279930178142b2afe43c8ff064a32ce7b59b86d
Henry Hoegelow <henry@hoegelow.com>
2020-10-29 17:34:52 +0100

    fix program change and midi device hot plug

--- projects/epc/audio-engine/src/synth/C15Synth.cpp
+++ projects/epc/audio-engine/src/synth/C15Synth.cpp
@@ -58,7 +58,7 @@ C15Synth::C15Synth(const AudioEngineOptions* options)
     MidiEvent e;
     std::copy(msg.rawBytes, msg.rawBytes + 3, e.raw);
 
-    if((e.raw[0] & 0xC0) == 0xC0)
+    if((e.raw[0] & 0xF0) == 0xC0)
     {
       // receive program changes midi-over-ip and dispatch it to playground
       send(nltools::msg::EndPoint::Playground, nltools::msg::Midi::ProgramChangeMessage { e.raw[1] });
