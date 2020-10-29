a6426368b999025d1513ff019839a7b123378934
Henry Hoegelow <henry@hoegelow.com>
2020-10-29 12:01:34 +0100

    support midi devices that have only input OR output, too

--- projects/bbb/midi-over-ip/src/Options.cpp
+++ projects/bbb/midi-over-ip/src/Options.cpp
@@ -23,7 +23,8 @@ Options::Options(int& argc, char**& argv)
   Glib::OptionContext ctx;
 
   OptionDetail::createOption(mainGroup, "audioengine-host", 'a', "where to find the audio engine", m_audioengineHost);
-  OptionDetail::createOption(mainGroup, "midi-device", 'm', "the device to use", m_midiDevice);
+  OptionDetail::createOption(mainGroup, "midi-in-device", 'i', "the device to use for input", m_iDevice);
+  OptionDetail::createOption(mainGroup, "midi-out-device", 'o', "the device to use for output", m_oDevice);
   ctx.set_main_group(mainGroup);
   ctx.set_help_enabled(true);
 
@@ -56,7 +57,12 @@ Glib::ustring Options::getAudioEngineHost() const
   return m_audioengineHost;
 }
 
-Glib::ustring Options::getMidiDevice() const
+Glib::ustring Options::getMidiInDevice() const
 {
-  return m_midiDevice;
+  return m_iDevice;
+}
+
+Glib::ustring Options::getMidiOutDevice() const
+{
+  return m_oDevice;
 }
