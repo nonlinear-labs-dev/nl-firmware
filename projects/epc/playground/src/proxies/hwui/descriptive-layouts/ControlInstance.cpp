7e35a63b1af0da29e2ba7ab3402f2499937faf2a
schlenkibus <justus.scheil@parteimail.de>
2020-11-12 11:01:42 +0100

    cleanup

--- projects/epc/playground/src/proxies/hwui/descriptive-layouts/ControlInstance.cpp
+++ projects/epc/playground/src/proxies/hwui/descriptive-layouts/ControlInstance.cpp
@@ -1,25 +1,7 @@
 #include <utility>
-
-#include <utility>
-
-#include <utility>
-
-#include <proxies/hwui/panel-unit/boled/parameter-screens/controls/MacroControlEditButtonMenu.h>
-#include <proxies/hwui/panel-unit/boled/parameter-screens/controls/ModulationCarousel.h>
-#include <proxies/hwui/panel-unit/boled/sound-screens/controls/ConvertSoundMenu.h>
-#include <proxies/hwui/panel-unit/boled/sound-screens/controls/SingleSoundEditMenuLegacy.h>
-#include <proxies/hwui/panel-unit/boled/parameter-screens/controls/ParameterCarousel.h>
-
-#include <proxies/hwui/panel-unit/boled/preset-screens/controls/PresetList.h>
-#include <proxies/hwui/descriptive-layouts/concrete/preset/LoadVoiceGroupPresetList.h>
-#include <proxies/hwui/descriptive-layouts/concrete/sound/menus/DualSoundEditMenu.h>
-#include <proxies/hwui/descriptive-layouts/concrete/sound/menus/SingleSoundEditMenu.h>
 #include "ControlInstance.h"
 #include "GenericControl.h"
-
-#include "proxies/hwui/panel-unit/boled/parameter-screens/controls/ParameterEditButtonMenu.h"
 #include "ControlRegistry.h"
-#include "PropertyOwner.h"
 
 namespace DescriptiveLayouts
 {
