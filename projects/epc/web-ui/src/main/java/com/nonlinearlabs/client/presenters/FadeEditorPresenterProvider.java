1c5d2ed295c58e5aa65d3033f61b12f1c507b254
schlenkibus <justus.scheil@parteimail.de>
2020-11-12 14:06:47 +0100

    fixed negative fade range issues in keybed

--- projects/epc/web-ui/src/main/java/com/nonlinearlabs/client/presenters/FadeEditorPresenterProvider.java
+++ projects/epc/web-ui/src/main/java/com/nonlinearlabs/client/presenters/FadeEditorPresenterProvider.java
@@ -67,8 +67,8 @@ public class FadeEditorPresenterProvider extends Notifier<FadeEditorPresenter> {
         BasicParameterModel rangeI = model.getParameter(new ParameterId(397, VoiceGroup.I));
         BasicParameterModel rangeII = model.getParameter(new ParameterId(397, VoiceGroup.II));
 
-        double rI = Math.max(Math.min(presenter.fadePointI.to + (rangeI.value.value.getValue() * 61), 61), 0);
-        double rII = Math.min(Math.max(presenter.fadePointII.from - (rangeII.value.value.getValue() * 61), 0), 61);
+        double rI = Math.max(Math.min(presenter.fadePointI.to + (rangeI.value.value.getValue() * 61), 61), presenter.fadePointI.to);
+        double rII = Math.min(Math.max(presenter.fadePointII.from - (rangeII.value.value.getValue() * 61), 0), presenter.fadePointII.from);
 
         presenter.fadeRangeI = rI;
         presenter.fadeRangeII = rII;
