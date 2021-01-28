57da5bf8cc1f0a5a2bec4d5d576e55ef828946ec
schlenkibus <justus.scheil@parteimail.de>
2021-01-28 16:39:38 +0100

    add tests and fix memory in stream

--- projects/epc/playground/src/testing/unit-tests/data/AttributeTest.cpp
+++ projects/epc/playground/src/testing/unit-tests/data/AttributeTest.cpp
@@ -3,6 +3,8 @@
 #include <xml/XmlReader.h>
 #include <xml/MemoryInStream.h>
 #include <serialization/Serializer.h>
+#include <CompileTimeOptions.h>
+#include <xml/FileOutStream.h>
 
 TEST_CASE("Glib::Markup::escape_text() newline")
 {
@@ -22,35 +24,49 @@ TEST_CASE("XML Reader Trim")
   CHECK(trim("Z1\nZ2\nZ3\n") == "Z1\nZ2\nZ3");
 }
 
-TEST_CASE("XML Reader Test")
+class MockSerializer : public Serializer
 {
-  class MockSerializer : public Serializer
+ public:
+  MockSerializer()
+      : Serializer(getTagName())
   {
-   public:
-    MockSerializer()
-        : Serializer(getTagName())
-    {
-    }
+  }
 
-    static Glib::ustring getTagName()
-    {
-      return "a";
-    }
+  static Glib::ustring getTagName()
+  {
+    return "a";
+  }
 
-   protected:
-    void writeTagContent(Writer &writer) const override
-    {
-    }
-    void readTagContent(Reader &reader) const override
-    {
-      reader.onTextElement("b", [](auto str, const Attributes &attr) {
-        CHECK(str == "Z1\nZ2");
-      });
-    }
-  };
+ protected:
+  void writeTagContent(Writer &writer) const override
+  {
+  }
+  void readTagContent(Reader &reader) const override
+  {
+    reader.onTextElement("b", [](auto str, const Attributes &attr) { CHECK(str == "Z1\nZ2"); });
+  }
+};
 
+TEST_CASE("XML Reader Test - Memory In Stream")
+{
   Glib::ustring testXML = "<a><b>Z1\nZ2</b></a>";
   MemoryInStream stream(testXML, false);
+  auto scope = TestHelper::createTestScope();
+  XmlReader reader(stream, scope->getTransaction());
+  reader.read<MockSerializer>();
+}
+
+TEST_CASE("XML Reader Test - File In Stream")
+{
+  const auto testFilePath = "/tmp/" + std::to_string(std::chrono::system_clock::now().time_since_epoch().count()) + ".xml";
+
+  {
+    FileOutStream stream(testFilePath, false);
+    stream.write("<a><b>Z1\nZ2</b></a>");
+    stream.commit();
+  }
+
+  FileInStream stream(testFilePath, false);
   auto scope = TestHelper::createTestScope();
   XmlReader reader(stream, scope->getTransaction());
   reader.read<MockSerializer>();
