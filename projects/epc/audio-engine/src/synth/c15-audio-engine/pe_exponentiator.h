75ae3d4dab341ed5e5982ca68b04e9232ebf088e
Henry Hoegelow <henry@hoegelow.com>
2021-04-18 12:36:25 +0200

    refactor class so it has less state, making it suitable for multiple threads removing unnecessary member write should improve performance inline frequently called methods add constexpr to help compiler optimizing

--- projects/epc/audio-engine/src/synth/c15-audio-engine/pe_exponentiator.h
+++ projects/epc/audio-engine/src/synth/c15-audio-engine/pe_exponentiator.h
@@ -13,30 +13,29 @@
 #include <stdint.h>
 #include "pe_defines_config.h"
 #include "nltoolbox.h"
+#include <math.h>
 
 struct exponentiator
 {
-  /* table position variables */
-  uint32_t m_position_step = 0;
-  float m_position_fine = 0.0f;
   /* constants (table offsets, bases, exponent scalings, hyperbolic floor parameters) */
-  const float m_freqExponent_offset = 69.0f;       // pitch reference offset (where resulting frequency factor equals 1)
-  const float m_freqBase = 2.0f;                   // base for pitch to frequency conversion
-  const float m_scaleFreqExponent = 1.0f / 12.0f;  // exponent normalization for pitch conversion
-  const float m_gainBase = 10.0f;                  // base for level to amplitude and time conversion
-  const float m_scaleGainExponent = 1.0f / 20.0f;  // exponent normalization for level and time conversion
-  const float m_hyperfloor[2]
+  constexpr static float m_freqExponent_offset
+      = 69.0f;                               // pitch reference offset (where resulting frequency factor equals 1)
+  constexpr static float m_freqBase = 2.0f;  // base for pitch to frequency conversion
+  constexpr static float m_scaleFreqExponent = 1.0f / 12.0f;  // exponent normalization for pitch conversion
+  constexpr static float m_gainBase = 10.0f;                  // base for level to amplitude and time conversion
+  constexpr static float m_scaleGainExponent = 1.0f / 20.0f;  // exponent normalization for level and time conversion
+  constexpr static float m_hyperfloor[2]
       = { (300.0f / 13.0f),
           (280.0f / 13.0f) };  // two parameters needed to generate the hyperbolic floor function of oscillator pitches
   /* constant input value ranges for (clipped) table access */  // (range values provided by pe_defines_config.h)
-  const float m_linear_pitch_from = dsp_expon_lin_pitch_from;
-  const float m_linear_pitch_to = dsp_expon_lin_pitch_range + dsp_expon_lin_pitch_from;
-  const float m_oscillator_pitch_from = dsp_expon_osc_pitch_from;
-  const float m_oscillator_pitch_to = dsp_expon_osc_pitch_range + dsp_expon_osc_pitch_from;
-  const float m_level_from = dsp_expon_level_from;
-  const float m_level_to = dsp_expon_level_range + dsp_expon_level_from;
-  const float m_time_from = dsp_expon_time_from;
-  const float m_time_to = dsp_expon_time_range + dsp_expon_time_from;
+  constexpr static float m_linear_pitch_from = dsp_expon_lin_pitch_from;
+  constexpr static float m_linear_pitch_to = dsp_expon_lin_pitch_range + dsp_expon_lin_pitch_from;
+  constexpr static float m_oscillator_pitch_from = dsp_expon_osc_pitch_from;
+  constexpr static float m_oscillator_pitch_to = dsp_expon_osc_pitch_range + dsp_expon_osc_pitch_from;
+  constexpr static float m_level_from = dsp_expon_level_from;
+  constexpr static float m_level_to = dsp_expon_level_range + dsp_expon_level_from;
+  constexpr static float m_time_from = dsp_expon_time_from;
+  constexpr static float m_time_to = dsp_expon_time_range + dsp_expon_time_from;
   /* conversion tables (constructed on initialization) */  // (note: the additional table element (size + 1) prevents interpolation issues)
   float m_linear_pitch_table[dsp_expon_lin_pitch_range + 1];  // linear pitch table: [-150, 150] semitones
   float m_oscillator_pitch_table[dsp_expon_osc_pitch_range
@@ -48,13 +47,8 @@ struct exponentiator
   void init();  // perform construction of all four conversion tables
   /* hyperbolic osc pitch function (nonlinear pitch floor) */
   float hyperfloor(float _value);
-  /* table position clipping */
-  float clip(float _min, float _max, float _value);  // return clipped value according to min and max parameters
-  /* update (clipped) table position */
-  void setTablePos(float _value);  // transform a valid (clipped) table position into integer and fractional parts
+
   /* table interpolation */
-  float getInterpolated(float _fade, float _first,
-                        float _second);  // perform linear interpolation of two values (cross-fade value provided)
   /* run-time conversion methods (using table interpolation) */
   float eval_lin_pitch(
       float _value);  // linear pitch conversion (into frequency factor - multiples of 440 Hz - or different reference)
@@ -62,3 +56,33 @@ struct exponentiator
   float eval_level(float _value);      // gain/level conversion (into amplitude factor)
   float eval_time(float _value);       // time conversion (into milliseconds)
 };
+
+inline float eval(float _value, float from, float to, float* table)
+{
+  const auto v = std::clamp(_value, from, to) - from;
+  const auto vFloor = floor(v);
+  const auto position_step = static_cast<uint32_t>(vFloor);
+  const auto position_fine = v - vFloor;
+  return (((1 - position_fine) * table[position_step]) + (position_fine * table[position_step + 1]));
+}
+
+/* main, run-time conversion methods */
+inline float exponentiator::eval_lin_pitch(float _value)
+{
+  return eval(_value, m_linear_pitch_from, m_linear_pitch_to, m_linear_pitch_table);
+}
+
+inline float exponentiator::eval_osc_pitch(float _value)
+{
+  return eval(_value, m_oscillator_pitch_from, m_oscillator_pitch_to, m_oscillator_pitch_table);
+}
+
+inline float exponentiator::eval_level(float _value)
+{
+  return eval(_value, m_level_from, m_level_to, m_level_table);
+}
+
+inline float exponentiator::eval_time(float _value)
+{
+  return eval(_value, m_time_from, m_time_to, m_time_table);
+}
