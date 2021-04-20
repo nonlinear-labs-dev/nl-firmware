b01dc97f90a70bf73f6e7263476e89f51aef40d2
kleiner-bill <matthias.seeber@nonlinear-labs.de>
2021-04-20 18:55:26 +0200

    added DspHostDualTester class for AudioEngine acceptance tests

---
+++ projects/epc/acceptance-tests/src/mock/DspHostDualTester.cpp
@@ -0,0 +1,35 @@
+#include "DspHostDualTester.h"
+
+DspHostDualTester::DspHostDualTester(dsp_host_dual* _host)
+    : m_host{ _host }
+{
+}
+
+uint8_t DspHostDualTester::getActiveVoices(const VoiceGroup _group = VoiceGroup::Global)
+{
+  // retrieving the polyphonic Gate Envelope signal from both Poly Sections/Parts (12 Voices per Part)
+  // (per Voice: Gate Signal is 1.0 (immediately) after KeyPress, and (almost immediately) 0.0 after KeyRelease)
+
+  const uint32_t gateSignalIndex = static_cast<uint32_t>(C15::Signals::Truepoly_Signals::Env_G_Sig);
+  float gateSignal;
+
+  // obtain specific Part if desired, otherwise both Parts
+  switch(_group)
+  {
+    case VoiceGroup::I:
+      gateSignal = sumUp(m_host->m_poly[0].m_signals.get_poly(gateSignalIndex));
+      break;
+    case VoiceGroup::II:
+      gateSignal = sumUp(m_host->m_poly[1].m_signals.get_poly(gateSignalIndex));
+      break;
+    default:
+      gateSignal = sumUp(m_host->m_poly[0].m_signals.get_poly(gateSignalIndex))
+          + sumUp(m_host->m_poly[1].m_signals.get_poly(gateSignalIndex));
+      break;
+  }
+
+  // having summed up all voices, we obtain the number of "active" Voices (0 ... 24)
+  // the obtained value should be sample-precise and is not dependant from Sustain or Level Parameters
+
+  return static_cast<uint8_t>(gateSignal);  // 0 : no voice active / 1, 2, ... 23, 24 : n voices active
+}
---
+++ projects/epc/acceptance-tests/src/mock/DspHostDualTester.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <synth/c15-audio-engine/dsp_host_dual.h>
+
+class DspHostDualTester
+{
+public:
+  explicit DspHostDualTester(dsp_host_dual* _host);
+
+  // can be used to detect active Voices, for example with VoiceAllocation/Envelope Reset events
+  uint8_t getActiveVoices(const VoiceGroup _group);
+
+private:
+  dsp_host_dual* m_host;
+};
