9539cda02fce535e79c9eeb7b01faa340be5c55a
AntonSchmied <anton.schmied@hotmail.de>
2021-04-30 17:23:15 +0200

    reload and disable and enable not yet working

--- projects/epc/playground/src/device-settings/Wifi.cpp
+++ projects/epc/playground/src/device-settings/Wifi.cpp
@@ -7,10 +7,10 @@ Wifi::Wifi()
 
 void Wifi::setPassphrase(const Glib::ustring& _newPassphrase)
 {
-    SpawnAsyncCommandLine::spawn(std::vector<std::string> { "nmcli", "con", "modify", "C15", "802-11-wireless-security.psk ", _newPassphrase},
+    SpawnAsyncCommandLine::spawn(std::vector<std::string> { "nmcli", "con", "modify", "C15", "802-11-wireless-security.psk", _newPassphrase},
                                  [](auto) {},
                                  [&](const std::string& e) { nltools::Log::error(__FILE__, __LINE__, __PRETTY_FUNCTION__, e); });
-    reloadConnection();
+    // reloadConnection();
 }
 
 
@@ -19,15 +19,18 @@ void Wifi::setSSID(const Glib::ustring& _newSSID)
     SpawnAsyncCommandLine::spawn(std::vector<std::string> { "nmcli", "con", "modify", "C15", "wifi.ssid", _newSSID},
                                  [](auto) {},
                                  [&](const std::string& e) { nltools::Log::error(__FILE__, __LINE__, __PRETTY_FUNCTION__, e); });
-    reloadConnection();
+    // reloadConnection();
 }
 
 void Wifi::reloadConnection()
 {
-    SpawnAsyncCommandLine::spawn(std::vector<std::string> { "systemctl", "restart", "NetworkManager"},
+    SpawnAsyncCommandLine::spawn(std::vector<std::string> { "nmcli", "connection", "down", "C15"},
                                  [](auto) {},
                                  [&](const std::string& e) { nltools::Log::error(__FILE__, __LINE__, __PRETTY_FUNCTION__, e); });
 
+    SpawnAsyncCommandLine::spawn(std::vector<std::string> { "nmcli", "connection", "up", "C15"},
+                                 [](auto) {},
+                                 [&](const std::string& e) { nltools::Log::error(__FILE__, __LINE__, __PRETTY_FUNCTION__, e); });
 }
 
 void Wifi::enableConnection()
@@ -40,6 +43,7 @@ void Wifi::enableConnection()
 
 void Wifi::disableConnection()
 {
-    SpawnAsyncCommandLine::spawn(std::vector<std::string> { "nmcli", "con", "down", "C15"}, [](auto) {}, [](auto) {});
-
+    SpawnAsyncCommandLine::spawn(std::vector<std::string> { "nmcli", "con", "down", "C15"},
+                                 [](auto) {},
+                                 [&](const std::string& e) { nltools::Log::error(__FILE__, __LINE__, __PRETTY_FUNCTION__, e); });
 }
