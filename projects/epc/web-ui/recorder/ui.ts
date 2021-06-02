0c812ba9e28d5daa38eee23a7026fc7433edd750
schlenkibus <justus.scheil@parteimail.de>
2021-06-02 16:31:08 +0200

    fixed shortcuts: z now loads preset at selected postion, added left/right nudge, added tooltips for action buttons, closes #2622

--- projects/epc/web-ui/recorder/ui.ts
+++ projects/epc/web-ui/recorder/ui.ts
@@ -33,32 +33,40 @@ class UI {
             c15.reset();
             this.waveform.selectedRange.playbackRange.reset();
 
-	}
+	    }
 
         document.onkeypress = (e) => {
+            var undoEnabled = !undo.classList.contains("disabled");
+            var downloadEnabled = !download.classList.contains("disabled");
+
             if (e.key == ' ')
                 c15!.togglePlayback();
-            else if (e.key == 'z' || e.key == 'Z')
-                c15!.reset();
+            else if ((e.key == 'z' || e.key == 'Z') && undoEnabled)
+                c15!.undo();
             else if (e.key == 'r' || e.key == 'R')
                 c15!.toggleRecording();
-            else if (e.key == 's' || e.key == 'S')
+            else if ((e.key == 's' || e.key == 'S') && downloadEnabled)
                 c15!.download(this.waveform.selectedRange.playbackRange.min(), this.waveform.selectedRange.playbackRange.max());
             else if (e.key == 'ZoomIn' || e.key == '+')
                 this.waveform.zoomIn();
             else if (e.key == 'ZoomOut' || e.key == '-')
                 this.waveform.zoomOut();
-            else if (e.key == 'ArrowLeft')
-                console.log("nudge left");
+
+            this.update();
+        }
+
+        document.onkeydown = (e) => {
+            if (e.key == 'ArrowLeft')
+                this.waveform.scrollBy(50);
             else if (e.key == 'ArrowRight')
-                console.log("nudge right");
+                this.waveform.scrollBy(-50);
             else if (e.key == 'ArrowUp')
                 console.log("prev preset");
             else if (e.key == 'ArrowDown')
                 console.log("next preset");
 
-            this.update();
-        }
+                this.update();
+        };
 
         document.getElementById("waveform")!.onkeydown = (e) => c15.togglePlayback();
 
