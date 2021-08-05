de65431b705b3e17f9d978dcf461dca26c3a5eb1
Henry Hoegelow <henry@hoegelow.com>
2021-08-05 10:53:23 +0200

    avoid looping over all the frames to export, instead do some housekeeping enabling us to calculate the estimated file sizes in O(0).

--- projects/epc/audio-engine/src/recorder/Recorder.cpp
+++ projects/epc/audio-engine/src/recorder/Recorder.cpp
@@ -117,23 +117,18 @@ nlohmann::json Recorder::prepareDownload(FrameId begin, FrameId end) const
 {
   nlohmann::json ret;
 
+  auto numFrames = end - begin + 1;
+  size_t numFlacBytes = 0;
+  size_t numWaveBytes = 44 + numFrames * (FlacEncoder::flacFrameSize * 3 * 2);
+
   auto stream = m_storage->startStream(begin, end);
 
   stream->getFirstAndLast([&](const auto &first, const auto &last) {
     ret["range"] = { { "from", first.recordTime.time_since_epoch().count() },
                      { "to", last.recordTime.time_since_epoch().count() } };
+
+    numFlacBytes = last.summedUpFlacMemUsage - first.summedUpFlacMemUsage + first.buffer.size();
   });
-
-  size_t numFlacBytes = 0;
-  size_t numWaveBytes = 44;
-
-  auto onFrame = [&](auto &frame, auto isLast) {
-    numFlacBytes += frame.getMemUsage();
-    numWaveBytes += FlacEncoder::flacFrameSize * 3 * 2;  // 24 bit
-  };
-
-  while(stream->next(onFrame))
-    ;
 
   ret["flac"] = { { "size", numFlacBytes } };
   ret["wave"] = { { "size", numWaveBytes } };
