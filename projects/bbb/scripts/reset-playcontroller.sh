2ffc6058fc26fb2ab277d0ddae62e1b122c5d50a
KlausStrohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2021-10-12 13:22:40 +0200

    refactor scripts and services related to UHID

--- projects/bbb/scripts/reset-playcontroller.sh
+++ projects/bbb/scripts/reset-playcontroller.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
+# reset the playcontroller and obtain and store UHID if successful
 if /bin/playcontroller reset 5; then
   /bin/playcontroller req uhid64
   /bin/playcontroller-read -a -h +q +6 +o > /tmp/UHID
-  /usr/C15/text2soled/text2soled multitext "Starting the C15...@SC0" "#`/bin/cat /tmp/UHID`@SC2" "Welcome,@BC2" "those who love to play keys!@BC4"
 fi
