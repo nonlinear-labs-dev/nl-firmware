296665e6e14da7c6fda3091ac04ab3c2f09c9a35
schlenkibus <justus.scheil@parteimail.de>
2021-10-28 10:30:19 +0200

    moved defaults in same order as the webui has it, closes #2878

--- projects/epc/playground/src/proxies/hwui/panel-unit/boled/setup/SetupLayout.cpp
+++ projects/epc/playground/src/proxies/hwui/panel-unit/boled/setup/SetupLayout.cpp
@@ -890,9 +890,9 @@ namespace NavTree
       children.emplace_back(new EnumSettingItem<BenderCCMapping>(this, "Bender"));
       children.emplace_back(new EnumSettingItem<AftertouchCCMapping>(this, "Aftertouch"));
       children.emplace_back(new EnumSettingItem<Enable14BitSupport>(this, "High-Res. CCs (use LSB)"));
+      children.emplace_back(new EnumSettingItem<EnableHighVelocityCC>(this, "High-Res. Velocity (CC 88)"));
       children.emplace_back(new ResetMidiSettingsToClassic(this));
       children.emplace_back(new ResetMidiSettingsToHighRes(this));
-      children.emplace_back(new EnumSettingItem<EnableHighVelocityCC>(this, "High-Res. Velocity (CC 88)"));
     }
   };
 
