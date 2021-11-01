c7b121d9761d6f457a8e2309f1d0275467f55cbc
Klaus Strohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2021-11-01 14:58:33 +0100

    reconfig'd linker scripts
    Memory model adapted to the smallest LPC43xx suitable four our application, the LPC4322, and made compatible to all larger LPC43xx's :
    - 256k Flash in Bank A, and no Bank B
    - 32k SRAM at 0x1000 0000 (M0 Code, Data and Stack)
    - 40k SRAM at 0x1008 0000 (M4 Data and Stack)
    - 16k SRAM at 0x2000 0000 (shared mem between M0 and M4)
    - 16k SRAM at 0x2000 C000 (unused)

--- projects/playcontroller/firmware/src/shared/ipc/emphase_ipc.c
+++ projects/playcontroller/firmware/src/shared/ipc/emphase_ipc.c
@@ -9,8 +9,8 @@
 
 //
 // Places the variable(s) in the shared memory block.
-// This block must be defined in C15_LPC4337_MEM.ld.
-// The section used must be defined in both C15_LPC4337_M0.ld and C15_LPC4337_M4.ld identically
+// This block must be defined in C15_LPC43xx_MEM.ld.
+// The section used must be defined in both C15_LPC43xx_M0.ld and C15_LPC43xx_M4.ld identically
 // and must be defined properly and identically in the *_MEM.ld script includes.
 // When the size of all shared variables is larger than the block the linker will throw an error.
 // To make sure all variable are at the same memory location and have the same layout,
