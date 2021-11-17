3299e690ec326da98de100aa74a4264b18a18b6c
Henry Hoegelow <henry@hoegelow.com>
2021-11-17 16:11:52 +0100

    fix invalidation of version string based on git commit date

---
+++ cmake/generate-version.cmake
@@ -0,0 +1,8 @@
+execute_process(
+  COMMAND git log -n 1 "--pretty=format:%cd" "--date=format:%Y-%m-%d %H:%M:%S"
+  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+  OUTPUT_VARIABLE LAST_COMMIT_STAMP
+)
+
+configure_file(${INPUT_FILE} ${OUTPUT_FILE} @ONLY)
+message("Got Git Version ${LAST_COMMIT_STAMP}")
