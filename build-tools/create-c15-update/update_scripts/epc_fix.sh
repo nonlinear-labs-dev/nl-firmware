7ebc46c1788bde37d50e89a29afc29ea4159d60b
Henry Hoegelow <henry@hoegelow.com>
2022-01-05 16:34:38 +0100

    WIP

--- build-tools/create-c15-update/update_scripts/epc_fix.sh
+++ build-tools/create-c15-update/update_scripts/epc_fix.sh
@@ -22,23 +22,23 @@ epc_fix() {
         fi
     fi
     
-    DESIRED_KERNEL_PARAMS="quiet ip=192.168.10.10:::::eth0:none mitigations=off isolcpus=0,2 snd-hda-intel.bdl_pos_adj=1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1"
-
-    if ! cat /etc/default/grub | grep -F "$DESIRED_KERNEL_PARAMS" > /dev/null; then
-      if ! (mkdir -p /mnt/sda2 \
-        && mount /dev/sda2 /mnt/sda2 \
-        && mount /dev/sda1 /mnt/sda2/boot \
-        && arch-chroot /mnt/sda2 /bin/bash -c "sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT.*$/GRUB_CMDLINE_LINUX_DEFAULT=\\\"$DESIRED_KERNEL_PARAMS\\\"/g' /etc/default/grub" \
-        && arch-chroot /mnt/sda2 /bin/bash -c "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck" \
-        && arch-chroot /mnt/sda2 /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg" \
-        && arch-chroot /mnt/sda2 /bin/bash -c "mkdir -p /boot/EFI/BOOT" \
-        && arch-chroot /mnt/sda2 /bin/bash -c "cp /boot/EFI/arch_grub/grubx64.efi /boot/EFI/BOOT/BOOTX64.EFI" \
-        && umount /mnt/sda2/boot \
-        && umount /mnt/sda2); then
-          printf "E48 ePC update: setting Kernel Params failed" >> /tmp/fix_error.log && ((fix_errors++))
-      fi
+    if ! cat /etc/default/grub | grep "isolcpus" > /dev/null; then
+        if ! cat /etc/default/grub | grep "mitigations" > /dev/null; then
+            if ! (mkdir -p /mnt/sda2 \
+                && mount /dev/sda2 /mnt/sda2 \
+                && mount /dev/sda1 /mnt/sda2/boot \
+                && arch-chroot /mnt/sda2 /bin/bash -c "sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT.*$/GRUB_CMDLINE_LINUX_DEFAULT=\\\"quiet ip=192.168.10.10:::::eth0:none mitigations=off isolcpus=0,2\\\"/g' /etc/default/grub" \
+                && arch-chroot /mnt/sda2 /bin/bash -c "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck" \
+                && arch-chroot /mnt/sda2 /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg" \
+                && arch-chroot /mnt/sda2 /bin/bash -c "mkdir -p /boot/EFI/BOOT" \
+                && arch-chroot /mnt/sda2 /bin/bash -c "cp /boot/EFI/arch_grub/grubx64.efi /boot/EFI/BOOT/BOOTX64.EFI" \
+                && umount /mnt/sda2/boot \
+                && umount /mnt/sda2); then
+                   printf "E48 ePC update: setting Kernel Params failed" >> /tmp/fix_error.log && ((fix_errors++))
+            fi
+        fi
     fi
-
+    
     if ! fsck /dev/sda4 -n; then
         umount /dev/sda4
         if ! mkfs.ext4 /dev/sda4; then
