d67bf9b3e7cfb9dcccfc6c46d1115b5482d84571
Henry Hoegelow <henry@hoegelow.com>
2022-01-19 14:26:00 +0100

    little tweaks to c16 ui

--- projects/web/client/routes.js
+++ projects/web/client/routes.js
@@ -1,4 +1,4 @@
-import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+  import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import './c16-hwui/parameter-editor.ts';
 import './c16-hwui/preset-manager.ts';
