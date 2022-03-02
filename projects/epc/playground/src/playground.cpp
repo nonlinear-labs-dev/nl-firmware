40e4caf9340760b775a21f83d75c73628bfb9ed1
schlenkibus <justus.scheil@parteimail.de>
2022-03-02 20:56:20 +0100

    cleanup

--- projects/epc/playground/src/playground.cpp
+++ projects/epc/playground/src/playground.cpp
@@ -3,27 +3,6 @@
 #include "Options.h"
 #include "playground-helpers.h"
 #include <giomm.h>
-#include <nltools/messaging/Messaging.h>
-
-
-{
-  std::vector<void*> stack;
-  stack.reserve(maxFrames);
-  auto numFrames = backtrace(stack.data(), maxFrames);
-  if(numFrames == maxFrames)
-  {
-    nltools::Log::error("You could be missing some frames from your backtrace. Try to increase your maxFrames");
-  }
-
-  auto symbols = backtrace_symbols(stack.data(), numFrames); //symbols is mallocced here
-
-  for(auto i = 0; i < numFrames; i++)
-  {
-    nltools::Log::printWithLevel(level, i, symbols[i]);
-  }
-
-  free(symbols);
-}
 
 int main(int numArgs, char **argv)
 {
