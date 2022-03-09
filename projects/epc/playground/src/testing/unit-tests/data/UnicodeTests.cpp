111c6e622c8e15160c04c03c38554329ab1c5afb
Henry Hoegelow <henry@hoegelow.com>
2022-03-09 14:36:31 +0100

    issue-3054: support NL utf-8 characters
    We are using some special utf-8 characters that are unknown
    to the system. In order to enable the system to compare and
    sort those chars, we have to add them in a locale.
    
    This patch;
    - adds the target install-nl-locale which (at least on ubuntu)
      creates and installs the locale "en_US@nonlinear.UTF-8" when
      executed as root
    - builds and adds the locale for epc and epc updates

--- projects/epc/playground/src/testing/unit-tests/data/UnicodeTests.cpp
+++ projects/epc/playground/src/testing/unit-tests/data/UnicodeTests.cpp
@@ -2,15 +2,49 @@
 #include <parameters/names/ParameterDB.h>
 #include "parameter_declarations.h"
 
+TEST_CASE("NL chars are sorted properly")
+{
+  auto chars
+      = u8"\uE000\uE001\uE002\uE003\uE200\uE201\uE071\uE072\uE081\uE082\uE0BA\uE0BB\uE0C0\uE0C1\uE0CA\uE0F0\uE0F1\uE0F2"
+        u8"\uE0F3\uE0F4\uE0F5\uE20A\uE20B\uE20C\uE20D\uE20E\uE20F\uE210\uE300\uE301\uE302\uE303\uE30E";
+
+  auto numChars = g_utf8_strlen(chars, -1);
+  CHECK(numChars == 33);
+
+  for(int i = 0; i < numChars; i++)
+  {
+    auto left = g_utf8_offset_to_pointer(chars, i);
+
+    Glib::ustring leftStr(left, left + 4);
+
+    for(int j = i; j < numChars; j++)
+    {
+      auto right = g_utf8_offset_to_pointer(chars, j);
+      Glib::ustring rightStr(right, right + 4);
+
+      if(i == j)
+      {
+        CHECK(leftStr == rightStr);
+      }
+      else
+      {
+        CHECK(leftStr < rightStr);
+        CHECK(leftStr != rightStr);
+        CHECK(rightStr > leftStr);
+      }
+    }
+  }
+}
+
 TEST_CASE("Macro Labels are sortable A < B < C < D < E < F")
 {
   auto& db = ParameterDB::get();
-  auto mca = db.getLongName({C15::PID::MC_A, VoiceGroup::Global});
-  auto mcb = db.getLongName({C15::PID::MC_B, VoiceGroup::Global});
-  auto mcc = db.getLongName({C15::PID::MC_C, VoiceGroup::Global});
-  auto mcd = db.getLongName({C15::PID::MC_D, VoiceGroup::Global});
-  auto mce = db.getLongName({C15::PID::MC_E, VoiceGroup::Global});
-  auto mcf = db.getLongName({C15::PID::MC_F, VoiceGroup::Global});
+  auto mca = db.getLongName({ C15::PID::MC_A, VoiceGroup::Global });
+  auto mcb = db.getLongName({ C15::PID::MC_B, VoiceGroup::Global });
+  auto mcc = db.getLongName({ C15::PID::MC_C, VoiceGroup::Global });
+  auto mcd = db.getLongName({ C15::PID::MC_D, VoiceGroup::Global });
+  auto mce = db.getLongName({ C15::PID::MC_E, VoiceGroup::Global });
+  auto mcf = db.getLongName({ C15::PID::MC_F, VoiceGroup::Global });
 
   CHECK(mca == mca);
   CHECK(mcb == mcb);
