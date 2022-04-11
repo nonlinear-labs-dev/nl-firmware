97e7600e641a8908b2e039330620eed533235d26
kleiner-bill <matthias.seeber@nonlinear-labs.de>
2022-04-11 10:41:01 +0200

    param-db fixes for #2908
    Comb Allpass and Lowpass Filter KeyTrackings had outdated definitions for the Synth's Scaling mechanism. The spreadsheet (and header files) have been updated accordingly.

--- projects/shared/parameter-db/generated/c15_config.h
+++ projects/shared/parameter-db/generated/c15_config.h
@@ -2,7 +2,7 @@
 
 /******************************************************************************/
 /**	@file       c15_config.h
-    @date       2022-01-10, 15:17
+    @date       2022-04-11, 10:07
     @version    1.7B-9
     @author     M. Seeber
     @brief      crucial information about the audio engine
--- projects/shared/parameter-db/generated/parameter_declarations.h
+++ projects/shared/parameter-db/generated/parameter_declarations.h
@@ -2,7 +2,7 @@
 
 /******************************************************************************/
 /**	@file       parameter_declarations.h
-    @date       2022-01-10, 15:17
+    @date       2022-04-11, 10:07
     @version    1.7B-9
     @author     M. Seeber
     @brief      descriptors for all parameter-related details
