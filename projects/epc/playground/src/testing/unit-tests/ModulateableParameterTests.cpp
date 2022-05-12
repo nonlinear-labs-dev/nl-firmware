ff2488694c845540954140fcd67e3950ef1000cc
schlenkibus <justus.scheil@parteimail.de>
2022-05-12 12:07:59 +0200

    fixed remaining unit-tests

--- projects/epc/playground/src/testing/unit-tests/ModulateableParameterTests.cpp
+++ projects/epc/playground/src/testing/unit-tests/ModulateableParameterTests.cpp
@@ -5,17 +5,19 @@
 #include <testing/parameter/TestGroupSet.h>
 #include <parameters/scale-converters/Linear100PercentScaleConverter.h>
 #include <sync/SyncMasterMockRoot.h>
+#include "testing/unit-tests/mock/MockSettingsObject.h"
 
 TEST_CASE("Modulateable Parameter")
 {
+  MockSettingsObject settings("/tmp/dksakda.xml", &SyncMasterMockRoot::get());
   TestGroupSet groupSet(&SyncMasterMockRoot::get());
-  groupSet.init(nullptr);
-  TestGroup group(&groupSet, VoiceGroup::I);
+  groupSet.init(&settings);
+  TestGroup group(&groupSet, VoiceGroup::Global);
 
   SECTION("1.4pct to 112 playcontroller")
   {
     auto scope = TestHelper::createTestScope();
-    ModulateableParameter peter(&group, { 1, VoiceGroup::Global },
+    ModulateableParameter peter(&group, { C15::PID::FB_Mix_FX, VoiceGroup::Global },
                                 ScaleConverter::get<Linear100PercentScaleConverter>());
     peter.setModulationAmount(scope->getTransaction(), 0.014);
     peter.setModulationSource(scope->getTransaction(), MacroControls::MC1);
