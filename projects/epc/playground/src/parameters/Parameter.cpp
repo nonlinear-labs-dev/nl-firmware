7e9a3924d40f554f5a85294fb7c714b0f819b135
schlenkibus <justus.scheil@parteimail.de>
2022-05-18 09:38:44 +0200

    fixed webui name update for dynamic labels that differ in sound-types

--- projects/epc/playground/src/parameters/Parameter.cpp
+++ projects/epc/playground/src/parameters/Parameter.cpp
@@ -41,6 +41,10 @@ Parameter::Parameter(ParameterGroup *group, ParameterId id, const ScaleConverter
     , m_voiceGroup { group->getVoiceGroup() }
     , m_lastSnapshotedValue(c_invalidSnapshotValue)
 {
+  if(auto eb = getParentEditBuffer())
+  {
+    eb->onSoundTypeChanged(sigc::mem_fun(this, &Parameter::onSoundTypeChanged));
+  }
 }
 
 Parameter::~Parameter()
@@ -452,14 +456,14 @@ void Parameter::writeDocProperties(Writer &writer, tUpdateID knownRevision) cons
   writer.writeTextElement("value", to_string(m_value.getRawValue()));
   writer.writeTextElement("default", to_string(m_value.getDefaultValue()));
   writer.writeTextElement("boolean", to_string(m_value.isBoolean()));
+  writer.writeTextElement("long-name", getLongName());
+  writer.writeTextElement("short-name", getShortName());
+  writer.writeTextElement("info-text", getInfoText());
 
   if(shouldWriteDocProperties(knownRevision))
   {
     writer.writeTextElement("bipolar", to_string(m_value.isBiPolar()));
     writer.writeTextElement("scaling", m_value.getScaleConverter()->controlPositionToDisplayJS());
-    writer.writeTextElement("long-name", getLongName());
-    writer.writeTextElement("short-name", getShortName());
-    writer.writeTextElement("info-text", getInfoText());
     writer.writeTextElement("coarse-denominator", to_string(m_value.getCoarseDenominator()));
     writer.writeTextElement("fine-denominator", to_string(m_value.getFineDenominator()));
   }
@@ -665,4 +669,9 @@ bool Parameter::isDisabled() const
 void Parameter::stepCP(UNDO::Transaction *transaction, int incs, bool fine, bool shift)
 {
   setCPFromHwui(transaction, getNextStepValue(incs, fine, shift));
+}
+
+void Parameter::onSoundTypeChanged(SoundType t)
+{
+  invalidate();
 }
