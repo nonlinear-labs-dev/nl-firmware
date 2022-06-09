007d1ab84b8ce38b70a33e36b63fa11b442a25e5
Henry Hoegelow <henry@hoegelow.com>
2022-06-09 11:32:12 +0200

    using xz as compressor for epc update (109 MB vs 161 MB) streamline 'if' statements in shell scripts: use if [ "..." = "..." ] instead of ==.

--- build-tools/epc-one-for-all/createUpdate.sh
+++ build-tools/epc-one-for-all/createUpdate.sh
@@ -14,6 +14,11 @@ for var in $@; do
   cp $var $DIR/
 done
 
+tar --extract --file=$DIR/xz-5.2.5-linux-x86_64.tar.gz ./xz-5.2.5-linux-x86_64/xz
+rm $DIR/xz-5.2.5-linux-x86_64.tar.gz
+mv ./xz-5.2.5-linux-x86_64/xz $DIR/
+rm -rf ./xz-5.2.5-linux-x86_64
+
 cat << EOF > $DIR/backdoor.sh
 #!/bin/sh
 set -e
@@ -30,8 +35,9 @@ mount -t overlay -o lowerdir=/lroot,upperdir=/nloverlay/os-overlay,workdir=/nlov
 
 mv /mnt/usr/lib/modules /tmp/
 rm -rf /mnt/*
-tar -C /mnt -xzf /nloverlay/update-scratch/update/c15-rootfs.tar.gz
+./xz -c -k -d /nloverlay/update-scratch/update/c15-rootfs.tar.xz | tar -C /mnt -x
 mv /tmp/modules /mnt/usr/lib/
+
 umount /mnt
 
 for dir in "runtime-overlay" "work"; do
