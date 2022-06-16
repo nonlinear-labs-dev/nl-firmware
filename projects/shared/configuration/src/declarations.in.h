3e080063326ce6e1f60b71a990b85b2bad3156b0
Matthias Seeber <matthias.seeber@nonlinear-labs.de>
2022-06-16 20:20:25 +0200

    wip

--- projects/shared/configuration/src/declarations.in.h
+++ projects/shared/configuration/src/declarations.in.h
@@ -11,29 +11,41 @@
 
 namespace C15
 {
-    namespace Descriptors {
-        enum class ParameterType {
+    namespace Descriptors
+    {
+        enum class ParameterType
+        {
             ${enums.parameter_type}
         };
-        enum class SmootherSection {
+        enum class SmootherSection
+        {
             ${enums.smoother_section}
         };
-        enum class SmootherClock {
+        enum class SmootherClock
+        {
             ${enums.smoother_clock}
         };
-        enum class ParameterSignal {
+        enum class ParameterSignal
+        {
             ${enums.parameter_signal}
         };
     } // namespace C15::Descriptors
-    namespace Properties {
-        enum class SmootherScale {
+    namespace Properties
+    {
+        enum class SmootherScale
+        {
             ${enums.smoother_scale}
         };
     } // namespace C15::Properties
-    namespace Parameters {} // namespace C15::Parameters
-    namespace Smoothers {} // namespace C15::Smoothers
-    namespace Signals {} // namespace C15::Signals
-    namespace PID {
-        enum ParameterID {};
+    namespace Parameters
+    {} // namespace C15::Parameters
+    namespace Smoothers
+    {} // namespace C15::Smoothers
+    namespace Signals
+    {} // namespace C15::Signals
+    namespace PID
+    {
+        enum ParameterID
+        {};
     } // namespace C15::PID
 } // namespace C15
