a75262e278157781d48776c80a1fa879bd7d3db0
schlenkibus <justus.scheil@parteimail.de>
2022-07-05 11:18:29 +0200

    updated docker environment to better react to source changes

--- projects/shared/configuration/build-in-docker.sh
+++ projects/shared/configuration/build-in-docker.sh
@@ -2,12 +2,5 @@
 
 sourceDir="$1"
 binaryDir="$2"
-userID="$4"
-groupID="$5"
 
-echo "$sourceDir"
-echo "$binaryDir"
-echo "$userID"
-echo "$groupID"
-
-podman run -v "$sourceDir":/sourceDir -v "$binaryDir":/binaryDir -ti --privileged -e USER_ID="${userID}" -e GROUP_ID="${groupID}" nl-configuration-build-container
+podman run -v "$sourceDir":/sourceDir -v "$binaryDir":/binaryDir nl-configuration-build-container
--- projects/shared/configuration/copy-results-to-source.sh
+++ projects/shared/configuration/copy-results-to-source.sh
@@ -7,10 +7,6 @@ files=(c15_config.h parameter_declarations.h parameter_list.h parameter_descript
 targetFiles=(c15_config.h parameter_declarations.h parameter_list.h parameter_descriptor.h placeholder.h)
 
 numFiles=${#files[@]}
-echo "${numFiles}"
-
-ls "${srcDir}"
-
 dstDir="${dstDir}/generated"
 srcDir="${srcDir}/generated"
 
@@ -19,6 +15,5 @@ mkdir -p "${dstDir}"
 for ((i=0; i<numFiles; i++)); do
     src=${files[${i}]}
     tgt=${targetFiles[${i}]}
-    echo "$srcDir/$src -> $dstDir/$tgt"
     cp "${srcDir}/${src}" "${dstDir}/${tgt}"
 done
