45b0471fe7897aafc460bb02cbd5aaee0235e470
schlenkibus <justus.scheil@parteimail.de>
2022-09-29 15:32:45 +0200

    wip

--- projects/epc/playground/src/device-info/FreeDiscSpaceInformation.cpp
+++ projects/epc/playground/src/device-info/FreeDiscSpaceInformation.cpp
@@ -26,7 +26,7 @@ bool FreeDiscSpaceInformation::refresh()
 {
   SpawnAsyncCommandLine::spawn(
       Application::get().getMainContext(),
-      { "sh", "-c", "\"df", "-h", "|", "grep", "'persistent'", "|", "awk", "'{print $4}'\"" },
+      { "df", "-h", "|", "grep", "'persistent'", "|", "awk", "'{print $4}'" },
       [&](const std::string& success) {
         if(success.empty())
           m_value = "N/A";
