32a5cb6183b1a8abb8f3fffcb8a32e103f005a20
Matthias Seeber <matthias.seeber@nonlinear-labs.de>
2022-09-29 15:16:08 +0200

    PlayGround: removed getDenominator overrides from ScaleConverters (now completely relying on Yaml definitions)

--- projects/epc/playground/src/parameters/scale-converters/LinearBipolar200PercentScaleConverter.cpp
+++ projects/epc/playground/src/parameters/scale-converters/LinearBipolar200PercentScaleConverter.cpp
@@ -10,16 +10,6 @@ LinearBipolar200PercentScaleConverter::~LinearBipolar200PercentScaleConverter()
 {
 }
 
-tControlPositionValue LinearBipolar200PercentScaleConverter::getCoarseDenominator(const QuantizedValue &v) const
-{
-  return ScaleConverter::getCoarseDenominator(v) * 2;
-}
-
-tControlPositionValue LinearBipolar200PercentScaleConverter::getFineDenominator(const QuantizedValue &v) const
-{
-  return ScaleConverter::getFineDenominator(v) * 2;
-}
-
 LinearBipolar200PercentFineScaleConverter::LinearBipolar200PercentFineScaleConverter()
     : LinearScaleConverter(tDisplayRange(-200, 200), PercentageDimension::get())
 {
@@ -27,14 +17,4 @@ LinearBipolar200PercentFineScaleConverter::LinearBipolar200PercentFineScaleConve
 
 LinearBipolar200PercentFineScaleConverter::~LinearBipolar200PercentFineScaleConverter()
 {
-}
-
-tControlPositionValue LinearBipolar200PercentFineScaleConverter::getCoarseDenominator(const QuantizedValue &v) const
-{
-  return ScaleConverter::getCoarseDenominator(v) * 2;
-}
-
-tControlPositionValue LinearBipolar200PercentFineScaleConverter::getFineDenominator(const QuantizedValue &v) const
-{
-  return ScaleConverter::getFineDenominator(v) * 2;
 }
--- projects/epc/playground/src/parameters/scale-converters/LinearBipolar200PercentScaleConverter.h
+++ projects/epc/playground/src/parameters/scale-converters/LinearBipolar200PercentScaleConverter.h
@@ -8,8 +8,6 @@ class LinearBipolar200PercentScaleConverter : public LinearScaleConverter
   LinearBipolar200PercentScaleConverter();
   ~LinearBipolar200PercentScaleConverter() override;
 
-  [[nodiscard]] tControlPositionValue getCoarseDenominator(const QuantizedValue &v) const override;
-  [[nodiscard]] tControlPositionValue getFineDenominator(const QuantizedValue &v) const override;
 };
 
 class LinearBipolar200PercentFineScaleConverter : public LinearScaleConverter
@@ -17,7 +15,4 @@ class LinearBipolar200PercentFineScaleConverter : public LinearScaleConverter
  public:
   LinearBipolar200PercentFineScaleConverter();
   ~LinearBipolar200PercentFineScaleConverter() override;
-
-  [[nodiscard]] tControlPositionValue getCoarseDenominator(const QuantizedValue &v) const override;
-  [[nodiscard]] tControlPositionValue getFineDenominator(const QuantizedValue &v) const override;
 };
