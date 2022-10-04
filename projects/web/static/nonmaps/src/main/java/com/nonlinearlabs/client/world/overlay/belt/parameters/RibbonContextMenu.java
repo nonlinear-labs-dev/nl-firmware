e809b563dc963881750fd1b5dd248249d91f6b00
schlenkibus <justus.scheil@parteimail.de>
2022-10-04 16:32:07 +0200

    added seperator to webUI ribbon-contextmenu

--- projects/web/static/nonmaps/src/main/java/com/nonlinearlabs/client/world/overlay/belt/parameters/RibbonContextMenu.java
+++ projects/web/static/nonmaps/src/main/java/com/nonlinearlabs/client/world/overlay/belt/parameters/RibbonContextMenu.java
@@ -1,11 +1,18 @@
 package com.nonlinearlabs.client.world.overlay.belt.parameters;
 
+import com.google.gwt.canvas.dom.client.Context2d;
+import com.nonlinearlabs.client.Millimeter;
 import com.nonlinearlabs.client.dataModel.editBuffer.RibbonParameterModel.TouchBehaviour;
 import com.nonlinearlabs.client.dataModel.setup.SetupModel;
 import com.nonlinearlabs.client.world.Control;
+import com.nonlinearlabs.client.world.Gray;
 import com.nonlinearlabs.client.world.Position;
+import com.nonlinearlabs.client.world.RGB;
+import com.nonlinearlabs.client.world.RGBA;
+import com.nonlinearlabs.client.world.Rect;
 import com.nonlinearlabs.client.world.maps.parameters.PlayControls.SourcesAndAmounts.Sources.Ribbon;
 import com.nonlinearlabs.client.world.overlay.ContextMenuItem;
+import com.nonlinearlabs.client.world.overlay.OverlayControl;
 import com.nonlinearlabs.client.world.overlay.OverlayLayout;
 
 public class RibbonContextMenu extends ParameterContextMenu {
@@ -32,6 +39,28 @@ public class RibbonContextMenu extends ParameterContextMenu {
 			public Control click(Position eventPoint) {
 				getNonMaps().getServerProxy().setRibbonReturnMode(param.getParameterNumber(), "return");
 				return super.click(eventPoint);
+			}
+		});
+
+		addChild(new ContextMenuItem(this, "")
+		{
+			@Override
+			public Control click(Position eventPoint) {
+				return this;
+			}
+
+			@Override
+			public void draw(Context2d ctx, int invalidationMask) {
+				Rect pos = getPixRect();
+				ctx.setFillStyle(new Gray(128).toString());
+				ctx.beginPath();
+				ctx.rect(pos.getLeft(), pos.getTop(), pos.getWidth(), pos.getHeight());				
+				ctx.fill();
+			}
+
+			@Override
+			public double getDesiredHeight() {
+				return Millimeter.toPixels(0.5);
 			}
 		});
 
