ad5e1c907639c3499658be2f89c45d015cfde27a
AntonSchmied <anton.schmied@hotmail.de>
2022-10-07 11:58:37 +0200

    update scripts cleanup

--- build-tools/create-c15-update/update_scripts/epc_pull_update.sh
+++ build-tools/create-c15-update/update_scripts/epc_pull_update.sh
@@ -47,12 +47,13 @@ add_ping_test_to_nlhook() {
   executeAsRoot "mount /dev/sda1 /mnt/boot/"
 
   if ! executeAsRoot "cat /mnt/usr/lib/initcpio/hooks/nlhook | grep ping_bbb"; then
-    LINE_NUMBER=$(executeAsRoot "cat /mnt/usr/lib/initcpio/hooks/nlhook | grep -n \"try_update_from_network() {\" | cut -f1 -d:")
-    NEXT_LINE=$(( ${LINE_NUMBER} + 1 ))
-    executeAsRoot "sed -i \"${NEXT_LINE}i       ping_bbb\" /mnt/usr/lib/initcpio/hooks/nlhook"
-    executeAsRoot "sed -i \"${LINE_NUMBER}i ping_bbb() { for R in \$\(seq 20\)\; do ping -c1 -W1 192.168.10.11 \&\& break\; done \; }\" /mnt/usr/lib/initcpio/hooks/nlhook"
-    executeAsRoot "mount /dev/sda1 /mnt/boot/"
-    executeAsRoot "systemd-nspawn -D /mnt mkinitcpio -p linux-rt"
+    LINE_NUMBER=$(executeAsRoot "cat /mnt/usr/lib/initcpio/hooks/nlhook | grep -n \"try_update_from_network() {\" | cut -f1 -d:") \
+    && NEXT_LINE=$(( ${LINE_NUMBER} + 1 )) \
+    && executeAsRoot "sed -i \"${NEXT_LINE}i       ping_bbb\" /mnt/usr/lib/initcpio/hooks/nlhook" \
+    && executeAsRoot "sed -i \"${LINE_NUMBER}i ping_bbb() { for R in \$\(seq 20\)\; do ping -c1 -W1 192.168.10.11 \&\& break\; done \; }\" /mnt/usr/lib/initcpio/hooks/nlhook" \
+    && executeAsRoot "mount /dev/sda1 /mnt/boot/" \
+    && executeAsRoot "systemd-nspawn -D /mnt mkinitcpio -p linux-rt"
+    || printf "E48 ePC fix: adding ping test to nlhook" >> /update/errors.log
   fi
 
   executeAsRoot "umount /mnt/boot"
@@ -78,7 +79,7 @@ update(){
     fi
     
     add_ping_test_to_nlhook
-    epc_fix
+    epc_fix || /update/utilities/sshpass -p "sscl" scp -r sscl@$EPC_IP:/tmp/fix_error.log /dev/stdout | cat - >> /update/errors.log
 
     for RETRYCOUNTER in {1..5}; do
         echo "restarting epc (try $RETRYCOUNTER)"
@@ -96,6 +97,7 @@ update(){
     done
 
     killall thttpd
+    cat /update/EPC/server.log >> /update/errors.log
     rm /update/EPC/server.log
     report_and_quit "E47 ePC update: pulling update failed ..." "47"
     return 0
