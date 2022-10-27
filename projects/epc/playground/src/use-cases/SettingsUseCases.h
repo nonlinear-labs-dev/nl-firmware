f3d17c51d4f2db142f857b0cddfd1cdc6ff7f4d5
schlenkibus <justus.scheil@parteimail.de>
2022-10-27 16:24:57 +0200

    added screensaver reset in Setup-Navigation closes #3370

--- projects/epc/playground/src/use-cases/SettingsUseCases.h
+++ projects/epc/playground/src/use-cases/SettingsUseCases.h
@@ -24,6 +24,9 @@ class SettingsUseCases
   void toggleRibbonSelection();
   void setRibbonSelection(SelectedRibbons ribbonsToSelect);
 
+  //Screensaver
+  void refreshScreenSaverTimeout();
+
   //AE
   static void panicAudioEngine();
 
