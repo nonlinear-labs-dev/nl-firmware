d44b95244b4c13fef6ca34d21ee03ccf236e99e6
schlenkibus <justus.scheil@parteimail.de>
2022-11-10 17:41:48 +0100

    closes #3386, introduced freeze/thaw-PresetMessage to prevent the intermittent EditBuffer that is modified while storing to not be send to the AE, to prevent glitches and possible stolen notes (through modulation ending envelopes for example)

--- projects/epc/playground/src/use-cases/EditBufferStorePreparation.cpp
+++ projects/epc/playground/src/use-cases/EditBufferStorePreparation.cpp
@@ -1,12 +1,15 @@
 #include "EditBufferStorePreparation.h"
 #include <presets/EditBuffer.h>
 #include <libundo/undo/Scope.h>
+#include <proxies/audio-engine/AudioEngineProxy.h>
 
 EditBufferStorePreparation::EditBufferStorePreparation(EditBuffer& eb) : m_eb{eb}
 {
   auto scope = UNDO::Scope::startTrashTransaction();
   auto transaction = scope->getTransaction();
   m_oldPositions = m_eb.setHWSourcesToDefaultValues(transaction);
+  eb.getAudioEngineProxy().freezeParameterMessages();
+  eb.getAudioEngineProxy().freezePresetMessages();
 }
 
 EditBufferStorePreparation::~EditBufferStorePreparation()
@@ -14,4 +17,6 @@ EditBufferStorePreparation::~EditBufferStorePreparation()
   auto scope = UNDO::Scope::startTrashTransaction();
   auto transaction = scope->getTransaction();
   m_eb.setHWSourcesToOldPositions(transaction, m_oldPositions);
+  m_eb.getAudioEngineProxy().thawParameterMessages(false);
+  m_eb.getAudioEngineProxy().thawPresetMessages(false);
 }
