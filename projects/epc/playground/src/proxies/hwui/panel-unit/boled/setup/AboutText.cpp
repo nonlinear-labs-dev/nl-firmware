5733a66be1837975b74e170ec622ae2754a3998d
Stephan <33938408+nonlinear-labs@users.noreply.github.com>
2022-11-22 17:04:23 +0100

    Update AboutText.cpp

--- projects/epc/playground/src/proxies/hwui/panel-unit/boled/setup/AboutText.cpp
+++ projects/epc/playground/src/proxies/hwui/panel-unit/boled/setup/AboutText.cpp
@@ -8,10 +8,10 @@ AboutText::AboutText()
   setText("Development and Design:\n"
           "Stephan Schmitt, Henry Högelow, Matthias Seeber, "
           "Jan Justus Scheil, Klaus Strohhäcker, Anton Schmied, "
-          "Tino Meinert, Daniel Tzschentke, Simone Fabbri, "
-          "Christian Radtke, Pascal Hürst, Nemanja Nikodijevic, "
-          "Gabriel Mulzer, Mike Daliot, Paul Schulz, "
-          "Thomas Jaufmann, David Johannes Meyer, Ulrich Wappler",
+          "Tino Meinert, David Johannes Meyer, Daniel Tzschentke, "
+          "Simone Fabbri, Christian Radtke, Pascal Hürst, "
+          "Nemanja Nikodijevic, Thomas Jaufmann, Gabriel Mulzer, "
+          "Mike Daliot, Paul Schulz, Ulrich Wappler"
           FrameBufferColors::C255);
 }
 
