70d20066b5f34005b8f553a182fecf34e6d33578
schlenkibus <justus.scheil@parteimail.de>
2023-01-18 11:03:50 +0100

    added single sound box like split and layer have. added indication SVGs

--- projects/web/static/nonmaps/src/main/java/com/nonlinearlabs/client/world/overlay/belt/sound/SingleSoundLayout.java
+++ projects/web/static/nonmaps/src/main/java/com/nonlinearlabs/client/world/overlay/belt/sound/SingleSoundLayout.java
@@ -4,6 +4,8 @@ import com.nonlinearlabs.client.world.overlay.OverlayLayout;
 
 public class SingleSoundLayout extends SoundLayout {
 
+	private SingleSoundSettings settings;
+
 	protected SingleSoundLayout(OverlayLayout parent) {
 		super(parent);
 		setSettings(new SingleSoundSettings(this));
