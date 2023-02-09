f08833a45536f24c94e938b1dcddbd1fb3873764
schlenkibus <justus.scheil@parteimail.de>
2023-02-09 17:23:28 +0100

    removed unused member

--- projects/epc/playground/src/proxies/hwui/panel-unit/boled/setup/SetupLayout.h
+++ projects/epc/playground/src/proxies/hwui/panel-unit/boled/setup/SetupLayout.h
@@ -45,5 +45,4 @@ class SetupLayout : public Layout
   std::unique_ptr<NavTree::Setup> m_tree;
   FocusAndMode m_focusAndMode;
   SetupEditor *m_editor{};
-  sigc::connection m_afterTouchCalibratedConnection;
 };
