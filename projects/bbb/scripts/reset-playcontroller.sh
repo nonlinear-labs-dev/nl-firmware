9babea2a2a290ef8d8c23d90e1aabd4a3295f9d0
AntonSchmied <anton.schmied@hotmail.de>
2023-02-16 17:30:04 +0100

    Had to remove playcontroller-read, since this caused the reset-playcontroller.service to hang when lpc and bbb SW versions are divergent. Removed unneeded code.

--- projects/bbb/scripts/reset-playcontroller.sh
+++ projects/bbb/scripts/reset-playcontroller.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# reset the playcontroller and obtain and store UHID if successful
-if /bin/playcontroller reset 5; then
-  /bin/playcontroller req uhid64
-  /bin/playcontroller-read -a -h +q +6 +o > /tmp/UHID
-fi
+# reset the playcontroller
+/bin/playcontroller reset 5
