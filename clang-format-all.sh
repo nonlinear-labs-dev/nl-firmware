47df5abd283a3d21c9daf66ec9b829ffef302386
schlenkibus <justus.scheil@parteimail.de>
2023-03-29 13:05:18 +0200

    wip

--- clang-format-all.sh
+++ clang-format-all.sh
@@ -6,14 +6,12 @@ clang_format_file=/workdir/.clang-format
 
 echo $clang_format_file
 
+
+clang_format_files=$(find $work_dir -iname .clang-format -printf '/workdir/%P\n')
+
+echo $clang_format_files
+exit 1
+
 files=$(find $work_dir/projects/ -iname *.h -o -iname *.cpp -printf '/workdir/projects/%P\n')
 echo $files
 docker run --rm -v "$work_dir":/workdir -w /workdir clang-format:14 clang-format-14 -style=file:$clang_format_file --verbose -i $files
-exit 0
-
-for file in $(find $work_dir/projects/ -iname *.h -o -iname *.cpp -printf '/workdir/projects/%P\n'); do
-	docker run --rm -v "$work_dir":/workdir -w /workdir clang-format:14 clang-format-14 -style=file:$clang_format_file -i $file
-	echo $file
-done
-exit 1
-| xargs docker run --rm -v "$work_dir":/workdir -w /workdir clang-format:14 clang-format-14 -style=file:$clang_format_style -i
