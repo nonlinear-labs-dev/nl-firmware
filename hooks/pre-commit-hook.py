9ac21a012422adb90f872b68682b0b2603672fe0
schlenkibus <justus.scheil@parteimail.de>
2023-03-30 15:07:56 +0200

    test

--- hooks/pre-commit-hook.py
+++ hooks/pre-commit-hook.py
@@ -11,5 +11,6 @@ work_dir=reformat.do_shell_action(["git", "rev-parse", "--show-toplevel"])[0]
 for file in changed_files:
   if file.endswith(".cpp") or file.endswith(".c") or file.endswith(".h") or file.endswith(".hpp"):
     file = os.path.join(work_dir, file)
+    print("formatting file {}".format(file))
     reformat.do_shell_action(["clang-format-14", "-i", file])
     reformat.do_shell_action(["git", "add", file])
