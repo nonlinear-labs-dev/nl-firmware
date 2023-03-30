87f88ed711334a46052546330fe7f28fe1808cad
schlenkibus <justus.scheil@parteimail.de>
2023-03-30 12:25:06 +0200

    use podman instead of docker

--- hooks/create_clang_format_docker.sh
+++ hooks/create_clang_format_docker.sh
@@ -1,5 +1,5 @@
-if ! docker images | grep -q "clang-format:14"; then
-docker build -t clang-format:14 - <<EOF
+if ! podman images | grep -q "clang-format:14"; then
+podman build -t clang-format:14 - <<EOF
 FROM ubuntu:latest
 RUN apt-get update && apt-get install -y clang-format-14 python3 git
 EOF
