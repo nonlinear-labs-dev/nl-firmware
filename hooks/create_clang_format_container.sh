5c0b4f9e9d3e576e5ecfddf3799ba6c5f58ccfb8
schlenkibus <justus.scheil@parteimail.de>
2023-04-04 13:36:20 +0200

    cleanup

--- hooks/create_clang_format_container.sh
+++ hooks/create_clang_format_container.sh
@@ -1,6 +1,3 @@
 if ! podman images | grep -q "clang-format:14"; then
-podman build -t clang-format:14 - <<EOF
-FROM ubuntu:latest
-RUN apt-get update && apt-get install -y clang-format-14 python3 git
-EOF
+    podman build -t clang-format:14 $1/hooks > /dev/null
 fi
