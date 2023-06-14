bc8c61a7d106f90244a9664096813e3134681716
Matthias Seeber <matthias.seeber@nonlinear-labs.de>
2023-06-14 18:21:01 +0200

    added podman build to build-in-docker.sh

--- projects/shared/configuration/build-in-docker.sh
+++ projects/shared/configuration/build-in-docker.sh
@@ -3,4 +3,5 @@
 sourceDir="$1"
 binaryDir="$2"
 
+podman build -t nl-configuration-build-container $1
 podman run -v "$sourceDir":/sourceDir -v "$binaryDir":/binaryDir nl-configuration-build-container
