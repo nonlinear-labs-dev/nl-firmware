8f4d2dc57aed76ca068cbaba2fe5572926d34dcd
schlenkibus <justus.scheil@parteimail.de>
2023-06-27 11:14:15 +0200

    hotfix update and local build

--- projects/shared/configuration/build-in-docker.sh
+++ projects/shared/configuration/build-in-docker.sh
@@ -2,7 +2,16 @@
 
 sourceDir="$1"
 binaryDir="$2"
+isDevPC="$3"
 container="nl-configuration-build-container"
 
-podman build -t $container $1
-podman run -v "$sourceDir":/sourceDir -v "$binaryDir":/binaryDir $container /sourceDir/process.sh /sourceDir /binaryDir
+if [ "$isDevPC" == "On" ] || [ "$isDevPC" == "ON" ] || [ "$isDevPC" == "on" ]
+then
+  echo "building configuration in podman (Development PC)"
+  podman build -t $container $1
+  podman run -v "$sourceDir":/sourceDir -v "$binaryDir":/binaryDir $container /sourceDir/process.sh /sourceDir /binaryDir
+else
+  echo "building configuration with native tools (Build Update)"
+  /bin/bash "$sourceDir"/process.sh "$sourceDir" "$binaryDir"
+fi
+
